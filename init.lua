7b9a82194ff7f0526b2075cac1a1f5b598555b63
Shekhov Ismoil <veyxov@gmail.com>
2022-07-03 16:43:13 +0500

    Added the base template.

---
+++ init.lua
@@ -0,0 +1,6 @@
+require 'impatient'
+
+require 'plugins'
+require 'options'
+require 'autocmd'
+require 'mapings'
---
+++ lua/plugins.lua
@@ -0,0 +1,15 @@
+local kfg = function(name) return string.format('require("cfg/%s")', name) end
+
+require 'packer'.startup({ function(use)
+    -- Global dependencies
+    use {
+        'lewis6991/impatient.nvim',
+        'wbthomason/packer.nvim',
+        'nvim-lua/plenary.nvim',
+        'kyazdani42/nvim-web-devicons'
+    }
+
+end,
+    config = {
+        git = { clone_timeout = nil }
+    } })
