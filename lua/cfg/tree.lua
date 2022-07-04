3dc43884a976d469ff8d4779db0b3ac3f690b7e1
Shekhov Ismoil <veyxov@gmail.com>
2022-07-04 09:24:15 +0500

    Removed unneccessary configurations

--- lua/cfg/tree.lua
+++ lua/cfg/tree.lua
@@ -1,18 +1,1 @@
-require("nvim-tree").setup({
-    sort_by = "case_sensitive",
-    view = {
-        adaptive_size = true,
-        mappings = {
-            list = {
-                { key = "u", action = "dir_up" },
-            },
-        },
-    },
-    renderer = {
-        group_empty = true,
-    },
-    filters = {
-        dotfiles = true,
-    },
-})
-vim.opt.number=true
+require("nvim-tree").setup({ })
