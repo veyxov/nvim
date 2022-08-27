c84c0258a9ceb86d087fd55209d81d3dd0b2f0d7
Shekhov Ismoil <veyxov@gmail.com>
2022-08-27 16:31:53 +0500

    don't set a colorscheme upon startup

--- lua/cfg/solarized.lua
+++ lua/cfg/solarized.lua
@@ -3,9 +3,8 @@ vim.g.NeoSolarized_lineNr = 0 -- 0 or 1 (default) -> To Show backgroung in LineN
 
 vim.cmd [[
     try
-    colorscheme NeoSolarized
     catch /^Vim\%((\a\+)\)\=:E18/
-    colorscheme default
+
     set background=dark
     endtry
     highlight FloatBorder guibg=NONE ctermbg=NONE  " Removes the border of float menu (LSP and Autocompletion uses it)
