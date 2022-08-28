98d58e52311dd19fcf30b36d853c49a0af998fa2
Shekhov Ismoil <veyxov@gmail.com>
2022-08-28 21:11:47 +0500

    added hydra config for gitsigns

--- lua/cfg/hydra.lua
+++ lua/cfg/hydra.lua
@@ -1,5 +1,6 @@
 local Cmd = require 'globals'.Cmd
 local Hydra = require("hydra")
+local gitsigns = require "gitsigns"
 
 Hydra({
 	name = "Change / Resize Window",
@@ -81,4 +82,60 @@ Hydra({
         { 'b', Cmd 'lua require "dap".toggle_breakpoint()' },
         { 'x', Cmd 'lua require "dap".disconnect()' },
     }
+})
+Hydra({
+   name = 'Git',
+   hint = hint,
+   config = {
+      buffer = bufnr,
+      color = 'pink',
+      invoke_on_body = true,
+      hint = {
+         border = 'rounded'
+      },
+      on_enter = function()
+         vim.cmd 'mkview'
+         vim.cmd 'silent! %foldopen!'
+         vim.bo.modifiable = false
+         gitsigns.toggle_signs(true)
+         gitsigns.toggle_linehl(true)
+      end,
+      on_exit = function()
+         local cursor_pos = vim.api.nvim_win_get_cursor(0)
+         vim.cmd 'loadview'
+         vim.api.nvim_win_set_cursor(0, cursor_pos)
+         vim.cmd 'normal zv'
+         gitsigns.toggle_signs(false)
+         gitsigns.toggle_linehl(false)
+         gitsigns.toggle_deleted(false)
+      end,
+   },
+   mode = {'n','x'},
+   body = '<leader>g',
+   heads = {
+      { 'J',
+         function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gitsigns.next_hunk() end)
+            return '<Ignore>'
+         end,
+         { expr = true, desc = 'next hunk' } },
+      { 'K',
+         function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gitsigns.prev_hunk() end)
+            return '<Ignore>'
+         end,
+         { expr = true, desc = 'prev hunk' } },
+      { 's', ':Gitsigns stage_hunk<CR>', { silent = true, desc = 'stage hunk' } },
+      { 'u', gitsigns.undo_stage_hunk, { desc = 'undo last stage' } },
+      { 'S', gitsigns.stage_buffer, { desc = 'stage buffer' } },
+      { 'p', gitsigns.preview_hunk, { desc = 'preview hunk' } },
+      { 'd', gitsigns.toggle_deleted, { nowait = true, desc = 'toggle deleted' } },
+      { 'b', gitsigns.blame_line, { desc = 'blame' } },
+      { 'B', function() gitsigns.blame_line{ full = true } end, { desc = 'blame show full' } },
+      { '/', gitsigns.show, { exit = true, desc = 'show base file' } }, -- show the base of the file
+      { '<Enter>', '<Cmd>Neogit<CR>', { exit = true, desc = 'Neogit' } },
+      { 'q', nil, { exit = true, nowait = true, desc = 'exit' } },
+   }
 })
