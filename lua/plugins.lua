a67ba534dc6137aa2621a0ca213a81cbc86f946c
Shekhov Ismoil <veyxov@gmail.com>
2022-09-06 16:34:34 +0500

    cleanup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,83 +11,15 @@ require 'packer'.startup({ function(use)
     use {
         'nvim-lualine/lualine.nvim',
         config = function ()
-            require('lualine').setup() {
-                options = { section_separators = '', component_separators = '' }
-            }
+            require('lualine').setup()
         end
     }
 
     use {
         'nvim-treesitter/nvim-treesitter-context',
         config = function()
-            require'treesitter-context'.setup{
-                enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-                max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-                trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-                patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-                -- For all filetypes
-                -- Note that setting an entry here replaces all other patterns for this entry.
-                -- By setting the 'default' entry below, you can control which nodes you want to
-                -- appear in the context window.
-                default = {
-                    'class',
-                    'function',
-                    'method',
-                    'for',
-                    'while',
-                    'if',
-                    'switch',
-                    'case',
-                },
-                -- Patterns for specific filetypes
-                -- If a pattern is missing, *open a PR* so everyone can benefit.
-                tex = {
-                    'chapter',
-                    'section',
-                    'subsection',
-                    'subsubsection',
-                },
-                rust = {
-                    'impl_item',
-                    'struct',
-                    'enum',
-                },
-                scala = {
-                    'object_definition',
-                },
-                vhdl = {
-                    'process_statement',
-                    'architecture_body',
-                    'entity_declaration',
-                },
-                markdown = {
-                    'section',
-                },
-                elixir = {
-                    'anonymous_function',
-                    'arguments',
-                    'block',
-                    'do_block',
-                    'list',
-                    'map',
-                    'tuple',
-                    'quoted_content',
-                },
-                json = {
-                    'pair',
-                },
-                yaml = {
-                    'block_mapping_pair',
-                },
-            },
-            zindex = 20, -- The Z-index of the context window
-            mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
-            -- Separator between context and content. Should be a single character string, like '-'.
-            -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-            separator = nil,
-        }
+            require 'treesitter-context'.setup {}
         end
-
     }
 
     use 'ThePrimeagen/harpoon'
@@ -199,15 +131,11 @@ require 'packer'.startup({ function(use)
         },
     }
 
-    use({
+    use {
         "kylechui/nvim-surround",
-        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-        config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
-            })
-        end
-    })
+        tag = "*",
+        config = function() require("nvim-surround").setup({ }) end
+    }
 end,
     config = { git = { clone_timeout = nil } }
 })
