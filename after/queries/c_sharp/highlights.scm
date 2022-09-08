b09e5a37656e472f247f81acd8b4f6a5408edf02
Shekhov Ismoil <veyxov@gmail.com>
2022-09-08 20:57:08 +0500

    added more conceal

--- after/queries/c_sharp/highlights.scm
+++ after/queries/c_sharp/highlights.scm
@@ -1,12 +1,37 @@
 (("if" @keyword) (#set! conceal "?"))
+(("else" @keyword) (#set! conceal "¿"))
+
 (("using" @keyword) (#set! conceal ""))
 (("namespace" @keyword) (#set! conceal "笠"))
-(("else" @keyword) (#set! conceal "¿"))
+
 (("await" @keyword) (#set! conceal "神"))
+(("async" @keyword) (#set! conceal ""))
+
 (("return" @keyword) (#set! conceal ""))
 (("break" @keyword) (#set! conceal ""))
-(("new" @keyword) (#set! conceal ""))
-(("foreach" @keyword) (#set! conceal ""))
-(((method_declaration type:(generic_name (identifier) @iden (#eq? @iden "Task")))) (#set! conceal "ﲆ"))
 
-(((variable_declaration type:(implicit_type) @variable) (#set! conceal "")))
+(("public" @comment) (#set! conceal ""))
+(("private" @keyword) (#set! conceal ""))
+(("partial" @keyword) (#set! conceal ""))
+(("protected" @keyword) (#set! conceal ""))
+(("readonly" @keyword) (#set! conceal ""))
+(("const" @keyword) (#set! conceal ""))
+(("static" @keyword) (#set! conceal ""))
+(("class" @keyword) (#set! conceal "更"))
+(("abstract" @keyword) (#set! conceal "擄"))
+(("new" @keyword) (#set! conceal ""))
+(("interface" @keyword) (#set! conceal ""))
+(("get" @keyword) (#set! conceal "ﰵ"))
+(("set" @keyword) (#set! conceal "ﰬ"))
+(("init" @keyword) (#set! conceal ""))
+
+(((method_declaration type:(void_keyword) @keyword)) (#set! conceal "ﳠ"))
+
+(("foreach" @keyword) (#set! conceal ""))
+(("in" @keyword) (#set! conceal ""))
+
+(((method_declaration type:(generic_name (identifier) @TStag (#eq? @TStag "Task")))) (#set! conceal "ﲆ"))
+(((method_declaration type:(generic_name (identifier) @TStag (#eq? @TStag "ErrorOr")))) (#set! conceal ""))
+
+(("var" @keyword) (#set! conceal ""))
+(((variable_declaration type:(implicit_type) @variable) (#set! conceal "")))
