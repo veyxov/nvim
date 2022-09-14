a19378d031cb2b214bb5a821e377c82d23ac1b9f
Shekhov Ismoil <veyxov@gmail.com>
2022-09-14 20:19:49 +0500

    added folding plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,6 +13,23 @@ require 'packer'.startup({ function(use)
     }
 
     use {
+        'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async',
+        config = function()
+            vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+            vim.o.foldlevelstart = 99
+            vim.o.foldenable = true
+
+            vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+            vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+            require('ufo').setup({
+                provider_selector = function(bufnr, filetype, buftype)
+                    return {'treesitter', 'indent'}
+                end
+            })
+        end
+    }
+
+    use {
         'nvim-treesitter/nvim-treesitter-context',
         config = function()
             require 'treesitter-context'.setup {}
