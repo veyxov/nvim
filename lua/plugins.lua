cac99e40c5651bd09ffb1ff7232e2d9e36f176d3
Shekhov Ismoil <veyxov@gmail.com>
2022-09-14 20:30:55 +0500

    added i swap

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,6 +13,52 @@ require 'packer'.startup({ function(use)
     }
 
     use {
+        "mizlan/iswap.nvim",
+        config = function ()
+            require('iswap').setup{
+                keys = 'aorisetndhywufmgq',
+
+                -- Grey out the rest of the text when making a selection
+                -- (enabled by default)
+                grey = 'disable',
+
+                -- Highlight group for the sniping value (asdf etc.)
+                -- default 'Search'
+                hl_snipe = 'ErrorMsg',
+
+                -- Highlight group for the visual selection of terms
+                -- default 'Visual'
+                hl_selection = 'WarningMsg',
+
+                -- Highlight group for the greyed background
+                -- default 'Comment'
+                hl_grey = 'LineNr',
+
+                -- Post-operation flashing highlight style,
+                -- either 'simultaneous' or 'sequential', or false to disable
+                -- default 'sequential'
+                flash_style = false,
+
+                -- Highlight group for flashing highlight afterward
+                -- default 'IncSearch'
+                hl_flash = 'ModeMsg',
+
+                -- Move cursor to the other element in ISwap*With commands
+                -- default false
+                move_cursor = true,
+
+                -- Automatically swap with only two arguments
+                -- default nil
+                autoswap = true,
+
+                -- Other default options you probably should not change:
+                debug = nil,
+                hl_grey_priority = '1000',
+            }
+        end
+    }
+
+    use {
         'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async',
         config = function()
             vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
@@ -39,7 +85,9 @@ require 'packer'.startup({ function(use)
     use {
         'monkoose/matchparen.nvim',
         config = function ()
-            require('matchparen').setup()
+            require('matchparen').setup({
+                hl_group = 'WarningMsg', -- highlight group for matched characters
+            })
         end
     }
 
@@ -61,8 +109,7 @@ require 'packer'.startup({ function(use)
     },{
         'glepnir/lspsaga.nvim',
         config = kfg 'lspsaga',
-    }
-    }
+    }}
 
     -- Auto completion and code analiz
     use {
