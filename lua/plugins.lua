c275865d681ffa4c84041f7f1c5ea81605942039
Shekhov Ismoil <veyxov@gmail.com>
2022-10-08 15:40:32 +0500

    disable built-in virtual text for diagnostics

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -188,6 +188,9 @@ require 'packer'.startup({ function(use)
     use {
         "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
         config = function()
+            vim.diagnostic.config({
+                virtual_text = false,
+            })
             require("lsp_lines").setup()
         end,
         after = "nvim-lspconfig"
