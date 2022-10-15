f224a2a45fde523b10bdb32e8c0b6e233eeedc4c
Shekhov Ismoil <veyxov@gmail.com>
2022-10-15 17:45:44 +0500

    update

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -75,7 +75,7 @@ time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["Comment.nvim"] = {
-    config = { "\27LJ\2\nﬂ\1\0\0\4\0\f\0\0156\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0=\3\a\0025\3\b\0=\3\t\0025\3\n\0=\3\v\2B\0\2\1K\0\1\0\rmappings\1\0\3\nextra\2\rextended\1\nbasic\2\nextra\1\0\3\nbelow\bgco\beol\agA\nabove\bgcO\ropleader\1\0\2\tline\agc\nblock\agb\ftoggler\1\0\0\1\0\2\tline\bgcc\nblock\bgbc\nsetup\fComment\frequire\0" },
+    config = { "\27LJ\2\nﬂ\1\0\0\4\0\f\0\0156\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0=\3\a\0025\3\b\0=\3\t\0025\3\n\0=\3\v\2B\0\2\1K\0\1\0\rmappings\1\0\3\rextended\1\nbasic\2\nextra\2\nextra\1\0\3\nabove\bgcO\beol\agA\nbelow\bgco\ropleader\1\0\2\tline\agc\nblock\agb\ftoggler\1\0\0\1\0\2\tline\bgcc\nblock\bgbc\nsetup\fComment\frequire\0" },
     loaded = true,
     path = "/home/iz/.local/share/nvim/site/pack/packer/start/Comment.nvim",
     url = "https://github.com/numToStr/Comment.nvim"
@@ -89,7 +89,7 @@ _G.packer_plugins = {
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["centerpad.nvim"] = {
-    config = { "\27LJ\2\n®\1\0\0\6\0\a\0\t6\0\0\0009\0\1\0009\0\2\0'\2\3\0'\3\4\0'\4\5\0005\5\6\0B\0\5\1K\0\1\0\1\0\2\vsilent\2\fnoremap\2I<cmd>lua require'centerpad'.toggle{ leftpad = 45, rightpad = 0 }<cr>\14<leader>z\6n\20nvim_set_keymap\bapi\bvim\0" },
+    config = { "\27LJ\2\n®\1\0\0\6\0\a\0\t6\0\0\0009\0\1\0009\0\2\0'\2\3\0'\3\4\0'\4\5\0005\5\6\0B\0\5\1K\0\1\0\1\0\2\fnoremap\2\vsilent\2I<cmd>lua require'centerpad'.toggle{ leftpad = 45, rightpad = 0 }<cr>\14<leader>z\6n\20nvim_set_keymap\bapi\bvim\0" },
     loaded = true,
     path = "/home/iz/.local/share/nvim/site/pack/packer/start/centerpad.nvim",
     url = "https://github.com/smithbm2316/centerpad.nvim"
@@ -157,7 +157,7 @@ _G.packer_plugins = {
     url = "https://github.com/sainnhe/everforest"
   },
   ["gruvbox.nvim"] = {
-    config = { "\27LJ\2\ne\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\3\rcontrast\thard\21invert_selection\1\finverse\2\nsetup\fgruvbox\frequire\0" },
+    config = { "\27LJ\2\ne\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\3\21invert_selection\1\finverse\2\rcontrast\thard\nsetup\fgruvbox\frequire\0" },
     loaded = false,
     needs_bufread = false,
     only_cond = false,
@@ -181,7 +181,7 @@ _G.packer_plugins = {
     url = "https://github.com/anuvyklack/keymap-layer.nvim"
   },
   ["leap.nvim"] = {
-    config = { "\27LJ\2\n’\2\0\0\5\0\15\0\0226\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\0016\0\0\0'\2\1\0B\0\2\0029\0\3\0005\2\5\0005\3\4\0=\3\6\0025\3\a\0=\3\b\0025\3\t\0005\4\n\0=\4\v\0035\4\f\0=\4\r\3=\3\14\2B\0\2\1K\0\1\0\17special_keys\15prev_match\1\3\0\0\6,\n<tab>\15next_match\1\3\0\0\6;\f<enter>\1\0\4\19next_aot_match\f<enter>\15prev_group\n<tab>\18repeat_search\f<enter>\15next_group\f<space>\vlabels\1\t\0\0\6n\6e\6i\6o\6a\6r\6s\6t\16safe_labels\1\0\0\1\t\0\0\6n\6e\6i\6o\6a\6r\6s\6t\nsetup\24set_default_keymaps\tleap\frequire\0" },
+    config = { "\27LJ\2\n’\2\0\0\5\0\15\0\0226\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\0016\0\0\0'\2\1\0B\0\2\0029\0\3\0005\2\5\0005\3\4\0=\3\6\0025\3\a\0=\3\b\0025\3\t\0005\4\n\0=\4\v\0035\4\f\0=\4\r\3=\3\14\2B\0\2\1K\0\1\0\17special_keys\15prev_match\1\3\0\0\6,\n<tab>\15next_match\1\3\0\0\6;\f<enter>\1\0\4\15prev_group\n<tab>\18repeat_search\f<enter>\15next_group\f<space>\19next_aot_match\f<enter>\vlabels\1\t\0\0\6n\6e\6i\6o\6a\6r\6s\6t\16safe_labels\1\0\0\1\t\0\0\6n\6e\6i\6o\6a\6r\6s\6t\nsetup\24set_default_keymaps\tleap\frequire\0" },
     keys = { { "", "s" }, { "", "S" } },
     loaded = false,
     needs_bufread = false,
@@ -242,7 +242,7 @@ _G.packer_plugins = {
     url = "https://github.com/MunifTanjim/nui.nvim"
   },
   ["nvim-cmp"] = {
-    after = { "cmp-buffer", "cmp-cmdline", "cmp-path", "cmp_luasnip", "cmp-nvim-lsp" },
+    after = { "cmp-buffer", "cmp-cmdline", "cmp-nvim-lsp", "cmp_luasnip", "cmp-path" },
     config = { 'require("cfg/cmp")' },
     load_after = {
       ["nvim-lspconfig"] = true
@@ -253,7 +253,7 @@ _G.packer_plugins = {
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
-    after = { "lsp_lines.nvim", "nvim-cmp" },
+    after = { "nvim-cmp", "lsp_lines.nvim" },
     config = { 'require("cfg/lsp")' },
     load_after = {
       ["mason.nvim"] = true
@@ -264,7 +264,7 @@ _G.packer_plugins = {
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-surround"] = {
-    config = { "\27LJ\2\n–\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\fkeymaps\1\0\0\1\0\t\vchange\akc\16normal_line\akS\15normal_cur\bkss\vnormal\aks\16insert_line\v<C-g>S\vdelete\ads\16visual_line\agS\vvisual\6K\20normal_cur_line\bySS\nsetup\18nvim-surround\frequire\0" },
+    config = { "\27LJ\2\n–\1\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\fkeymaps\1\0\0\1\0\t\20normal_cur_line\bySS\vchange\akc\16normal_line\akS\15normal_cur\bkss\vnormal\aks\16insert_line\v<C-g>S\vdelete\ads\16visual_line\agS\vvisual\6K\nsetup\18nvim-surround\frequire\0" },
     loaded = false,
     needs_bufread = false,
     only_cond = false,
@@ -339,37 +339,37 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: centerpad.nvim
-time([[Config for centerpad.nvim]], true)
-try_loadstring("\27LJ\2\n®\1\0\0\6\0\a\0\t6\0\0\0009\0\1\0009\0\2\0'\2\3\0'\3\4\0'\4\5\0005\5\6\0B\0\5\1K\0\1\0\1\0\2\vsilent\2\fnoremap\2I<cmd>lua require'centerpad'.toggle{ leftpad = 45, rightpad = 0 }<cr>\14<leader>z\6n\20nvim_set_keymap\bapi\bvim\0", "config", "centerpad.nvim")
-time([[Config for centerpad.nvim]], false)
--- Config for: hydra.nvim
-time([[Config for hydra.nvim]], true)
-require("cfg/hydra")
-time([[Config for hydra.nvim]], false)
--- Config for: delaytrain.nvim
-time([[Config for delaytrain.nvim]], true)
-try_loadstring("\27LJ\2\n¨\1\0\0\5\0\n\0\r6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\5\0005\4\4\0=\4\6\0035\4\a\0=\4\b\3=\3\t\2B\0\2\1K\0\1\0\tkeys\bnvi\1\5\0\0\v<Left>\v<Down>\t<Up>\f<Right>\anv\1\0\0\1\5\0\0\6h\6j\6k\6l\1\0\2\17grace_period\3\1\rdelay_ms\3–\15\nsetup\15delaytrain\frequire\0", "config", "delaytrain.nvim")
-time([[Config for delaytrain.nvim]], false)
--- Config for: stay-centered.nvim
-time([[Config for stay-centered.nvim]], true)
-try_loadstring("\27LJ\2\n-\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\18stay-centered\frequire\0", "config", "stay-centered.nvim")
-time([[Config for stay-centered.nvim]], false)
 -- Config for: everforest
 time([[Config for everforest]], true)
 try_loadstring("\27LJ\2\na\0\0\3\0\6\0\t6\0\0\0009\0\1\0'\1\3\0=\1\2\0006\0\0\0009\0\4\0'\2\5\0B\0\2\1K\0\1\0\21color everforest\bcmd\thard\26everforest_background\6g\bvim\0", "config", "everforest")
 time([[Config for everforest]], false)
 -- Config for: Comment.nvim
 time([[Config for Comment.nvim]], true)
-try_loadstring("\27LJ\2\nﬂ\1\0\0\4\0\f\0\0156\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0=\3\a\0025\3\b\0=\3\t\0025\3\n\0=\3\v\2B\0\2\1K\0\1\0\rmappings\1\0\3\nextra\2\rextended\1\nbasic\2\nextra\1\0\3\nbelow\bgco\beol\agA\nabove\bgcO\ropleader\1\0\2\tline\agc\nblock\agb\ftoggler\1\0\0\1\0\2\tline\bgcc\nblock\bgbc\nsetup\fComment\frequire\0", "config", "Comment.nvim")
+try_loadstring("\27LJ\2\nﬂ\1\0\0\4\0\f\0\0156\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\0025\3\6\0=\3\a\0025\3\b\0=\3\t\0025\3\n\0=\3\v\2B\0\2\1K\0\1\0\rmappings\1\0\3\rextended\1\nbasic\2\nextra\2\nextra\1\0\3\nabove\bgcO\beol\agA\nbelow\bgco\ropleader\1\0\2\tline\agc\nblock\agb\ftoggler\1\0\0\1\0\2\tline\bgcc\nblock\bgbc\nsetup\fComment\frequire\0", "config", "Comment.nvim")
 time([[Config for Comment.nvim]], false)
+-- Config for: centerpad.nvim
+time([[Config for centerpad.nvim]], true)
+try_loadstring("\27LJ\2\n®\1\0\0\6\0\a\0\t6\0\0\0009\0\1\0009\0\2\0'\2\3\0'\3\4\0'\4\5\0005\5\6\0B\0\5\1K\0\1\0\1\0\2\fnoremap\2\vsilent\2I<cmd>lua require'centerpad'.toggle{ leftpad = 45, rightpad = 0 }<cr>\14<leader>z\6n\20nvim_set_keymap\bapi\bvim\0", "config", "centerpad.nvim")
+time([[Config for centerpad.nvim]], false)
+-- Config for: hydra.nvim
+time([[Config for hydra.nvim]], true)
+require("cfg/hydra")
+time([[Config for hydra.nvim]], false)
+-- Config for: stay-centered.nvim
+time([[Config for stay-centered.nvim]], true)
+try_loadstring("\27LJ\2\n-\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\18stay-centered\frequire\0", "config", "stay-centered.nvim")
+time([[Config for stay-centered.nvim]], false)
+-- Config for: delaytrain.nvim
+time([[Config for delaytrain.nvim]], true)
+try_loadstring("\27LJ\2\n¨\1\0\0\5\0\n\0\r6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0005\3\5\0005\4\4\0=\4\6\0035\4\a\0=\4\b\3=\3\t\2B\0\2\1K\0\1\0\tkeys\bnvi\1\5\0\0\v<Left>\v<Down>\t<Up>\f<Right>\anv\1\0\0\1\5\0\0\6h\6j\6k\6l\1\0\2\17grace_period\3\1\rdelay_ms\3–\15\nsetup\15delaytrain\frequire\0", "config", "delaytrain.nvim")
+time([[Config for delaytrain.nvim]], false)
 
 -- Command lazy-loads
 time([[Defining lazy-load commands]], true)
 pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file LspStart lua require("packer.load")({'mason-lspconfig.nvim'}, { cmd = "LspStart", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
-pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file Lspsaga lua require("packer.load")({'lspsaga.nvim'}, { cmd = "Lspsaga", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file Neotree lua require("packer.load")({'neo-tree.nvim'}, { cmd = "Neotree", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file Telescope lua require("packer.load")({'telescope.nvim'}, { cmd = "Telescope", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
+pcall(vim.cmd, [[command -nargs=* -range -bang -complete=file Lspsaga lua require("packer.load")({'lspsaga.nvim'}, { cmd = "Lspsaga", l1 = <line1>, l2 = <line2>, bang = <q-bang>, args = <q-args>, mods = "<mods>" }, _G.packer_plugins)]])
 time([[Defining lazy-load commands]], false)
 
 -- Keymap lazy-loads
@@ -382,7 +382,7 @@ vim.cmd [[augroup packer_load_aucmds]]
 vim.cmd [[au!]]
   -- Event lazy-loads
 time([[Defining lazy-load event autocommands]], true)
-vim.cmd [[au InsertEnter * ++once lua require("packer.load")({'LuaSnip', 'vim-moonfly-colors', 'smart-pairs', 'gruvbox.nvim', 'nvim-treesitter', 'nvim-surround'}, { event = "InsertEnter *" }, _G.packer_plugins)]]
+vim.cmd [[au InsertEnter * ++once lua require("packer.load")({'gruvbox.nvim', 'vim-moonfly-colors', 'nvim-treesitter', 'nvim-surround', 'LuaSnip', 'smart-pairs'}, { event = "InsertEnter *" }, _G.packer_plugins)]]
 time([[Defining lazy-load event autocommands]], false)
 vim.cmd("augroup END")
 
