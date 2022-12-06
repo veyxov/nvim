56bb366158d70513c544bc24493d3edd81770d0d
Shekhov Ismoil <veyxov@gmail.com>
2022-12-06 22:53:47 +0500

    remove incremental selection

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -86,16 +86,6 @@ require('packer').startup(function(use)
 		config = function()
 			require('nvim-treesitter.configs').setup {
 				highlight = { enable = true },
-				indent = { enable = true },
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = '<c-space>',
-						node_incremental = '<c-space>',
-						scope_incremental = '<c-s>',
-						node_decremental = '<c-backspace>',
-					},
-				},
 				textobjects = {
 					select = {
 						enable = true,
