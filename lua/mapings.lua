85f8cb744ac945ae15e4956e1501bb84f78840be
Shekhov Ismoil <veyxov@gmail.com>
2022-12-09 22:59:30 +0500

    documentation for mappings

--- lua/mapings.lua
+++ lua/mapings.lua
@@ -1,23 +1,24 @@
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true }) -- What is this ?
 
-vim.keymap.set('n', '<cr>w', ':wall<CR>')
-vim.keymap.set('n', '<cr>q', ':q<CR>')
-vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('n', '<cr>w', ':wall<CR>') -- Save file ***
+vim.keymap.set('n', '<cr>q', ':q<CR>') -- Close buffer **
+vim.keymap.set('n', '<cr>Q', ':qall!<CR>') -- Force quit
+vim.keymap.set('n', '<leader>y', '"+y') -- Copy to system clipboar
 
-vim.keymap.set('n', '<cr>g', ":silent !tmux neww 'lazygit; exit'<CR>")
+vim.keymap.set('n', '<cr>g', ":silent !tmux neww 'lazygit; exit'<CR>") -- Open git manager **
 
-vim.keymap.set('n', '<cr>s', "<cmd>Telescope find_files<cr>", { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<cr>f', "<cmd>Format<cr>", { desc = 'Format current file' })
+vim.keymap.set('n', '<cr>s', "<cmd>Telescope find_files<cr>", { desc = '[S]earch [F]iles' }) -- Fuzzy find files ****
+vim.keymap.set('n', '<cr>f', "<cmd>Format<cr>", { desc = 'Format current file' }) -- Format code using LSP
 
-vim.keymap.set('n', 'hh', "<cmd>Telescope help_tags<cr>", { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', 'hg', "<cmd>Telescope grep_string<cr>", { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', 'hd', "<cmd>Telescope diagnostics<cr>", { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', 'hh', "<cmd>Telescope help_tags<cr>", { desc = '[S]earch [H]elp' }) -- Seach in help
+vim.keymap.set('n', 'hg', "<cmd>Telescope grep_string<cr>", { desc = '[S]earch by [G]rep' }) -- Grep search *
+vim.keymap.set('n', 'hd', "<cmd>Telescope diagnostics<cr>", { desc = '[S]earch [D]iagnostics' }) -- Search in diagnostics
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '<leader>Q', vim.diagnostic.setloclist)
+vim.keymap.set('n', 'dn', vim.diagnostic.goto_prev) -- Go to next problem/warning
+vim.keymap.set('n', 'dN', vim.diagnostic.goto_next) -- Go to prev problem/warning
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float) -- Show full diagnostics for current line
+vim.keymap.set('n', '<leader>D', vim.diagnostic.setloclist) -- Show list of diagnostics
 
 vim.keymap.set('n', '<C-N>', "<cmd>NnnPicker<cr>")
 vim.keymap.set('n', '<C-A>', "<cmd>NnnExplorer<cr>")
@@ -31,18 +32,17 @@ local nmap = function(keys, func, desc)
 end
 
 nmap('lr', vim.lsp.buf.rename, '[R]e[n]ame')
-nmap('la', vim.lsp.buf.code_action, '[C]ode [A]ction')
+nmap('<cr>a', vim.lsp.buf.code_action, '[C]ode [A]ction') -- ***
 
 nmap('ld', vim.lsp.buf.definition, '[G]oto [D]efinition')
 nmap('lD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-nmap('li', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+nmap('li', vim.lsp.buf.implementation, '[G]oto [I]mplementation') -- **
 
 -- See `:help K` for why this keymap
 nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help)
+vim.keymap.set('i', '<C-e>', vim.lsp.buf.signature_help)
 
 -- Lesser used LSP functionality
-nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
 nmap('lwa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
 nmap('lwr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
 nmap('lwl', function()
