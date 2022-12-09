5a777ef76457c323bef37824b03398577216fbbb
Shekhov Ismoil <veyxov@gmail.com>
2022-12-09 23:03:40 +0500

    single autogroup

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,19 +1,18 @@
 -- Automatically source and re-compile packer whenever you save this init.lua
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+local GROUP = vim.api.nvim_create_augroup('VEYXOV_GROUP', { clear = true })
 vim.api.nvim_create_autocmd('BufWritePost', {
   command = 'source <afile> | PackerCompile',
-  group = packer_group,
+  group = GROUP,
   pattern = 'plugins.lua',
 })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     vim.highlight.on_yank()
   end,
-  group = highlight_group,
+  group = GROUP,
   pattern = '*',
 })
 
