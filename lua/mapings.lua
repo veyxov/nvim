275356f3a0f99754b2cdaddc4161da198d8cc12a
Shekhov Ismoil <veyxov@gmail.com>
2023-01-01 15:31:53 +0500

    make the leader yank work also in visual mode

--- lua/mapings.lua
+++ lua/mapings.lua
@@ -1,7 +1,7 @@
 vim.keymap.set('n', '<leader>Q', '<cmd>qall!<cr>') -- Force quit
 vim.keymap.set('n', '<cr>w', '<cmd>wall<cr>') -- Save file ***
 vim.keymap.set('n', '<cr>q', '<cmd>q<cr>') -- Close buffer **
-vim.keymap.set('n', '<leader>y', '"+y') -- Copy to system clipboar
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y') -- Copy to system clipboar
 vim.keymap.set('n', '<leader>v', '<C-V>') -- Copy to system clipboar
 
 vim.keymap.set('n', '<c-g>', ":silent !tmux neww 'lazygit; exit'<CR>") -- Open git manager **
