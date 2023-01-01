d320b76a65a7bf23c36650a1191da4859e94a07c
Shekhov Ismoil <veyxov@gmail.com>
2023-01-01 15:32:08 +0500

    use a newer version of the gruvbox theme

--- lua/plugz/gruvbox.lua
+++ lua/plugz/gruvbox.lua
@@ -1,10 +1,13 @@
 local M = {
-    "sainnhe/gruvbox-material",
+    "luisiacc/gruvbox-baby",
     event = "InsertEnter"
 }
 
-M.config = function() 
-    vim.cmd.colorscheme "gruvbox-material"
+M.config = function()
+    -- vim.g.gruvbox_baby_telescope_theme = 1
+    vim.g.gruvbox_baby_transparent_mode = 1
+
+    vim.cmd.colorscheme "gruvbox-baby"
 end
 
 return M
