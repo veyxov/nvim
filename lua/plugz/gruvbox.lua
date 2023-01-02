c3797b8672d704c5756afb80030a57851ad4ac77
Shekhov Ismoil <veyxov@gmail.com>
2023-01-02 13:31:25 +0500

    non transparent gruvbox

--- lua/plugz/gruvbox.lua
+++ lua/plugz/gruvbox.lua
@@ -4,8 +4,8 @@ local M = {
 }
 
 M.config = function()
-    -- vim.g.gruvbox_baby_telescope_theme = 1
-    vim.g.gruvbox_baby_transparent_mode = 1
+    vim.g.gruvbox_baby_telescope_theme = 1
+    -- vim.g.gruvbox_baby_transparent_mode = 1
 
     vim.cmd.colorscheme "gruvbox-baby"
 end
