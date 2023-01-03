5381d0cd108f72717a92deaf9687ea9c2c7669ce
Shekhov Ismoil <veyxov@gmail.com>
2023-01-03 12:11:07 +0500

    added more mappings

--- lua/plugz/lspsaga.lua
+++ lua/plugz/lspsaga.lua
@@ -8,14 +8,19 @@ M.init = function()
     vim.keymap.set('n', 'dn',        vim.diagnostic.goto_prev)
     vim.keymap.set('n', 'dN',        vim.diagnostic.goto_next)
 
+    -- Lspsaga
     vim.keymap.set("n", 'lr', '<cmd>Lspsaga rename<cr>')
     vim.keymap.set("n", '<cr>a', '<cmd>Lspsaga code_action<cr>')
+    vim.keymap.set("n", 'K', '<cmd>Lspsaga hover_doc<cr>')
 
+    -- Builtin lsp
     vim.keymap.set('n', 'ld', vim.lsp.buf.definition)
     vim.keymap.set('n', 'lD', vim.lsp.buf.declaration)
     vim.keymap.set('n', 'li', vim.lsp.buf.implementation)
+    vim.keymap.set('n', 'lf', vim.lsp.buf.references)
     vim.keymap.set('i', '<C-e>', vim.lsp.buf.signature_help)
 
+    -- Custom
     vim.keymap.set('n', '<cr>f', "<cmd>Format<cr>") -- Format code using LSP
 
 end
