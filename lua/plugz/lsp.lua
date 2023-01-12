b4fcb9286c54a856f85bc553bfedcb27293d0db5
Shekhov Ismoil <veyxov@gmail.com>
2023-01-12 20:13:48 +0500

    fix: moved keybinds to lsp configuration

--- lua/plugz/lsp.lua
+++ lua/plugz/lsp.lua
@@ -16,6 +16,27 @@ function M.config()
 
     -- Typescript
     require'lspconfig'.tsserver.setup{}
+
+
+    -- Diagnostic keymaps
+    vim.keymap.set('n', 'dn',        vim.diagnostic.goto_prev)
+    vim.keymap.set('n', 'dN',        vim.diagnostic.goto_next)
+
+    -- Lspsaga
+    vim.keymap.set("n", 'lr', '<cmd>Lspsaga rename<cr>')
+    vim.keymap.set("n", '<cr>a', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+    vim.keymap.set("n", 'K', '<cmd>Lspsaga hover_doc<cr>')
+
+    -- Builtin lsp
+    vim.keymap.set('n', '<leader>d', vim.lsp.buf.definition)
+    vim.keymap.set('n', '<cr>D', vim.lsp.buf.declaration)
+    vim.keymap.set('n', '<leader>i', vim.lsp.buf.implementation)
+    vim.keymap.set('n', 'lf', vim.lsp.buf.references)
+    vim.keymap.set('i', '<C-e>', vim.lsp.buf.signature_help)
+
+    -- Custom
+    vim.keymap.set('n', '<cr>f', "<cmd>Format<cr>") -- Format code using LSP
+
 end
 
 return M
