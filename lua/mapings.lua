be2bf24aeec2ddc01e6f473310cc0b7ba32bc719
Shekhov Ismoil <veyxov@gmail.com>
2023-01-12 22:26:59 +0500

    same keybind as on the terminal

--- lua/mapings.lua
+++ lua/mapings.lua
@@ -4,7 +4,7 @@ vim.keymap.set('n', '<cr>q', '<cmd>q<cr>') -- Close buffer **
 vim.keymap.set({'n', 'v'}, '<leader>y', '"+y') -- Copy to system clipboar
 vim.keymap.set('n', '<leader>v', '<C-V>') -- Copy to system clipboar
 
-vim.keymap.set('n', '<c-g>', ":silent !tmux neww 'lazygit; exit'<CR>") -- Open git manager **
+vim.keymap.set('n', '<c-s>', ":silent !tmux neww 'lazygit; exit'<CR>") -- Open git manager **
 
 -- New, experimental stuff
 vim.keymap.set("n", "<cr>cw", function() vim.fn.jobstart("setwall")end)
