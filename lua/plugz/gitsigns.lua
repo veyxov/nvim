873cf7b401914eb288756621e28607f2459f4e1a
Shekhov Ismoil <veyxov@gmail.com>
2023-01-12 20:28:11 +0500

    gisigns configuration

--- lua/plugz/gitsigns.lua
+++ lua/plugz/gitsigns.lua
@@ -25,9 +25,9 @@ M.config = function()
         current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
         current_line_blame_opts = {
             virt_text = true,
-            virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+            virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
             delay = 1000,
-            ignore_whitespace = false,
+            ignore_whitespace = true,
         },
         current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
         sign_priority = 6,
