5403673800ef956964f1bfd8d25418b9349ca33d
Shekhov Ismoil <veyxov@gmail.com>
2023-01-15 14:40:22 +0500

    fixed mappings

--- lua/plugz/nnn.lua
+++ lua/plugz/nnn.lua
@@ -24,15 +24,13 @@ M.config = function()
         },
         auto_close = true,
         mappings = {
-            mappings = {
-                { "<C-t>", builtin.open_in_tab }, -- open file(s) in tab
-                { "<C-s>", builtin.open_in_split }, -- open file(s) in split
-                { "<C-v>", builtin.open_in_vsplit }, -- open file(s) in vertical split
-                { "<C-p>", builtin.open_in_preview }, -- open file in preview split keeping nnn focused
-                { "<C-y>", builtin.copy_to_clipboard }, -- copy file(s) to clipboard
-                { "<C-w>", builtin.cd_to_path }, -- cd to file directory
-                { "<C-e>", builtin.populate_cmdline }, -- populate cmdline (:) with file(s)
-            }
+            { "ot", builtin.open_in_tab }, -- open file(s) in tab
+            { "os", builtin.open_in_split }, -- open file(s) in split
+            { "ov", builtin.open_in_vsplit }, -- open file(s) in vertical split
+            { "op", builtin.open_in_preview }, -- open file in preview split keeping nnn focused
+            { "oy", builtin.copy_to_clipboard }, -- copy file(s) to clipboard
+            { "oc", builtin.cd_to_path }, -- cd to file directory
+            { "oC", builtin.populate_cmdline }, -- populate cmdline (:) with file(s)
         }, -- table containing mappings, see below
     }
     nnn.setup(config)
