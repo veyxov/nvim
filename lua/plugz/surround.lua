95f18cd7fb7a3806e7b6b07b16a15729d2697c60
Shekhov Ismoil <veyxov@gmail.com>
2023-01-22 21:57:12 +0500

    nvim-surround: added the plugin

---
+++ lua/plugz/surround.lua
@@ -0,0 +1,18 @@
+local M = {
+    'kylechui/nvim-surround',
+    lazy = false
+}
+
+M.config = function(arst)
+    -- We aliased b->(, r->[
+    -- But ab ar motions does not exist in vim
+    vim.keymap.set("o", "ir", "i[")
+    vim.keymap.set("o", "ar", "a[")
+    vim.keymap.set("o", "ia", "i<")
+    vim.keymap.set("o", "aa", "a<")
+
+    require 'nvim-surround'.setup {
+    }
+end
+
+return M
