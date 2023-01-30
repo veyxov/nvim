929c241780d62b4f735979898dd40b700925f356
Shekhov Ismoil <veyxov@gmail.com>
2023-01-30 22:27:02 +0500

    twilight: configuration pasted from docs

--- lua/plugz/twilight.lua
+++ lua/plugz/twilight.lua
@@ -3,7 +3,17 @@ local M = {
 }
 
 M.config = function()
-    require("twilight").setup {}
+    require("twilight").setup {
+      context = 5, -- amount of lines we will try to show around the current line
+      treesitter = true, -- use treesitter when available for the filetype
+      expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
+        "function",
+        "method",
+        "table",
+        "if_statement",
+      },
+    }
+
 end
 
 return M
