86bcdca30651a797cc19ef1601897b54771825f0
Shekhov Ismoil <veyxov@gmail.com>
2023-01-31 10:41:19 +0500

    cleanup

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,7 +1,5 @@
--- Automatically source and re-compile packer whenever you save this init.lua
 local GROUP = vim.api.nvim_create_augroup('VEYXOV_GROUP', { clear = true })
 
--- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
