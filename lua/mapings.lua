0c07c92fb9f6f7c5b37a943658550a8c9f4c6674
Shekhov Ismoil <veyxov@gmail.com>
2023-01-31 10:42:49 +0500

    mappings: cleanup

--- lua/mapings.lua
+++ lua/mapings.lua
@@ -1,12 +1,9 @@
 vim.keymap.set('n', '<leader>Q', '<cmd>qall!<cr>') -- Force quit
-vim.keymap.set('n', '<cr>w', '<cmd>wall<cr>') -- Save file ***
-vim.keymap.set('n', '<cr>q', '<cmd>q<cr>') -- Close buffer **
+vim.keymap.set('n', '<cr>w', '<cmd>wall<cr>') -- Save file
+vim.keymap.set('n', '<cr>q', '<cmd>q<cr>') -- Close buffer
 vim.keymap.set({'n', 'v'}, '<leader>y', '"+y') -- Copy to system clipboar
-vim.keymap.set('n', '<leader>v', '<C-V>') -- Copy to system clipboar
+vim.keymap.set('n', '<leader>v', '<C-V>') -- Visual block mode
 
-vim.keymap.set('n', '<c-s>', ":silent !tmux neww 'lazygit; exit'<CR>") -- Open git manager **
-
-vim.keymap.set("n", "ls", "<cmd>Lazy sync<cr>")
 vim.keymap.set("n", "ll", "<cmd>Lazy<cr>")
 
 -- Insert a semicolon at the end of the line and create a new one
