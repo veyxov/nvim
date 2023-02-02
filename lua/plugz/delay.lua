404eeb1a8ed7429d5ee05f0ab1ea3c41956df722
Shekhov Ismoil <veyxov@gmail.com>
2023-02-02 13:09:52 +0500

    delay-train: installed

---
+++ lua/plugz/delay.lua
@@ -0,0 +1,14 @@
+local M = {
+    "ja-ford/delaytrain.nvim",
+    lazy = false
+}
+
+M.config = function()
+    require 'delaytrain'.setup {
+        keys = {
+            ['nvi'] = {'<Left>', '<Down>', '<Up>', '<Right>', 'w', 'b', '<c-d>'},
+        },
+    }
+end
+
+return M
