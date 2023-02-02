64224bfcc060a08527930ea1b02631016888a2c2
Shekhov Ismoil <veyxov@gmail.com>
2023-02-02 15:27:09 +0500

    harpoon: numbers for switching between frequent buffers

--- lua/plugz/harpoon.lua
+++ lua/plugz/harpoon.lua
@@ -1,16 +1,19 @@
 local M = {
     'ThePrimeagen/harpoon',
+    keys = {"ht", "ha", "1", "2", "3", "4"}
 }
-
-M.init = function()
-    vim.keymap.set('n', 'ha', function() require("harpoon.mark").add_file() end)
-    vim.keymap.set('n', 'ht', '<cmd>Telescope harpoon marks<cr>')
-end
 
 M.config = function()
     require 'harpoon'.setup {}
 
     require 'telescope'.load_extension('harpoon')
+
+    vim.keymap.set('n', 'ha', function() require("harpoon.mark").add_file() end)
+    vim.keymap.set('n', 'ht', '<cmd>Telescope harpoon marks<cr>')
+
+    for i = 1, 4, 1 do
+        vim.keymap.set('n', tostring(i), '<cmd>lua require("harpoon.ui").nav_file(' .. tostring(i) .. ')<cr>')
+    end  
 end
 
 return M
