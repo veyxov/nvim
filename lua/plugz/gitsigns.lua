f288d04b57c9bd95f01dbf13e1dcffe526f49f29
Shekhov Ismoil <veyxov@gmail.com>
2023-02-06 08:58:28 +0500

    formating

--- lua/plugz/gitsigns.lua
+++ lua/plugz/gitsigns.lua
@@ -5,7 +5,7 @@ local M = {
 
 M.config = function()
     require('gitsigns').setup {
-        signs = {
+        signs                        = {
             add          = { text = '│' },
             change       = { text = '│' },
             delete       = { text = '_' },
@@ -13,28 +13,28 @@ M.config = function()
             changedelete = { text = '~' },
             untracked    = { text = '┆' },
         },
-        signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-        numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-        linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-        word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
-        watch_gitdir = {
+        signcolumn                   = true, -- Toggle with `:Gitsigns toggle_signs`
+        numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
+        linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
+        word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
+        watch_gitdir                 = {
             interval = 1000,
             follow_files = true
         },
-        attach_to_untracked = true,
-        current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-        current_line_blame_opts = {
+        attach_to_untracked          = true,
+        current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame_opts      = {
             virt_text = true,
             virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
             delay = 1000,
             ignore_whitespace = true,
         },
         current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-        sign_priority = 6,
-        update_debounce = 100,
-        status_formatter = nil, -- Use default
-        max_file_length = 40000, -- Disable if file is longer than this (in lines)
-        preview_config = {
+        sign_priority                = 6,
+        update_debounce              = 100,
+        status_formatter             = nil, -- Use default
+        max_file_length              = 40000, -- Disable if file is longer than this (in lines)
+        preview_config               = {
             -- Options passed to nvim_open_win
             border = 'single',
             style = 'minimal',
@@ -42,10 +42,13 @@ M.config = function()
             row = 0,
             col = 1
         },
-        yadm = {
+        yadm                         = {
             enable = false
         },
     }
+
+    vim.keymap.set("n", "hs", "<cmd>Gitsigns stage_hunk<cr>");
+    vim.keymap.set("n", "hS", "<cmd>Gitsigns undo_stage_hunk<cr>");
 end
 
 return M
