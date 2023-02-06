41b27dfb092f7f718b0006daf1785ddbe336dd7f
Shekhov Ismoil <veyxov@gmail.com>
2023-02-06 14:39:10 +0500

    config: move lazy initialization and configuration to separate file

--- init.lua
+++ init.lua
@@ -1,9 +1,5 @@
--- Install lazy
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then vim.fn.system({ "git", "clone", "--filter=blob:none", "--single-branch", "https://github.com/folke/lazy.nvim.git", lazypath }) end
-vim.opt.runtimepath:prepend(lazypath)
-
 -- Neovim configuration
+require 'lazyopt'
 require 'options'
 require 'plugins'
 require 'autocmd'
