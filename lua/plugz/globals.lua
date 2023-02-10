ae79f33c8521de97ad56100d4f559ae0257f6daf
Shekhov Ismoil <veyxov@gmail.com>
2023-02-10 17:29:27 +0500

    fix: install nvim-web-devicons

--- lua/plugz/globals.lua
+++ lua/plugz/globals.lua
@@ -1,5 +1,6 @@
 return {
     { 'nvim-lua/plenary.nvim' },
+    { 'nvim-tree/nvim-web-devicons', config = function() require 'nvim-web-devicons'.setup {} end },
     { 'windwp/nvim-autopairs', config = function()
         require("nvim-autopairs").setup {}
     end }
