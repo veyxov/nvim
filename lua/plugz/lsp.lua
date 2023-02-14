bf7cbbb7e274189aa729708b4cc191fb225be05b
Shekhov Ismoil <veyxov@gmail.com>
2023-02-14 09:37:43 +0500

    lsp: fixed deprecated version

--- lua/plugz/lsp.lua
+++ lua/plugz/lsp.lua
@@ -23,7 +23,28 @@ function M.config()
     lsp.tsserver.setup {}
 
     -- Lua
-    lsp.sumneko_lua.setup {}
+    require 'lspconfig'.lua_ls.setup {
+        settings = {
+            Lua = {
+                runtime = {
+                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                    version = 'LuaJIT',
+                },
+                diagnostics = {
+                    -- Get the language server to recognize the `vim` global
+                    globals = { 'vim' },
+                },
+                workspace = {
+                    -- Make the server aware of Neovim runtime files
+                    library = vim.api.nvim_get_runtime_file("", true),
+                },
+                -- Do not send telemetry data containing a randomized but unique identifier
+                telemetry = {
+                    enable = false,
+                },
+            },
+        },
+    }
 
     -- Rust
     lsp.rust_analyzer.setup {}
