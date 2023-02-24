ee83a216dd3363605cb49924ec5d0c01b573082f
Shekhov Ismoil <veyxov@gmail.com>
2023-02-24 16:20:11 +0500

    obsidian: fixed setup

--- lua/plugz/obsidian.lua
+++ lua/plugz/obsidian.lua
@@ -3,4 +3,13 @@ local M = {
     lazy = false
 }
 
+M.config = function()
+    require("obsidian").setup({
+        dir = "~/Obsidian_Vaults/",
+        completion = {
+            nvim_cmp = true, -- if using nvim-cmp, otherwise set to false
+        }
+    })
+end
+
 return M
