1cf9c4216150349301f81d6ec457dd70f71ce8c7
Shekhov Ismoil <veyxov@gmail.com>
2023-02-24 16:35:01 +0500

    obsidian: added mapping

--- lua/mapings.lua
+++ lua/mapings.lua
@@ -1,10 +1,23 @@
 vim.keymap.set('n', '<leader>Q', '<cmd>qall!<cr>') -- Force quit
 vim.keymap.set('n', '<cr>w', '<cmd>wall<cr>') -- Save file
 vim.keymap.set('n', '<cr>q', '<cmd>q<cr>') -- Close buffer
-vim.keymap.set({'n', 'v'}, '<leader>y', '"+y') -- Copy to system clipboar
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y') -- Copy to system clipboar
 vim.keymap.set('n', '<leader>v', '<C-V>') -- Visual block mode
 
 vim.keymap.set("n", "ll", "<cmd>Lazy<cr>")
 
 -- Insert a semicolon at the end of the line and create a new one
 vim.cmd [[inoremap <C-O> <esc>A;<esc>o]]
+
+vim.keymap.set(
+    "n",
+    "gf",
+    function()
+        if require('obsidian').util.cursor_on_markdown_link() then
+            return "<cmd>ObsidianFollowLink<CR>"
+        else
+            return "gf"
+        end
+    end,
+    { noremap = false, expr = true }
+)
