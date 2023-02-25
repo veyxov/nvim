3d3925d1739251be8612ebf19a8731ad4b5b832a
Shekhov Ismoil <veyxov@gmail.com>
2023-02-25 15:05:35 +0500

    tree-sitter: enable bundled tree-sitter parser

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -2,17 +2,22 @@ local GROUP = vim.api.nvim_create_augroup('VEYXOV_GROUP', { clear = true })
 
 -- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = GROUP,
-  pattern = '*',
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    group = GROUP,
+    pattern = '*',
 })
 
 vim.api.nvim_create_autocmd('BufWritePre', {
-    callback = function ()
-        vim.lsp.buf.format ( { async = false } )
+    callback = function()
+        vim.lsp.buf.format({ async = false })
     end,
     group = GROUP,
     pattern = "*"
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 'lua', 'help' },
+    callback = function() vim.treesitter.start() end,
 })
