5a0f4bfb714c5f602045b6570442f1e05e358046
Shekhov Ismoil <veyxov@gmail.com>
2023-02-25 21:20:00 +0500

    cmp: get results from all buffers (open)

--- lua/plugz/cmp.lua
+++ lua/plugz/cmp.lua
@@ -43,7 +43,18 @@ function M.config()
         sources = cmp.config.sources({
             { name = "nvim_lsp" },
             { name = "luasnip" },
-            { name = "buffer" },
+            {
+                name = 'buffer',
+                option = {
+                    get_bufnrs = function()
+                        local bufs = {}
+                        for _, win in ipairs(vim.api.nvim_list_wins()) do
+                            bufs[vim.api.nvim_win_get_buf(win)] = true
+                        end
+                        return vim.tbl_keys(bufs)
+                    end
+                }
+            }
         }),
     })
 end
