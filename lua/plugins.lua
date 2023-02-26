901cbef6e0c99e734f6fa784e6d35efad1706829
Shekhov Ismoil <veyxov@gmail.com>
2023-02-26 15:07:27 +0500

    format

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,30 +1,16 @@
 require("lazy").setup("plugz", {
-    defaults = {
-        lazy = true, -- should plugins be lazy-loaded?
-    },
-    install = {
-        missing = true,
-    },
-    ui = {
-        -- a number <1 is a percentage., >1 is a fixed size
-        size = { width = 1, height = 1 },
-        throttle = 1,
-    },
-    checker = {
-        enabled = true,
-        notify = false, -- get a notification when new updates are found
-        frequency = 3600, -- check for updates every hour
-    },
+    defaults = { lazy = true },
+    install = { missing = true },
+    ui = { size = { width = 1, height = 1 } },
     change_detection = {
-        -- automatically check for config file changes and reload the ui
         enabled = true,
-        notify = false, -- get a notification when changes are found
+        notify = false,
     },
     performance = {
         rtp = {
             disabled_plugins = {
-                "gzip", "matchit", "matchparen", "netrwPlugin", "tarPlugin", "tohtml", "tutor", "zipPlugin",
-                "spellfile", "rplugin"
+                "gzip", "matchit", "matchparen", "netrwPlugin", "tarPlugin",
+                "tohtml", "tutor", "zipPlugin", "spellfile", "rplugin"
             },
         },
     },
