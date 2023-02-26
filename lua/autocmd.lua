5864e57544842c92aa6dace084c6e4bd53300432
Shekhov Ismoil <veyxov@gmail.com>
2023-02-26 22:20:46 +0500

    format

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,6 +1,5 @@
 local GROUP = vim.api.nvim_create_augroup('VEYXOV_GROUP', { clear = true })
 
--- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function()
         vim.highlight.on_yank()
