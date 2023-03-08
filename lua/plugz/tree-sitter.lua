b4055d5f946b67ced003c103e4a4a856208d7459
Shekhov Ismoil <veyxov@gmail.com>
2023-03-08 21:33:50 +0500

    tree-sitter: use [] for method navigation

--- lua/plugz/tree-sitter.lua
+++ lua/plugz/tree-sitter.lua
@@ -15,58 +15,49 @@ function M.config()
         textobjects = {
             select = {
                 enable = true,
-
                 -- Automatically jump forward to textobj, similar to targets.vim
                 lookahead = true,
-
                 keymaps = {
-                    ["am"] = "@function.outer",
-                    ["im"] = "@function.inner",
-
-                    ["aC"] = "@class.outer",
-                    ["iC"] = "@class.inner",
-
-                    ["aif"] = "@conditional.outer",
-                    ["iif"] = "@conditional.inner",
-
-                    ["ib"] = "@block.inner",
-                    ["ab"] = "@block.outer",
-
-                    ["il"] = "@loop.inner",
-                    ["al"] = "@loop.outer",
-
-                    ["ic"] = "@call.inner",
-                    ["ac"] = "@call.outer",
-
-                    ["aa"] = "@parameter.inner",
+                        ["am"] = "@function.outer",
+                        ["im"] = "@function.inner",
+                        ["aC"] = "@class.outer",
+                        ["iC"] = "@class.inner",
+                        ["aif"] = "@conditional.outer",
+                        ["iif"] = "@conditional.inner",
+                        ["ib"] = "@block.inner",
+                        ["ab"] = "@block.outer",
+                        ["il"] = "@loop.inner",
+                        ["al"] = "@loop.outer",
+                        ["ic"] = "@call.inner",
+                        ["ac"] = "@call.outer",
+                        ["aa"] = "@parameter.inner",
                 },
-
                 include_surrounding_whitespace = false,
             },
             move = {
                 enable = true,
                 set_jumps = true, -- whether to set jumps in the jumplist
                 goto_next_start = {
-                    ["gm"] = "@function.outer",
-                    ["gb"] = "@block.inner"
+                        ["["] = "@function.outer",
+                        ["gb"] = "@block.inner"
                 },
                 goto_next_end = {
-                    ["gM"] = "@function.outer",
+                        ["gM"] = "@function.outer",
                 },
                 goto_previous_start = {
-                    ["Gm"] = "@function.outer",
+                        ["]"] = "@function.outer",
                 },
                 goto_previous_end = {
-                    ["GM"] = "@function.outer",
+                        ["GM"] = "@function.outer",
                 },
             },
             swap = {
                 enable = true,
                 swap_next = {
-                    ["<leader>gm"] = "@function.outer",
+                        ["<leader>gm"] = "@function.outer",
                 },
                 swap_previous = {
-                    ["<leader>A"] = "@parameter.inner",
+                        ["<leader>A"] = "@parameter.inner",
                 },
             },
         },
