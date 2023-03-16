f9ef9415438e6828d50312237e19d294350d7226
Shekhov Ismoil <veyxov@gmail.com>
2023-03-16 22:29:20 +0500

    format

--- lua/plugz/telescope.lua
+++ lua/plugz/telescope.lua
@@ -2,9 +2,11 @@ local M = {
     "nvim-telescope/telescope.nvim",
     cmd = { "Telescope" },
     dependencies = {
-        { "nvim-telescope/telescope-fzf-native.nvim",
-                                                           build =
-            "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build" },
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build =
+            "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+        },
         { 'nvim-telescope/telescope-ui-select.nvim' },
         { 'nvim-telescope/telescope-file-browser.nvim' }
     },
