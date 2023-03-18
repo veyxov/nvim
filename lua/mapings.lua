451124b76f48fbe49d9af3b1036a104f91905f3e
Shekhov Ismoil <veyxov@gmail.com>
2023-03-18 21:14:57 +0500

    mappings: added better visual mode indenting

--- lua/mapings.lua
+++ lua/mapings.lua
@@ -1,12 +1,16 @@
 vim.keymap.set('n', '<leader>Q', '<cmd>qall!<cr>') -- Force quit
-vim.keymap.set('n', '<cr>q', '<cmd>q<cr>') -- Close buffer
+vim.keymap.set('n', '<cr>q', '<cmd>q<cr>')         -- Close buffer
 
-vim.keymap.set('n', '<cr>w', '<cmd>wall<cr>') -- Save file
+vim.keymap.set('n', '<cr>w', '<cmd>wall<cr>')      -- Save file
 
-vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y') -- Copy to system clipboar
-vim.keymap.set('n', '<leader>v', '<C-V>') -- Visual block mode
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y')   -- Copy to system clipboar
+vim.keymap.set('n', '<leader>v', '<C-V>')          -- Visual block mode
 
 vim.keymap.set("n", "ll", "<cmd>Lazy<cr>")
 
 -- Insert a semicolon at the end of the line and create a new one
 vim.cmd [[inoremap <C-O> <esc>A;<esc>o]]
+
+-- better indenting
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
