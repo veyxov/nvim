2cb46d37b1dfe556063c4984d807ad7becea68b6
Shekhov Ismoil <veyxov@gmail.com>
2023-03-20 21:30:03 +0500

    plugins: installed kmonad-vim highlighting

--- lua/plugz/globals.lua
+++ lua/plugz/globals.lua
@@ -1,5 +1,6 @@
 return {
     { 'nvim-lua/plenary.nvim' },
+    { 'https://github.com/kmonad/kmonad-vim', lazy = false },
     { 'nvim-tree/nvim-web-devicons', config = function() require 'nvim-web-devicons'.setup {} end },
     { 'windwp/nvim-autopairs', config = function()
         require("nvim-autopairs").setup {}
