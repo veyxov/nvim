8474e7a69161a30ede338af5334f3f4800b7b0f0
Shekhov Ismoil <veyxov@gmail.com>
2023-03-25 17:42:53 +0500

    globals: format

--- lua/plugz/globals.lua
+++ lua/plugz/globals.lua
@@ -1,8 +1,14 @@
 return {
     { 'nvim-lua/plenary.nvim' },
     { 'https://github.com/kmonad/kmonad-vim', lazy = false },
-    { 'nvim-tree/nvim-web-devicons', config = function() require 'nvim-web-devicons'.setup {} end },
-    { 'windwp/nvim-autopairs', config = function()
-        require("nvim-autopairs").setup {}
-    end }
+    {
+        'nvim-tree/nvim-web-devicons',
+        config = function() require 'nvim-web-devicons'.setup {} end
+    },
+    {
+        'windwp/nvim-autopairs',
+        config = function()
+            require("nvim-autopairs").setup {}
+        end
+    }
 }
