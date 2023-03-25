8cd0246e0768c74c19b3f1f135271b3994214b78
Shekhov Ismoil <veyxov@gmail.com>
2023-03-25 17:41:29 +0500

    telescope: format, refactor

--- lua/plugz/telescope.lua
+++ lua/plugz/telescope.lua
@@ -5,7 +5,8 @@ local M = {
         {
             "nvim-telescope/telescope-fzf-native.nvim",
             build =
-            "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+            [[cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build
+            build --config Release && cmake --install build --prefix build]]
         },
         { 'nvim-telescope/telescope-ui-select.nvim' },
         { 'nvim-telescope/telescope-file-browser.nvim' }
@@ -56,15 +57,37 @@ function M.config()
 end
 
 function M.init()
-    vim.keymap.set('n', '<cr>s', "<cmd>Telescope find_files<cr>")                                                       -- Fuzzy find files ****
-    vim.keymap.set('n', 'hh', "<cmd>Telescope help_tags<cr>")                                                           -- Seach in help
-    vim.keymap.set('n', '<cr>g', "<cmd>Telescope live_grep<cr>")                                                        -- Grep search *
-    vim.keymap.set('n', '<leader>/', "<cmd>Telescope current_buffer_fuzzy_find theme=get_dropdown previewer=false<cr>") -- Search in diagnostics
+    vim.keymap.set('n', '<cr>s', "<cmd>Telescope find_files<cr>") -- Fuzzy find files ****
+    vim.keymap.set('n', 'hh', "<cmd>Telescope help_tags<cr>")     -- Seach in help
+    vim.keymap.set('n', '<cr>g', "<cmd>Telescope live_grep<cr>")  -- Grep search *
+
+    vim.keymap.set('n', '<leader>/', [[
+        <cmd>
+            Telescope
+            current_buffer_fuzzy_find
+            theme=get_dropdown
+            previewer=false
+        <cr>
+    ]]) -- Search in diagnostics
+
     vim.keymap.set('n', '<cr>r', "<cmd>Telescope lsp_references<cr>")
     vim.keymap.set('n', '<cr>o', "<cmd>Telescope lsp_workspace_symbols<cr>")
+
     vim.keymap.set("n", "<cr>n",
-        function() require 'telescope'.extensions.file_browser.file_browser({ depth = 5, files = false }) end)
-    vim.keymap.set("n", "<cr>N", function() require 'telescope'.extensions.file_browser.file_browser({ depth = 3 }) end)
+        function() 
+            require 'telescope'
+            .extensions
+            .file_browser
+            .file_browser({ depth = 5, files = false })
+        end)
+
+    vim.keymap.set("n", "<cr>N",
+        function() 
+            require 'telescope'
+            .extensions
+            .file_browser
+            .file_browser({ depth = 3 })
+        end)
 end
 
 return M
