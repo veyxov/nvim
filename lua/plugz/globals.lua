a57e0d322b8fa645c3f21391758f01110f109222
Shekhov Ismoil <veyxov@gmail.com>
2023-03-30 13:25:08 +0500

    autopairs: move from `globals` to separate file

--- lua/plugz/globals.lua
+++ lua/plugz/globals.lua
@@ -5,19 +5,4 @@ return {
         'nvim-tree/nvim-web-devicons',
         config = function() require 'nvim-web-devicons'.setup {} end
     },
-    {
-        'windwp/nvim-autopairs',
-        config = function()
-            local lbls = 'nwyeisart'
-            require("nvim-autopairs").setup {
-                fast_wrap = {
-                    map = '<C-w>', -- mnemonic: control wrap
-                    keys = lbls,
-                    end_key = 'o'
-                }
-            }
-            -- Handy on class declaration
-            vim.keymap.set('i', '{{', '<space>{<cr>}<esc>O')
-        end
-    }
 }
