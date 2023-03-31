cb13e5d6a26d4232073c41091275d1b186302d11
Shekhov Ismoil <veyxov@gmail.com>
2023-03-31 10:17:31 +0500

    tree: custom window mappings

--- lua/plugz/tree.lua
+++ lua/plugz/tree.lua
@@ -9,52 +9,74 @@ local M = {
 
 M.init = function()
     vim.g.neo_tree_remove_legacy_commands = 1
+
+    vim.keymap.set('n', '<C-n>', '<cmd>Neotree<cr>')
 end
 
 M.config = function()
-    local function getTelescopeOpts(state, path)
-        return {
-            cwd = path,
-            search_dirs = { path },
-            attach_mappings = function (prompt_bufnr, map)
-                local actions = require "telescope.actions"
-                actions.select_default:replace(function()
-                    actions.close(prompt_bufnr)
-                    local action_state = require "telescope.actions.state"
-                    local selection = action_state.get_selected_entry()
-                    local filename = selection.filename
-                    if (filename == nil) then
-                        filename = selection[1]
-                    end
-                    -- any way to open the file without triggering auto-close event of neo-tree?
-                    require("neo-tree.sources.filesystem").navigate(state, state.path, filename)
-                end)
-                return true
-            end
-        }
-    end
-    require("neo-tree").setup({
-        filesystem = {
-            window = {
-                mappings = {
-                    ["tf"] = "telescope_find",
-                    ["tg"] = "telescope_grep",
+    require 'neo-tree'.setup {
+        close_if_last_window = true,
+        enable_git_status = false,
+        enable_diagnostic = false,
+        window = {
+            position = "left",
+            width = 40,
+            mapping_options = {
+                noremap = true,
+                nowait = true,
+            },
+            mappings = {
+                ["<space>"] = {
+                    "toggle_node",
+                    nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
                 },
-            },
-            commands = {
-                telescope_find = function(state)
-                    local node = state.tree:get_node()
-                    local path = node:get_id()
-                    require('telescope.builtin').find_files(getTelescopeOpts(state, path))
-                end,
-                telescope_grep = function(state)
-                    local node = state.tree:get_node()
-                    local path = node:get_id()
-                    require('telescope.builtin').live_grep(getTelescopeOpts(state, path))
-                end,
-            },
+                ["<cr>"] = "open",
+                ["<esc>"] = "revert_preview",
+                ["P"] = { "toggle_preview", config = { use_float = true } },
+                ["l"] = "focus_preview",
+                ["S"] = "open_split",
+                ["s"] = "open_vsplit",
+                -- ["S"] = "split_with_window_picker",
+                -- ["s"] = "vsplit_with_window_picker",
+                ["t"] = "open_tabnew",
+                -- ["<cr>"] = "open_drop",
+                -- ["t"] = "open_tab_drop",
+                ["w"] = "open_with_window_picker",
+                --["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
+                ["C"] = "close_node",
+                -- ['C'] = 'close_all_subnodes',
+                ["z"] = "close_all_nodes",
+                --["Z"] = "expand_all_nodes",
+                ["a"] = {
+                    "add",
+                    -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+                    -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+                    config = {
+                        show_path = "none" -- "none", "relative", "absolute"
+                    }
+                },
+                ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+                ["d"] = "delete",
+                ["r"] = "rename",
+                ["y"] = "copy_to_clipboard",
+                ["x"] = "cut_to_clipboard",
+                ["p"] = "paste_from_clipboard",
+                ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+                -- ["c"] = {
+                --  "copy",
+                --  config = {
+                --    show_path = "none" -- "none", "relative", "absolute"
+                --  }
+                --}
+                ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+                ["q"] = "close_window",
+                ["R"] = "refresh",
+                ["?"] = "show_help",
+                ["<"] = "prev_source",
+                [">"] = "next_source",
+            }
         },
-    })
+    }
 end
 
 return M
