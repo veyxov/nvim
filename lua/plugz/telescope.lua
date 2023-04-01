d6101bd70a9a6d41faf48fe9720e1e20fb6e43b4
Shekhov Ismoil <veyxov@gmail.com>
2023-04-01 17:54:20 +0500

    telescope: remove file-browser

--- lua/plugz/telescope.lua
+++ lua/plugz/telescope.lua
@@ -8,7 +8,6 @@ local M = {
             [[cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build
             build --config Release && cmake --install build --prefix build]]
         },
-        { 'nvim-telescope/telescope-file-browser.nvim' }
     },
 }
 
@@ -57,7 +56,6 @@ function M.config()
     })
 
     telescope.load_extension("fzf")
-    telescope.load_extension("file_browser")
 end
 
 function M.init()
@@ -76,22 +74,6 @@ function M.init()
 
     vim.keymap.set('n', '<cr>r', "<cmd>Telescope lsp_references<cr>")
     vim.keymap.set('n', '<cr>o', "<cmd>Telescope lsp_workspace_symbols<cr>")
-
-    vim.keymap.set("n", "<cr>n",
-        function()
-            require 'telescope'
-                .extensions
-                .file_browser
-                .file_browser({ depth = 5, files = false })
-        end)
-
-    vim.keymap.set("n", "<cr>N",
-        function()
-            require 'telescope'
-                .extensions
-                .file_browser
-                .file_browser({ depth = 3 })
-        end)
 end
 
 return M
