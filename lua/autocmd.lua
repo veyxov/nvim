cbb6e5186418cff6e324fdb1f6c66e14525619b4
Shekhov Ismoil <veyxov@gmail.com>
2023-04-01 17:00:35 +0500

    autocmd: got to last loc when opening a new buffer

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -16,6 +16,19 @@ vim.api.nvim_create_autocmd({ "VimResized" }, {
         vim.cmd("tabdo wincmd =")
     end,
 })
+
+-- Go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = GROUP,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 -- Format before writing
 vim.api.nvim_create_autocmd('BufWritePre', {
     callback = function()
