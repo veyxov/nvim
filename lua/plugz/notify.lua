39efecfdf725fdd99e1e0f4c8c18862248f3fa5e
Shekhov Ismoil <veyxov@gmail.com>
2023-04-01 17:24:00 +0500

    nvim-notify: installed

---
+++ lua/plugz/notify.lua
@@ -0,0 +1,26 @@
+local M = {
+    "rcarriga/nvim-notify",
+    event = "VeryLazy",
+    keys = {
+        {
+            "<leader>un",
+            function()
+                require("notify").dismiss({ silent = true, pending = true })
+            end,
+            desc = "Delete all Notifications",
+        },
+    },
+    opts = {
+        timeout = 3000,
+        max_height = function()
+            return math.floor(vim.o.lines * 0.75)
+        end,
+        max_width = function()
+            return math.floor(vim.o.columns * 0.75)
+        end,
+    },
+    init = function()
+        vim.notify = require("notify")
+    end
+}
+return M
