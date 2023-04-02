7cb509c05cbb40a16d26ae188b905320a0856c6f
Shekhov Ismoil <veyxov@gmail.com>
2023-04-02 18:24:36 +0500

    harpoon: don't use the telescope selector

--- lua/plugz/harpoon.lua
+++ lua/plugz/harpoon.lua
@@ -6,10 +6,8 @@ local M = {
 M.config = function()
     require 'harpoon'.setup {}
 
-    require 'telescope'.load_extension('harpoon')
-
-    vim.keymap.set('n', 'ha', function() require'harpoon.mark'.add_file() end)
-    vim.keymap.set('n', 'ht', '<cmd>Telescope harpoon marks<cr>')
+    vim.keymap.set('n', 'ha', function() require 'harpoon.mark'.add_file() end)
+    vim.keymap.set('n', 'ht', function() require 'harpoon.ui'.toggle_quick_menu() end)
 
     for i = 1, 4, 1 do
         local i_str = tostring(i);
