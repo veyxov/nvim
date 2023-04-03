c7889d4dd2e0a7ad291db7c7c87801c70b7f8c21
Shekhov Ismoil <veyxov@gmail.com>
2023-04-03 20:34:49 +0500

    mappings: added brace helper in insert mode

--- lua/mapings.lua
+++ lua/mapings.lua
@@ -2,6 +2,7 @@ vim.keymap.set('n', '<leader>Q', '<cmd>qall!<cr>') -- Force quit
 vim.keymap.set('n', '<cr>q', '<cmd>q<cr>')         -- Close buffer
 
 vim.keymap.set('i', '<C-a>', '<esc>ggVG')          -- Select all
+vim.keymap.set('i', '{{', '<space>{<cr>}<esc>O')   -- Select all
 
 vim.keymap.set('n', '<cr>w', '<cmd>wall<cr>')      -- Save file
 
