f82f1a396d51ac629568962b8b4beeddd50fa038
Shekhov Ismoil <veyxov@gmail.com>
2023-04-08 22:44:06 +0500

    options: refactor

--- lua/options.lua
+++ lua/options.lua
@@ -1,52 +1,54 @@
+local opt = vim.opt
+
 -- Spaceship
 vim.g.mapleader = ' '
 
 -- Nice and juicy terminal colors.
-vim.o.termguicolors = true
+opt.termguicolors = true
 
 -- What is a mouse? Don't know, don't care.
-vim.o.mouse = ''
+opt.mouse = ''
 
 -- Allow empty-space selection in v-block mode
-vim.o.virtualedit = 'block'
+opt.virtualedit = 'block'
 
 -- Train your fingers to be faster, stronger, better...
-vim.opt.timeoutlen = 150
+opt.timeoutlen = 150
 
 -- Visual clutter
-vim.o.wrap = false
-vim.opt.list = true
-vim.o.hlsearch = true
-vim.wo.signcolumn = 'number'
+opt.wrap = false
+opt.list = true
+opt.hlsearch = true
+opt.signcolumn = 'number'
 
 -- Too risky? Nah, it'll be fine :)
-vim.o.swapfile = false
-vim.o.undofile = true
+opt.swapfile = false
+opt.undofile = true
 
 -- Minimal visual clutter.
-vim.o.ruler = false
-vim.o.showmode = false
-vim.o.cmdheight = 0
-vim.o.laststatus = 0
+opt.ruler = false
+opt.showmode = false
+opt.cmdheight = 0
+opt.laststatus = 0
 
-vim.opt.formatoptions = "jcroqlnt"
-vim.o.shortmess = 'aItFOW'
+opt.formatoptions = "jcroqlnt"
+opt.shortmess = 'aItFOW'
 
 -- Work smarter not harder.
-vim.o.ignorecase = true
-vim.o.smartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- Fast completion and diagnostic.
-vim.o.updatetime = 250
-vim.opt.wildmode = 'longest:full,full'
-vim.o.completeopt = 'menuone,noselect'
+opt.updatetime = 250
+opt.wildmode = 'longest:full,full'
+opt.completeopt = 'menuone,noselect'
 
 -- Tabs or spaces?
 local tabs = 4
-vim.o.expandtab = true
-vim.o.tabstop = tabs;
-vim.o.shiftwidth = tabs;
-vim.o.softtabstop = tabs;
+opt.expandtab = true
+opt.tabstop = tabs;
+opt.shiftwidth = tabs;
+opt.softtabstop = tabs;
 
 -- Beautiful circle for buggy situations.
 vim.diagnostic.config({ virtual_text = { prefix = "●" }, })
