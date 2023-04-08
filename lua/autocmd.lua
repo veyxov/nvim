8f010f61603066dce1bfaff1508c7f38ad6bb71a
Shekhov Ismoil <veyxov@gmail.com>
2023-04-08 22:50:07 +0500

    autocmd: refactor

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,39 +1,40 @@
 local GROUP = vim.api.nvim_create_augroup('VEYXOV_GROUP', { clear = true })
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Highlight on yank
-vim.api.nvim_create_autocmd('TextYankPost', {
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = GROUP,
-    pattern = '*',
+autocmd('TextYankPost', {
+        callback = function()
+                vim.highlight.on_yank()
+        end,
+        group = GROUP,
+        pattern = '*',
 })
 
 -- Resize splits if window got resized
-vim.api.nvim_create_autocmd({ "VimResized" }, {
-    group = GROUP,
-    callback = function()
-        vim.cmd("tabdo wincmd =")
-    end,
+autocmd({ "VimResized" }, {
+        group = GROUP,
+        callback = function()
+                vim.cmd("tabdo wincmd =")
+        end,
 })
 
 -- Go to last loc when opening a buffer
-vim.api.nvim_create_autocmd("BufReadPost", {
-  group = GROUP,
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
+autocmd("BufReadPost", {
+        group = GROUP,
+        callback = function()
+                local mark = vim.api.nvim_buf_get_mark(0, '"')
+                local lcount = vim.api.nvim_buf_line_count(0)
+                if mark[1] > 0 and mark[1] <= lcount then
+                        pcall(vim.api.nvim_win_set_cursor, 0, mark)
+                end
+        end,
 })
 
 -- Format before writing
-vim.api.nvim_create_autocmd('BufWritePre', {
-    callback = function()
-        vim.lsp.buf.format({ async = false })
-    end,
-    group = GROUP,
-    pattern = "*"
+autocmd('BufWritePre', {
+        callback = function()
+                vim.lsp.buf.format({ async = false })
+        end,
+        group = GROUP,
+        pattern = "*"
 })
