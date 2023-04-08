c4740675e0640ab2a97aa86d8d446340303a7d23
Shekhov Ismoil <veyxov@gmail.com>
2023-04-08 18:55:53 +0500

    mappings: added another auto-pair mapping

--- lua/mapings.lua
+++ lua/mapings.lua
@@ -2,12 +2,14 @@ vim.keymap.set('n', '<leader>Q', '<cmd>qall!<cr>') -- Force quit
 vim.keymap.set('n', '<cr>q', '<cmd>q<cr>')         -- Close buffer
 
 vim.keymap.set('i', '<C-a>', '<esc>ggVG')          -- Select all
-vim.keymap.set('i', '{{', '<space>{<cr>}<esc>O')   -- Select all
 
-vim.keymap.set('n', '<cr>w', '<cmd>wall<cr>')      -- Save file
+vim.keymap.set('i', '{{', '<space>{<cr>}<esc>O')
+vim.keymap.set('i', '((', '<space>()<space>{<cr>}<esc>O')
 
-vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y')   -- Copy to system clipboar
-vim.keymap.set('n', '<leader>v', '<C-V>')          -- Visual block mode
+vim.keymap.set('n', '<cr>w', '<cmd>wall<cr>')    -- Save file
+
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y') -- Copy to system clipboar
+vim.keymap.set('n', '<leader>v', '<C-V>')        -- Visual block mode
 
 vim.keymap.set("n", "ll", "<cmd>Lazy<cr>")
 
