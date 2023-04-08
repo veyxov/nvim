6fbcc8912a071e4c38dff83bd740e275e6167a2e
Shekhov Ismoil <veyxov@gmail.com>
2023-04-08 20:57:04 +0500

    mappings: added window navigation mappings

--- lua/mapings.lua
+++ lua/mapings.lua
@@ -6,6 +6,12 @@ vim.keymap.set('i', '<C-a>', '<esc>ggVG')          -- Select all
 vim.keymap.set('i', '{{', '<space>{<cr>}<esc>O')
 vim.keymap.set('i', '((', '<space>()<space>{<cr>}<esc>O')
 
+-- Window navigation
+vim.keymap.set('n', '<C-Down>', '<C-w><Down>')
+vim.keymap.set('n', '<C-Left>', '<C-w><Left>')
+vim.keymap.set('n', '<C-Right>', '<C-w><Right>')
+vim.keymap.set('n', '<C-Up>', '<C-w><Up>')
+
 vim.keymap.set('n', '<cr>w', '<cmd>wall<cr>')    -- Save file
 
 vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y') -- Copy to system clipboar
