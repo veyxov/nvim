0f415625603356090a22c4754da25c7bce031d14
Shekhov Ismoil <veyxov@gmail.com>
2023-04-10 09:34:08 +0500

    leap: add safe labels, caused big problems

--- lua/plugz/leap.lua
+++ lua/plugz/leap.lua
@@ -5,12 +5,13 @@ local M = {
 
 function M.config()
     local lbls = { 'n', 'e', 'i', 'o', 's', 'a', 'r', 't' }
+    local safe_lbls = { 'n', 's', 'r', 't' }
 
     local leap = require 'leap'
 
     leap.add_default_mappings()
-    leap.opts.safe_labels = lbls
     leap.opts.labels = lbls
+    leap.opts.safe_labels = safe_lbls
 end
 
 return M
