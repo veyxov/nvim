7d5bc15478ccfbddf997948c5a07e9dd36d42df8
Shekhov Ismoil <veyxov@gmail.com>
2023-04-11 21:57:47 +0500

    spider: refactor

--- lua/plugz/spider.lua
+++ lua/plugz/spider.lua
@@ -4,14 +4,13 @@ end
 
 local mode = { 'n', 'o', 'x' }
 
-local M = {
-    "chrisgrieser/nvim-spider",
-    keys = {
-        { "w", function() motion 'w' end,  mode = mode },
-        { "e", function() motion 'e' end,  mode = mode },
-        { "b", function() motion 'b' end,  mode = mode },
-        { "ge",function() motion 'ge' end, mode = mode },
-    }
+local M = { "chrisgrieser/nvim-spider" }
+
+M.keys = {
+    { "w", function() motion 'w' end,  mode = mode },
+    { "e", function() motion 'e' end,  mode = mode },
+    { "b", function() motion 'b' end,  mode = mode },
+    { "ge",function() motion 'ge' end, mode = mode },
 }
 
 M.init = function()
