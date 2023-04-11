e777a628b98bae2078a693346beaec88b15757f8
Shekhov Ismoil <veyxov@gmail.com>
2023-04-11 22:06:11 +0500

    surround: use mapping helper

--- lua/plugz/surround.lua
+++ lua/plugz/surround.lua
@@ -1,15 +1,17 @@
 local M = {
     'kylechui/nvim-surround',
-    keys = { "ys", "cs", "ds", {"S", mode="v"} }
+    keys = { "ys", "cs", "ds", { "S", mode = "v" } }
 }
 
 M.config = function()
     -- We aliased b->(, r->[
     -- But ab ar motions does not exist in vim
-    vim.keymap.set("o", "ir", "i[")
-    vim.keymap.set("o", "ar", "a[")
-    vim.keymap.set("o", "ia", "i<")
-    vim.keymap.set("o", "aa", "a<")
+    local map = require 'globals'.Map
+
+    map('ir', 'i[', 'o')
+    map('ar', 'a[', 'o')
+    map('ia', 'i<', 'o')
+    map('aa', 'a<', 'o')
 
     require 'nvim-surround'.setup {
         move_cursor = false
