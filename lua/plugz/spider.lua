9ccc277a1b0769076e6f7268641d1abdf5b6b33f
Shekhov Ismoil <veyxov@gmail.com>
2023-04-11 22:06:06 +0500

    spider: use mapping helper

--- lua/plugz/spider.lua
+++ lua/plugz/spider.lua
@@ -7,16 +7,18 @@ local mode = { 'n', 'o', 'x' }
 local M = { "chrisgrieser/nvim-spider" }
 
 M.keys = {
-    { "w", function() motion 'w' end,  mode = mode },
-    { "e", function() motion 'e' end,  mode = mode },
-    { "b", function() motion 'b' end,  mode = mode },
-    { "ge",function() motion 'ge' end, mode = mode },
+    { "w",  function() motion 'w' end,  mode = mode },
+    { "e",  function() motion 'e' end,  mode = mode },
+    { "b",  function() motion 'b' end,  mode = mode },
+    { "ge", function() motion 'ge' end, mode = mode },
 }
 
 M.init = function()
-    vim.keymap.set('n', 'W', 'w')
-    vim.keymap.set('n', 'E', 'e')
-    vim.keymap.set('n', 'b', 'B')
+    local map = require 'globals'.Map
+
+    map('W', 'w')
+    map('E', 'e')
+    map('b', 'B')
 end
 
 return M;
