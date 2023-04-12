a7f20e46e483d41e5ad64430e26238761cd64c5a
Shekhov Ismoil <veyxov@gmail.com>
2023-04-12 10:28:48 +0500

    autocmd: refactor

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -2,32 +2,29 @@ local GROUP = vim.api.nvim_create_augroup('VEYXOV_GROUP', { clear = true })
 
 -- Helper function
 local function autocmd(event, callback)
-        vim.api.nvim_create_autocmd(event, {
-                callback = callback,
-                group = GROUP
-        })
+    vim.api.nvim_create_autocmd(event, { callback = callback, group = GROUP })
 end
 
 -- Highlight on yank
 autocmd('TextYankPost', function()
-        vim.highlight.on_yank()
+    vim.highlight.on_yank()
 end)
 
 -- Resize splits if window got resized
 autocmd('VimResized', function()
-        vim.cmd 'tabdo wincmd ='
+    vim.cmd 'tabdo wincmd ='
 end)
 
 -- Go to last loc when opening a buffer
 autocmd('BufReadPost', function()
-        local mark = vim.api.nvim_buf_get_mark(0, '"')
-        local lcount = vim.api.nvim_buf_line_count(0)
-        if mark[1] > 0 and mark[1] <= lcount then
-                pcall(vim.api.nvim_win_set_cursor, 0, mark)
-        end
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+        pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
 end)
 
 -- Format before writing
 autocmd('BufWritePre', function()
-        vim.lsp.buf.format({ async = false })
+    vim.lsp.buf.format({ async = false })
 end)
