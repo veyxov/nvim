377c9a72e23cde5e46d9938ff629cdedab4197ac
Shekhov Ismoil <veyxov@gmail.com>
2023-04-12 08:48:25 +0500

    diffview: refactor

--- lua/plugz/diffview.lua
+++ lua/plugz/diffview.lua
@@ -1,10 +1,11 @@
 local M = {
     'sindrets/diffview.nvim',
-    cmd = { 'Diffview', 'DiffviewOpen', 'DiffviewFileHistory' }
 }
 
+M.cmd = { 'Diffview', 'DiffviewOpen', 'DiffviewFileHistory' }
+
 M.config = function()
-    require("diffview").setup()     -- TODO: Tinker
+    require("diffview").setup() -- TODO: Tinker
 end
 
 return M
