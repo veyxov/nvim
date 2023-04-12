cdce1c908181c7f5276254cb1c9daad3d7a04ae7
Shekhov Ismoil <veyxov@gmail.com>
2023-04-12 10:51:53 +0500

    center: install zen mode

--- lua/plugz/center.lua
+++ lua/plugz/center.lua
@@ -1,11 +1,27 @@
-local M = {
-    'arnamak/stay-centered.nvim'
+local stay = {
+    'arnamak/stay-centered.nvim',
+    event = 'VeryLazy',
+    config = function()
+        require 'stay-centered'
+    end
 }
 
-M.event = 'VeryLazy'
+local zen = {
+    'folke/zen-mode.nvim',
+    event = 'InsertEnter',
+    config = function()
+        require 'zen-mode'.setup()
+        require("zen-mode").toggle({
+            window = {
+                width = .85 -- width will be 85% of the editor width
+            }
+        })
+    end
+}
 
-M.config = function()
-    require 'stay-centered'
-end
+local M = {
+    stay,
+    zen
+}
 
 return M
