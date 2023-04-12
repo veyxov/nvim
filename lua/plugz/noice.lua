31f2c0c0c8bab4159f5cd1f7309db510741d3875
Shekhov Ismoil <veyxov@gmail.com>
2023-04-12 10:04:01 +0500

    noice: refactor

--- lua/plugz/noice.lua
+++ lua/plugz/noice.lua
@@ -37,52 +37,53 @@ M.keys = {
             mode = {
                 "i", "n", "s" }
             },
+        }
+
+M.config = function()
+    local opts = {
+        lsp = {
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                ["cmp.entry.get_documentation"] = true,
+            },
         },
+        presets = {
+            bottom_search = true,
+            command_palette = true,
+            long_message_to_split = true,
+        },
+        background_color = '#FF0000',
+        health = {
+            checker = false, -- Disable if you don't want health checks to run
+        },
+        views = {
+            cmdline_popup = {
+                relative = "editor",
+                border = {
+                    style = "none",
+                    padding = { 1, 1 },
+                },
+                filter_options = {},
+                win_options = {
+                    winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+                },
+            },
+            popupmenu = {
+                relative = "editor",
+                position = {
+                    row = 6,
+                    col = "50%",
+                },
+                border = {
+                    style = "none",
+                    padding = { 0, 1 },
+                },
+            },
+        },
+    }
 
-        M.config = function()
-            local opts = {
-                lsp = {
-                    override = {
-                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                        ["vim.lsp.util.stylize_markdown"] = true,
-                        ["cmp.entry.get_documentation"] = true,
-                    },
-                },
-                presets = {
-                    bottom_search = true,
-                    command_palette = true,
-                    long_message_to_split = true,
-                },
-                background_color = '#FF0000',
-                health = {
-                    checker = false, -- Disable if you don't want health checks to run
-                },
-                views = {
-                    cmdline_popup = {
-                        relative = "editor",
-                        border = {
-                            style = "none",
-                            padding = { 1, 1 },
-                        },
-                        filter_options = {},
-                        win_options = {
-                            winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
-                        },
-                    },
-                    popupmenu = {
-                        relative = "editor",
-                        position = {
-                            row = 6,
-                            col = "50%",
-                        },
-                        border = {
-                            style = "none",
-                            padding = { 0, 1 },
-                        },
-                    },
-                },
-            }
+    require 'noice'.setup(opts)
+end
 
-            require 'noice'.setup(opts)
-        end
-        return M
+return M
