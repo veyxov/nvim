1ba4c220d6bbae3aaa087dee4d8a7bcea0e6a26f
Shekhov Ismoil <veyxov@gmail.com>
2023-04-13 13:22:49 +0500

    tree-sitter: refactor

--- lua/plugz/tree-sitter.lua
+++ lua/plugz/tree-sitter.lua
@@ -1,72 +1,74 @@
 local M = {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = "InsertEnter",
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    event = 'InsertEnter',
 }
 
 M.dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
+    'nvim-treesitter/nvim-treesitter-textobjects',
+}
+
+local opts = {
+    ensure_installed = { 'lua', 'vim', 'json', 'c_sharp' },
+    -- highlight
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = { 'markdown' }
+    },
+    indent = { enable = false },
+    textobjects = {
+        select = {
+            enable = true,
+            -- Automatically jump forward to textobj
+            lookahead = true,
+            keymaps = {
+                ['am'] = '@function.outer',
+                ['im'] = '@function.inner',
+                ['aC'] = '@class.outer',
+                ['iC'] = '@class.inner',
+                ['aif'] = '@conditional.outer',
+                ['iif'] = '@conditional.inner',
+                ['ib'] = '@block.inner',
+                ['ab'] = '@block.outer',
+                ['il'] = '@loop.inner',
+                ['al'] = '@loop.outer',
+                ['ic'] = '@call.inner',
+                ['ac'] = '@call.outer',
+                ['aa'] = '@parameter.inner',
+            },
+            include_surrounding_whitespace = false,
+        },
+        move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+                ['['] = '@function.outer',
+                ['gb'] = '@block.inner'
+            },
+            goto_next_end = {
+                ['gM'] = '@function.outer',
+            },
+            goto_previous_start = {
+                [']'] = '@function.outer',
+            },
+            goto_previous_end = {
+                ['GM'] = '@function.outer',
+            },
+        },
+        swap = {
+            enable = true,
+            swap_next = {
+                ['<leader>gm'] = '@function.outer',
+            },
+            swap_previous = {
+                ['<leader>A'] = '@parameter.inner',
+            },
+        },
+    },
 }
 
 function M.config()
-    require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "vim", "json", "c_sharp" },
-        -- highlight
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = { "markdown" }
-        },
-        indent = { enable = false },
-        textobjects = {
-            select = {
-                enable = true,
-                -- Automatically jump forward to textobj
-                lookahead = true,
-                keymaps = {
-                    ["am"] = "@function.outer",
-                    ["im"] = "@function.inner",
-                    ["aC"] = "@class.outer",
-                    ["iC"] = "@class.inner",
-                    ["aif"] = "@conditional.outer",
-                    ["iif"] = "@conditional.inner",
-                    ["ib"] = "@block.inner",
-                    ["ab"] = "@block.outer",
-                    ["il"] = "@loop.inner",
-                    ["al"] = "@loop.outer",
-                    ["ic"] = "@call.inner",
-                    ["ac"] = "@call.outer",
-                    ["aa"] = "@parameter.inner",
-                },
-                include_surrounding_whitespace = false,
-            },
-            move = {
-                enable = true,
-                set_jumps = true, -- whether to set jumps in the jumplist
-                goto_next_start = {
-                    ["["] = "@function.outer",
-                    ["gb"] = "@block.inner"
-                },
-                goto_next_end = {
-                    ["gM"] = "@function.outer",
-                },
-                goto_previous_start = {
-                    ["]"] = "@function.outer",
-                },
-                goto_previous_end = {
-                    ["GM"] = "@function.outer",
-                },
-            },
-            swap = {
-                enable = true,
-                swap_next = {
-                    ["<leader>gm"] = "@function.outer",
-                },
-                swap_previous = {
-                    ["<leader>A"] = "@parameter.inner",
-                },
-            },
-        },
-    })
+    require 'nvim-treesitter.configs'.setup(opts)
 end
 
 return M
