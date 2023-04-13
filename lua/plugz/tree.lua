8df11554c54e1bd5dc6f18fda006d9e2dbd56e6d
Shekhov Ismoil <veyxov@gmail.com>
2023-04-13 10:31:25 +0500

    tree: fix

--- lua/plugz/tree.lua
+++ lua/plugz/tree.lua
@@ -1,18 +1,10 @@
 return {
     "lmburns/lf.nvim",
+    lazy = false,
     config = function()
-        -- This feature will not work if the plugin is lazy-loaded
-        vim.g.lf_netrw = 1
-
-        require("lf").setup(
-            {
-                escape_quit = false,
-                border = "rounded",
-                highlights = { FloatBorder = { guifg = require("kimbox.palette").colors.magenta } }
-            }
-        )
+        require("lf").setup({})
 
         vim.keymap.set("n", "<C-o>", ":Lf<CR>")
     end,
-    requires = { "plenary.nvim", "toggleterm.nvim" }
+    dependencies = { "plenary.nvim", "akinsho/toggleterm.nvim" }
 }
