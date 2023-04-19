76b9f39c67806d181eb730e601f703582dbdb743
Shekhov Ismoil <veyxov@gmail.com>
2023-04-19 21:18:29 +0500

    color: installed kanagawa

--- lua/plugz/color.lua
+++ lua/plugz/color.lua
@@ -15,4 +15,37 @@ local Gruvbox = {
     end,
 }
 
-return Moonlify;
+local Kanagawa = {
+    "rebelot/kanagawa.nvim",
+    event = 'VeryLazy',
+    config = function()
+       require('kanagawa').setup({
+            compile = true,     -- enable compiling the colorscheme
+            undercurl = true,   -- enable undercurls
+            dimInactive = true, -- dim inactive window `:h hl-NormalNC`
+            overrides = function(colors)
+                local theme = colors.theme
+                return {
+                    TelescopeTitle = { fg = theme.ui.special, bold = true },
+                    TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+                    TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+                    TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+                    TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+                    TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+                    TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+                }
+            end,
+            theme = "wave", -- Load "wave" theme when 'background' option is not set
+            background = {
+                -- map the value of 'background' option to a theme
+                dark = "wave", -- try "dragon" !
+                light = "lotus"
+            },
+        })
+
+        -- setup must be called before loading
+        vim.cmd("colorscheme kanagawa") 
+    end
+}
+
+return Kanagawa;
