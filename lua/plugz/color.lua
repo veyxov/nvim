fab8a2a7a59da08222e449ee072643acc716d686
Shekhov Ismoil <veyxov@gmail.com>
2023-04-21 18:09:24 +0500

    colors: cleanup

--- lua/plugz/color.lua
+++ lua/plugz/color.lua
@@ -43,12 +43,7 @@ local Kanagawa = {
                     NormalFloat = { bg = "none" },
                     FloatBorder = { bg = "none" },
                     FloatTitle = { bg = "none" },
-                    -- Save an hlgroup with dark background and dimmed foreground
-                    -- so that you can use it where your still want darker windows.
-                    -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
                     NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-                    -- Popular plugins that open floats will link to NormalFloat by default;
-                    -- set their background accordingly if you wish to keep them dark and borderless
                     LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
                     TelescopeTitle = { fg = theme.ui.special, bold = true },
                     TelescopePromptNormal = { bg = theme.ui.bg_p1 },
@@ -59,9 +54,8 @@ local Kanagawa = {
                     TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
                 }
             end,
-            theme = "wave", -- Load "wave" theme when 'background' option is not set
+            theme = "wave",
             background = {
-                -- map the value of 'background' option to a theme
                 dark = "wave", -- try "dragon" !
                 light = "lotus"
             },
@@ -72,4 +66,4 @@ local Kanagawa = {
     end
 }
 
-return Poimandres;
+return Kanagawa;
