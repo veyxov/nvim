ca354bafc7df141c212b7c7fff5c6eb4bf4aa65e
Shekhov Ismoil <veyxov@gmail.com>
2023-05-16 17:39:32 +0500

    git: added more fugitive mappings

--- lua/plugz/git.lua
+++ lua/plugz/git.lua
@@ -29,7 +29,8 @@ local Fugitive = {
     "tpope/vim-fugitive",
     lazy = false,
     init = function()
-        map('<leader>gc', '<cmd>G commit<cr>')
+        map('gc', '<cmd>G commit<cr>')
+        map('gb', '<cmd>G blame<cr>')
     end
 }
 
