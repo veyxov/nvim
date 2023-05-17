d0a743e795c5580091151fc30a565764387af20c
Shekhov Ismoil <veyxov@gmail.com>
2023-05-17 16:29:44 +0500

    telescope: fix fzf

--- lua/plugz/telescope.lua
+++ lua/plugz/telescope.lua
@@ -4,9 +4,7 @@ local M = {
     dependencies = {
         {
             "nvim-telescope/telescope-fzf-native.nvim",
-            build =
-            [[cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build
-            build --config Release && cmake --install build --prefix build]]
+            build = 'make'
         },
     },
 }
