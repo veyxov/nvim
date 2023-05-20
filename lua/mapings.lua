fd85c00d1f141787244d1846e015dccab92a4724
Shekhov Ismoil <veyxov@gmail.com>
2023-05-20 21:19:45 +0500

    mappings: add mapping for yank inside yi is a same finger biargram in my current keyboard layout

--- lua/mapings.lua
+++ lua/mapings.lua
@@ -26,6 +26,8 @@ map('<leader>w', '<cmd>wall<cr>') -- Save file
 map('<leader>y', '"+y', { 'n', 'v' }) -- Copy to system clipboar
 map('<leader>v', '<C-V>')             -- Visual block mode
 
+map("hi", "yi")
+
 -- Insert a semicolon at the end of the line and create a new one
 map('<C-o>', '<esc>A;<esc>o', 'i')
 
