b99e7ea1bf5c45ab2b3579b774640ea23d9a5577
Shekhov Ismoil <veyxov@gmail.com>
2023-05-24 10:01:54 +0500

    globals: remove the restart method There are plugins that do this in a better manner

--- lua/globals.lua
+++ lua/globals.lua
@@ -3,16 +3,6 @@ local M = {
     Map = function(lhs, rhs, mode)
         mode = mode or 'n' -- We are normal, most of the time
         vim.keymap.set(mode, lhs, rhs)
-    end,
-    Restart = function()
-        -- Get current pane id
-        local current_pane = os.getenv("WEZTERM_PANE")
-
-        -- Spawn a new nvim instance and return to last position
-        vim.fn.jobstart([[wezterm cli spawn -- nvim --cmd 'call feedkeys("\<C-o>")']])
-
-        -- Kill the previous pane containing the old instance
-        vim.fn.jobstart(string.format([[sleep 1; wezterm cli kill-pane --pane-id %s]], current_pane))
     end
 }
 
