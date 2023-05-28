4097b7eebdc8de920d8c0190e1b89be5ee68ff6a
Shekhov Ismoil <veyxov@gmail.com>
2023-05-28 16:53:06 +0500

    mappings: fix in and around objects

--- lua/mapings.lua
+++ lua/mapings.lua
@@ -22,6 +22,8 @@ map('<leader>w', '<cmd>wall<cr>') -- Save file
 
 -- Select and copy
 map('h', 'y', { 'n', 'v', 'o' })
+map('ha', 'ya', { 'n', 'v', 'o' })
+map('hi', 'yi', { 'n', 'v', 'o' })
 map('<leader>h', '"+y', { 'n', 'v', 'o' })
 map('<leader>hh', '"+yy', { 'n', 'v', 'o' })
 map('<leader>v', '<C-V>')             -- Visual block mode
