e1608594f20eea470793ba15907200ae08256e9b
Shekhov Ismoil <veyxov@gmail.com>
2023-05-28 23:05:15 +0500

    lualine: added file path (relative)

--- lua/plugz/ui.lua
+++ lua/plugz/ui.lua
@@ -17,18 +17,30 @@ local Lualine = {
                 theme = 'auto',
                 component_separators = { left = '', right = ''},
                 section_separators = { left = '', right = ''},
-                globalstatus = true,
-                refresh = {
-                    statusline = 1000,
-                    tabline = 1000,
-                    winbar = 1000,
-                }
+                globalstatus = true
             },
             sections = {
                 lualine_a = {'mode'},
                 lualine_b = {'branch', 'diff', 'diagnostics'},
-                lualine_c = {'filename'},
-                lualine_x = {'encoding', 'fileformat', 'filetype'},
+                lualine_c = {{
+                    'filename',
+                    file_status = true,      -- Displays file status (readonly status, modified status)
+                    newfile_status = false,  -- Display new file status (new file means no write after created)
+                    path = 1,                -- 0: Just the filename
+                    -- 1: Relative path
+                    -- 2: Absolute path
+                    -- 3: Absolute path, with tilde as the home directory
+                    -- 4: Filename and parent dir, with tilde as the home directory
+                    shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
+                    -- for other components. (terrible name, any suggestions?)
+                    symbols = {
+                        modified = '[+]',      -- Text to show when the file is modified.
+                        readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+                        unnamed = '[No Name]', -- Text to show for unnamed buffers.
+                        newfile = '[New]',     -- Text to show for newly created file before first write
+                    }
+                }},
+                lualine_x = {'FugitiveHead', 'fileformat', 'filetype'},
                 lualine_y = {'progress'},
                 lualine_z = {'location'}
             },
