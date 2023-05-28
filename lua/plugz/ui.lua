708686b3b2a7e8914994e65de1a3a681955e45b9
Shekhov Ismoil <veyxov@gmail.com>
2023-05-28 23:20:28 +0500

    dressing: format

--- lua/plugz/ui.lua
+++ lua/plugz/ui.lua
@@ -22,20 +22,15 @@ local Lualine = {
                 lualine_b = {'branch', 'diff', 'diagnostics'},
                 lualine_c = {{
                     'filename',
-                    file_status = true,      -- Displays file status (readonly status, modified status)
-                    newfile_status = false,  -- Display new file status (new file means no write after created)
-                    path = 1,                -- 0: Just the filename
-                    -- 1: Relative path
-                    -- 2: Absolute path
-                    -- 3: Absolute path, with tilde as the home directory
-                    -- 4: Filename and parent dir, with tilde as the home directory
-                    shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
-                    -- for other components. (terrible name, any suggestions?)
+                    file_status = true,
+                    newfile_status = false,
+                    path = 1,
+                    shorting_target = 40,
                     symbols = {
-                        modified = '[+]',      -- Text to show when the file is modified.
-                        readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
-                        unnamed = '[No Name]', -- Text to show for unnamed buffers.
-                        newfile = '[New]',     -- Text to show for newly created file before first write
+                        modified = '[+]',
+                        readonly = '[-]',
+                        unnamed = '[No Name]', 
+                        newfile = '[New]',
                     }
                 }},
                 lualine_x = {'fileformat', 'filetype'},
