22977cf5ab7a451118bf2cf8c62db9c54f5c3abf
Shekhov Ismoil <veyxov@gmail.com>
2023-06-05 11:23:39 +0500

    tree-sitter: make moves repeatable

--- lua/plugz/tree-sitter.lua
+++ lua/plugz/tree-sitter.lua
@@ -68,6 +68,12 @@ local opts = {
 
 function M.config()
     require 'nvim-treesitter.configs'.setup(opts)
+    local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+
+    -- Repeat movement with ; and ,
+    -- ensure ; goes forward and , goes backward regardless of the last direction
+    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+    vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
 end
 
 return M
