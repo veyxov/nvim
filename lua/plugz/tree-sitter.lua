709e9deb1e35510bf10511e687307a621e752ed8
Shekhov Ismoil <veyxov@gmail.com>
2023-06-12 14:21:07 +0500

    tree-sitter: added assignment text objects

--- lua/plugz/tree-sitter.lua
+++ lua/plugz/tree-sitter.lua
@@ -5,7 +5,30 @@ local M = {
 }
 
 M.dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
+    { 'nvim-treesitter/nvim-treesitter-textobjects' },
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        keys = {
+            { "<leader>xc", "<cmd>TSContextToggle<cr>" },
+            { 'gx',         function() require 'treesitter-context'.go_to_context() end }
+        },
+        config = function()
+            require 'treesitter-context'.setup {
+                enable = true,
+                max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
+                min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+                line_numbers = true,
+                multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
+                trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+                mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
+                -- Separator between context and content. Should be a single character string, like '-'.
+                -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+                separator = nil,
+                zindex = 9999,   -- The Z-index of the context window
+                on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+            }
+        end
+    }
 }
 
 local opts = {
@@ -37,6 +60,10 @@ local opts = {
                 ['ic'] = '@class.inner',
                 ['AA'] = '@parameter.inner',
                 ['aa'] = '@parameter.outer',
+                ['a=='] = '@assignment.outer',
+                ['i=='] = '@assignment.inner',
+                ['i=r'] = '@assignment.lhs',
+                ['i=d'] = '@assignment.rhs',
             },
             include_surrounding_whitespace = false,
         },
