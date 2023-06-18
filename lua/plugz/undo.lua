75af63360e36a62a347ae5b5c71255ee215b32e3
Shekhov Ismoil <veyxov@gmail.com>
2023-06-18 16:38:04 +0500

    undo: added undo/redo highlighter

---
+++ lua/plugz/undo.lua
@@ -0,0 +1,17 @@
+return {
+    'tzachar/highlight-undo.nvim',
+    keys = {
+        { 'u' },
+        { '<C-r>' },
+    },
+    config = function()
+        require('highlight-undo').setup({
+            hlgroup = 'DiffAdd',
+            duration = 300,
+            keymaps = {
+                { 'n', 'u',     'undo', {} },
+                { 'n', '<C-r>', 'redo', {} },
+            }
+        })
+    end
+}
