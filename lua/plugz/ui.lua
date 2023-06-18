12f6abaab63ff29b014620fac29c2b59346acc67
Shekhov Ismoil <veyxov@gmail.com>
2023-06-18 16:14:43 +0500

    ui: remove lualine

--- lua/plugz/ui.lua
+++ lua/plugz/ui.lua
@@ -5,56 +5,14 @@ local Noice = {
     dependencies = {
         'MunifTanjim/nui.nvim',
     },
-}
-
-local Lualine = {
-    'nvim-lualine/lualine.nvim',
-    event = 'InsertEnter',
-    config = function()
-        require('lualine').setup {
-            options = {
-                component_separators = { left = '', right = '' },
-                section_separators = { left = '', right = '' },
-                globalstatus = true,
-            },
-            sections = {
-                lualine_a = { 'mode' },
-                lualine_b = { 'branch', 'diff', 'diagnostics' },
-                lualine_c = {
-                    {
-                        'filename',
-                        file_status = true,
-                        newfile_status = false,
-                        path = 1,
-                        shorting_target = 40,
-                        symbols = {
-                            modified = '[+]',
-                            readonly = '[-]',
-                            unnamed = '[No Name]',
-                            newfile = '[New]',
-                        },
-                    },
-                },
-                lualine_x = { 'fileformat', 'filetype' },
-            },
-            inactive_sections = {
-                lualine_c = { 'filename' },
-                lualine_x = { 'location' },
-            },
-            inactive_winbar = {
-                lualine_c = { 'filename' },
-            },
-        }
-    end,
-}
-
-Noice.keys = {
-    {
-        '<leader>ma',
-        function()
-            require('noice').cmd 'all'
-        end,
-    },
+    keys = {
+        {
+            '<leader>ma',
+            function()
+                require('noice').cmd 'all'
+            end,
+        },
+    }
 }
 
 Noice.config = function()
