bd941ea4a99abda2b82502a4797f15af40f772c8
Shekhov Ismoil <veyxov@gmail.com>
2023-06-18 20:29:21 +0500

    comment: map to 'k'

--- lua/plugz/comment.lua
+++ lua/plugz/comment.lua
@@ -2,6 +2,16 @@ return {
     'numToStr/Comment.nvim',
     lazy = false,
     config = function()
-        require('Comment').setup()
+        require('Comment').setup({
+             opleader = {
+        ---Line-comment keymap
+        line = 'k',
+        ---Block-comment keymap
+        block = 'gb',
+    },
+            toggler = {
+                line = 'kk'
+            }
+        })
     end,
 }
