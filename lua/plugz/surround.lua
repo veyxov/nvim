496a3c8ff536114e5054700e674052da0c286021
Shekhov Ismoil <veyxov@gmail.com>
2023-06-18 19:59:56 +0500

    format

--- lua/plugz/surround.lua
+++ lua/plugz/surround.lua
@@ -16,15 +16,15 @@ M.config = function()
     require('nvim-surround').setup {
         move_cursor = false,
         keymaps = {
-            normal = "l",
-            normal_cur = "ll",
-            normal_line = "L",
-            normal_cur_line = "Ll",
-            visual = "l",
-            visual_line = "L",
-            delete = "ds",
-            change = "cs",
-            change_line = "cl",
+            normal = 'l',
+            normal_cur = 'll',
+            normal_line = 'L',
+            normal_cur_line = 'Ll',
+            visual = 'l',
+            visual_line = 'L',
+            delete = 'ds',
+            change = 'cs',
+            change_line = 'cl',
         },
     }
 end
