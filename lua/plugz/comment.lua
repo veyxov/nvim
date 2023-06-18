ec830189a2c6a64522293e7ca96fff37e1bcb89e
Shekhov Ismoil <veyxov@gmail.com>
2023-06-18 20:29:52 +0500

    format

--- lua/plugz/comment.lua
+++ lua/plugz/comment.lua
@@ -2,16 +2,16 @@ return {
     'numToStr/Comment.nvim',
     lazy = false,
     config = function()
-        require('Comment').setup({
-             opleader = {
-        ---Line-comment keymap
-        line = 'k',
-        ---Block-comment keymap
-        block = 'gb',
-    },
+        require('Comment').setup {
+            opleader = {
+                ---Line-comment keymap
+                line = 'k',
+                ---Block-comment keymap
+                block = 'gb',
+            },
             toggler = {
-                line = 'kk'
-            }
-        })
+                line = 'kk',
+            },
+        }
     end,
 }
