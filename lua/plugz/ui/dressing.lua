0ec7f507cfd9092d9d980e50dec8bf74ec782da3
Shekhov Ismoil <veyxov@gmail.com>
2023-06-22 21:19:18 +0500

    style: format

--- lua/plugz/ui/dressing.lua
+++ lua/plugz/ui/dressing.lua
@@ -5,26 +5,26 @@ end
 
 return {
     'stevearc/dressing.nvim',
--- Load the module when needed
-init = function()
-    vim.ui.select = function(...)
-        load_dressing()
-        return vim.ui.select(...)
-    end
-    vim.ui.input = function(...)
-        load_dressing()
-        return vim.ui.input(...)
-    end
-end,
+    -- Load the module when needed
+    init = function()
+        vim.ui.select = function(...)
+            load_dressing()
+            return vim.ui.select(...)
+        end
+        vim.ui.input = function(...)
+            load_dressing()
+            return vim.ui.input(...)
+        end
+    end,
 
-config = function()
-    require('dressing').setup {
-        input = {
-            default_prompt = '> ',
-            border = 'single',
-            prompt_align = 'center',
-            relative = 'win',
-        },
-    }
-end
+    config = function()
+        require('dressing').setup {
+            input = {
+                default_prompt = '> ',
+                border = 'single',
+                prompt_align = 'center',
+                relative = 'win',
+            },
+        }
+    end,
 }
--- lua/plugz/ui/noice.lua
+++ lua/plugz/ui/noice.lua
@@ -16,41 +16,40 @@ return {
         },
     },
 
-config = function()
-    local opts = {
-        lsp = {
-            override = {
-                ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-                ['vim.lsp.util.stylize_markdown'] = true,
-                ['cmp.entry.get_documentation'] = true,
-            },
-        },
-        presets = {
-            bottom_search = true,
-            command_palette = true,
-            long_message_to_split = true,
-            inc_rename = true, -- temp?
-        },
-        background_color = '#FF0000',
-        views = {
-            cmdline_popup = {
-                relative = 'editor',
-                border = {
-                    style = 'none',
-                    padding = { 0, 0 },
+    config = function()
+        local opts = {
+            lsp = {
+                override = {
+                    ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                    ['vim.lsp.util.stylize_markdown'] = true,
+                    ['cmp.entry.get_documentation'] = true,
                 },
             },
-            popupmenu = {
-                relative = 'editor',
-                border = {
-                    style = 'none',
-                    padding = { 0, 1 },
+            presets = {
+                bottom_search = true,
+                command_palette = true,
+                long_message_to_split = true,
+                inc_rename = true, -- temp?
+            },
+            background_color = '#FF0000',
+            views = {
+                cmdline_popup = {
+                    relative = 'editor',
+                    border = {
+                        style = 'none',
+                        padding = { 0, 0 },
+                    },
+                },
+                popupmenu = {
+                    relative = 'editor',
+                    border = {
+                        style = 'none',
+                        padding = { 0, 1 },
+                    },
                 },
             },
-        },
-    }
+        }
 
-    require('noice').setup(opts)
-end
+        require('noice').setup(opts)
+    end,
 }
-
