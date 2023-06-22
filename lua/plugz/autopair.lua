9e14657fcfb04bffceaa5e237df12e9c132b9578
Shekhov Ismoil <veyxov@gmail.com>
2023-06-22 16:13:18 +0500

    autopair: ignore error

--- lua/plugz/autopair.lua
+++ lua/plugz/autopair.lua
@@ -2,6 +2,7 @@ local cmp_integration = function()
     local cmp = require 'cmp'
     local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
 
+    ---@diagnostic disable-next-line: undefined-field
     cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 end
 
