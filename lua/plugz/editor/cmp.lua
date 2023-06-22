4e80f508b01ed93fc42a92583ffe11d91ff359d2
Shekhov Ismoil <veyxov@gmail.com>
2023-06-22 21:56:35 +0500

    cmp: added cmdline

--- lua/plugz/editor/cmp.lua
+++ lua/plugz/editor/cmp.lua
@@ -8,7 +8,8 @@ M.dependencies = {
     'hrsh7th/cmp-buffer',
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
-    'ray-x/cmp-treesitter'
+    'ray-x/cmp-treesitter',
+    'hrsh7th/cmp-cmdline',
 }
 
 function M.config()
@@ -60,6 +61,24 @@ function M.config()
             },
         },
     }
+
+    -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+            { name = 'buffer' },
+        },
+    })
+
+    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+    cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+            { name = 'path' },
+        }, {
+            { name = 'cmdline' },
+        }),
+    })
 end
 
 return M
