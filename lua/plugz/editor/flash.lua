6bcc91e6a13d3bf93ecf2b70523384ffb47f3418
Shekhov Ismoil <veyxov@gmail.com>
2023-06-23 10:03:39 +0500

    flash: added advanced use cases

--- lua/plugz/editor/flash.lua
+++ lua/plugz/editor/flash.lua
@@ -19,6 +19,57 @@ local M = {
                 require('flash').treesitter()
             end,
         },
+        {
+            -- Show diagnostic details
+            '<leader>sd',
+            function()
+                require('flash').jump {
+                    matcher = function(win)
+                        ---@param diag Diagnostic
+                        return vim.tbl_map(function(diag)
+                            return {
+                                pos = { diag.lnum + 1, diag.col },
+                                end_pos = {
+                                    diag.end_lnum + 1,
+                                    diag.end_col - 1,
+                                },
+                            }
+                        end, vim.diagnostic.get(
+                            vim.api.nvim_win_get_buf(win)
+                        ))
+                    end,
+                    action = function(match, state)
+                        vim.api.nvim_win_call(match.win, function()
+                            vim.api.nvim_win_set_cursor(match.win, match.pos)
+                            vim.diagnostic.open_float()
+                            vim.api.nvim_win_set_cursor(match.win, state.pos)
+                        end)
+                    end,
+                }
+            end,
+        },
+        {
+            '<leader>w',
+            function()
+                require('flash').jump {
+                    search = {
+                        mode = function(str)
+                            return '\\<' .. str
+                        end,
+                    },
+                }
+            end,
+        },
+        {
+            '<leader>l',
+            function()
+                require('flash').jump {
+                    search = { mode = 'search' },
+                    highlight = { label = { after = { 0, 0 } } },
+                    pattern = '^',
+                }
+            end,
+        },
     },
 }
 
