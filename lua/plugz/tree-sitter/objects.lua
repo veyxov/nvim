9c875376b742d8a09597ae49c9d6e4078583f204
Shekhov Ismoil <veyxov@gmail.com>
2023-06-28 06:38:04 +0500

    objects: move keys to separate section

--- lua/plugz/tree-sitter/objects.lua
+++ lua/plugz/tree-sitter/objects.lua
@@ -1,87 +1,6 @@
 local ox = { 'o', 'x' }
-
-return {
+local M = {
     'chrisgrieser/nvim-various-textobjs',
-    keys = {
-        {
-            'ii',
-            "<cmd>lua require('various-textobjs').indentation(true, true)<CR>",
-            mode = ox,
-        },
-        {
-            'aI',
-            "<cmd>lua require('various-textobjs').indentation(false, true)<CR>",
-            mode = ox,
-        },
-        {
-            'ai',
-            "<cmd>lua require('various-textobjs').indentation(false, false)<CR>",
-            mode = ox,
-        },
-        {
-            'iI',
-            "<cmd>lua require('various-textobjs').indentation(true, true)<CR>",
-            mode = ox,
-        },
-        {
-            'ah',
-            "<cmd>lua require('various-textobjs').subword(true)<CR>",
-            mode = ox,
-        },
-        {
-            'ih',
-            "<cmd>lua require('various-textobjs').subword(false)<CR>",
-            mode = ox,
-        },
-        {
-            't;',
-            "<cmd>lua require('various-textobjs').toNextClosingBracket()<CR>",
-            mode = ox,
-        },
-        {
-            'ax',
-            "<cmd>lua require('various-textobjs').lineCharacterwise(false)<CR>",
-            mode = ox,
-        },
-        {
-            'ar',
-            "<cmd>lua require('various-textobjs').value(false)<CR>",
-            mode = ox,
-        },
-        {
-            'ir',
-            "<cmd>lua require('various-textobjs').value(true)<CR>",
-            mode = ox,
-        },
-        {
-
-            'ak',
-            "<cmd>lua require('various-textobjs').key(false)<CR>",
-            mode = ox,
-        },
-        {
-
-            'ik',
-            "<cmd>lua require('various-textobjs').key(true)<CR>",
-            mode = ox,
-        },
-        {
-
-            'im',
-            "<cmd>lua require('various-textobjs').chainMember(true)<CR>",
-            mode = ox,
-        },
-        {
-            'am',
-            "<cmd>lua require('various-textobjs').chainMember(false)<CR>",
-            mode = ox,
-        },
-        {
-            'gw',
-            "<cmd>lua require('various-textobjs').visibleInWindow()<CR>",
-            mode = ox,
-        },
-    },
     config = function()
         require('various-textobjs').setup {
             -- lines to seek forwards for "small" textobjs (mostly characterwise textobjs)
@@ -93,3 +12,86 @@ return {
         }
     end,
 }
+
+M.keys = {
+    {
+        'ii',
+        "<cmd>lua require('various-textobjs').indentation(true, true)<CR>",
+        mode = ox,
+    },
+    {
+        'aI',
+        "<cmd>lua require('various-textobjs').indentation(false, true)<CR>",
+        mode = ox,
+    },
+    {
+        'ai',
+        "<cmd>lua require('various-textobjs').indentation(false, false)<CR>",
+        mode = ox,
+    },
+    {
+        'iI',
+        "<cmd>lua require('various-textobjs').indentation(true, true)<CR>",
+        mode = ox,
+    },
+    {
+        'ah',
+        "<cmd>lua require('various-textobjs').subword(true)<CR>",
+        mode = ox,
+    },
+    {
+        'ih',
+        "<cmd>lua require('various-textobjs').subword(false)<CR>",
+        mode = ox,
+    },
+    {
+        't;',
+        "<cmd>lua require('various-textobjs').toNextClosingBracket()<CR>",
+        mode = ox,
+    },
+    {
+        'ax',
+        "<cmd>lua require('various-textobjs').lineCharacterwise(false)<CR>",
+        mode = ox,
+    },
+    {
+        'ar',
+        "<cmd>lua require('various-textobjs').value(false)<CR>",
+        mode = ox,
+    },
+    {
+        'ir',
+        "<cmd>lua require('various-textobjs').value(true)<CR>",
+        mode = ox,
+    },
+    {
+
+        'ak',
+        "<cmd>lua require('various-textobjs').key(false)<CR>",
+        mode = ox,
+    },
+    {
+
+        'ik',
+        "<cmd>lua require('various-textobjs').key(true)<CR>",
+        mode = ox,
+    },
+    {
+
+        'im',
+        "<cmd>lua require('various-textobjs').chainMember(true)<CR>",
+        mode = ox,
+    },
+    {
+        'am',
+        "<cmd>lua require('various-textobjs').chainMember(false)<CR>",
+        mode = ox,
+    },
+    {
+        'gw',
+        "<cmd>lua require('various-textobjs').visibleInWindow()<CR>",
+        mode = ox,
+    },
+}
+
+return M
