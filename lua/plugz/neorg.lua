da04389600d907405b2654bd153afcb405e8927e
Shekhov Ismoil <veyxov@gmail.com>
2023-06-28 11:23:43 +0500

    neorg: concealer configuration

--- lua/plugz/neorg.lua
+++ lua/plugz/neorg.lua
@@ -6,7 +6,9 @@ return {
         require('neorg').setup {
             load = {
                 ['core.defaults'] = {}, -- Loads default behaviour
-                ['core.concealer'] = {}, -- Adds pretty icons to your documents
+                ['core.concealer'] = {
+                    config = { icon_preset = 'diamond' }
+                },
                 ['core.dirman'] = { -- Manages Neorg workspaces
                     config = {
                         workspaces = {
