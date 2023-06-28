e8b5014f3f54974ab644e5fa460cca29ee010224
Shekhov Ismoil <veyxov@gmail.com>
2023-06-28 06:36:50 +0500

    objects: move keybind declarations to lazy.keys

--- lua/plugz/tree-sitter/objects.lua
+++ lua/plugz/tree-sitter/objects.lua
@@ -1,9 +1,88 @@
+local ox = { 'o', 'x' }
+
 return {
     'chrisgrieser/nvim-various-textobjs',
-    lazy = false,
-    config = function()
-        local map = require('globals').Map
+    keys = {
+        {
+            'ii',
+            "<cmd>lua require('various-textobjs').indentation(true, true)<CR>",
+            mode = ox,
+        },
+        {
+            'aI',
+            "<cmd>lua require('various-textobjs').indentation(false, true)<CR>",
+            mode = ox,
+        },
+        {
+            'ai',
+            "<cmd>lua require('various-textobjs').indentation(false, false)<CR>",
+            mode = ox,
+        },
+        {
+            'iI',
+            "<cmd>lua require('various-textobjs').indentation(true, true)<CR>",
+            mode = ox,
+        },
+        {
+            'ah',
+            "<cmd>lua require('various-textobjs').subword(true)<CR>",
+            mode = ox,
+        },
+        {
+            'ih',
+            "<cmd>lua require('various-textobjs').subword(false)<CR>",
+            mode = ox,
+        },
+        {
+            't;',
+            "<cmd>lua require('various-textobjs').toNextClosingBracket()<CR>",
+            mode = ox,
+        },
+        {
+            'ax',
+            "<cmd>lua require('various-textobjs').lineCharacterwise(false)<CR>",
+            mode = ox,
+        },
+        {
+            'ar',
+            "<cmd>lua require('various-textobjs').value(false)<CR>",
+            mode = ox,
+        },
+        {
+            'ir',
+            "<cmd>lua require('various-textobjs').value(true)<CR>",
+            mode = ox,
+        },
+        {
 
+            'ak',
+            "<cmd>lua require('various-textobjs').key(false)<CR>",
+            mode = ox,
+        },
+        {
+
+            'ik',
+            "<cmd>lua require('various-textobjs').key(true)<CR>",
+            mode = ox,
+        },
+        {
+
+            'im',
+            "<cmd>lua require('various-textobjs').chainMember(true)<CR>",
+            mode = ox,
+        },
+        {
+            'am',
+            "<cmd>lua require('various-textobjs').chainMember(false)<CR>",
+            mode = ox,
+        },
+        {
+            'gw',
+            "<cmd>lua require('various-textobjs').visibleInWindow()<CR>",
+            mode = ox,
+        },
+    },
+    config = function()
         require('various-textobjs').setup {
             -- lines to seek forwards for "small" textobjs (mostly characterwise textobjs)
             -- set to 0 to only look in the current line
@@ -12,93 +91,5 @@ return {
             -- lines to seek forwards for "big" textobjs (linewise textobjs & url textobj)
             lookForwardBig = 15,
         }
-
-        local keymap = vim.keymap.set
-
-        -- indentation based objects
-        keymap(
-            { 'o', 'x' },
-            'ii',
-            "<cmd>lua require('various-textobjs').indentation(true, true)<CR>"
-        )
-        keymap(
-            { 'o', 'x' },
-            'aI',
-            "<cmd>lua require('various-textobjs').indentation(false, true)<CR>"
-        )
-        keymap(
-            { 'o', 'x' },
-            'iI',
-            "<cmd>lua require('various-textobjs').indentation(true, true)<CR>"
-        )
-        keymap(
-            { 'o', 'x' },
-            'ai',
-            "<cmd>lua require('various-textobjs').indentation(false, false)<CR>"
-        )
-
-        -- subword
-        keymap(
-            { 'o', 'x' },
-            'ah',
-            "<cmd>lua require('various-textobjs').subword(true)<CR>"
-        )
-        keymap(
-            { 'o', 'x' },
-            'ih',
-            "<cmd>lua require('various-textobjs').subword(false)<CR>"
-        )
-
-        -- to next closing bracket
-        keymap(
-            { 'o', 'x' },
-            't;',
-            "<cmd>lua require('various-textobjs').toNextClosingBracket()<CR>"
-        )
-        keymap(
-            { 'o', 'x' },
-            'ax',
-            "<cmd>lua require('various-textobjs').lineCharacterwise(false)<CR>"
-        )
-
-        -- rhs
-        keymap(
-            { 'o', 'x' },
-            'ar',
-            "<cmd>lua require('various-textobjs').value(false)<CR>"
-        )
-        keymap(
-            { 'o', 'x' },
-            'ir',
-            "<cmd>lua require('various-textobjs').value(true)<CR>"
-        )
-        -- lhs
-        keymap(
-            { 'o', 'x' },
-            'ak',
-            "<cmd>lua require('various-textobjs').key(false)<CR>"
-        )
-        keymap(
-            { 'o', 'x' },
-            'ik',
-            "<cmd>lua require('various-textobjs').key(true)<CR>"
-        )
-        -- Member funcions
-        keymap(
-            { 'o', 'x' },
-            'im',
-            "<cmd>lua require('various-textobjs').chainMember(true)<CR>"
-        )
-        keymap(
-            { 'o', 'x' },
-            'am',
-            "<cmd>lua require('various-textobjs').chainMember(false)<CR>"
-        )
-        -- Visible part of the screen
-        map(
-            'gw',
-            "<cmd>lua require('various-textobjs').visibleInWindow()<CR>",
-            { 'o', 'x' }
-        )
     end,
 }
