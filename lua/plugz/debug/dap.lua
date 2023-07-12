08590ee5817824b617024bdd9ed397bb41fcb59e
Shekhov Ismoil <veyxov@gmail.com>
2023-07-12 10:37:08 +0500

    dap: merge dap-ui

--- lua/plugz/debug/dap.lua
+++ lua/plugz/debug/dap.lua
@@ -2,17 +2,130 @@ local map = require('globals').Map
 
 local M = {
     'mfussenegger/nvim-dap',
-    lazy = false,
     dependencies = {
-        'rcarriga/nvim-dap-ui',
+        {
+            'rcarriga/nvim-dap-ui',
+            config = function()
+                local dap, dapui = require 'dap', require 'dapui'
+
+                dapui.setup {
+                    icons = {
+                        expanded = '▾',
+                        collapsed = '▸',
+                        current_frame = '▸',
+                    },
+                    mappings = {
+                        -- Use a table to apply multiple mappings
+                        expand = { '<CR>', '<2-LeftMouse>' },
+                        open = 'o',
+                        remove = 'd',
+                        edit = 'e',
+                        repl = 'r',
+                        toggle = 't',
+                    },
+                    -- Use this to override mappings for specific elements
+                    element_mappings = {
+                        -- Example:
+                        -- stacks = {
+                        --   open = "<CR>",
+                        --   expand = "o",
+                        -- }
+                    },
+                    -- Expand lines larger than the window
+                    -- Requires >= 0.7
+                    expand_lines = vim.fn.has 'nvim-0.7' == 1,
+                    -- Layouts define sections of the screen to place windows.
+                    -- The position can be "left", "right", "top" or "bottom".
+                    -- The size specifies the height/width depending on position. It can be an Int
+                    -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+                    -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+                    -- Elements are the elements shown in the layout (in order).
+                    -- Layouts are opened in order so that earlier layouts take priority in window sizing.
+                    layouts = {
+                        {
+                            elements = {
+                                -- Elements can be strings or table with id and size keys.
+                                { id = 'scopes', size = 0.25 },
+                                'breakpoints',
+                                'stacks',
+                                'watches',
+                            },
+                            size = 40, -- 40 columns
+                            position = 'left',
+                        },
+                        {
+                            elements = {
+                                'repl',
+                                'console',
+                            },
+                            size = 0.25, -- 25% of total lines
+                            position = 'bottom',
+                        },
+                    },
+                    controls = {
+                        -- Requires Neovim nightly (or 0.8 when released)
+                        enabled = true,
+                        -- Display controls in this element
+                        element = 'repl',
+                        icons = {
+                            pause = '',
+                            play = '',
+                            step_into = '',
+                            step_over = '',
+                            step_out = '',
+                            step_back = '',
+                            run_last = '↻',
+                            terminate = '□',
+                        },
+                    },
+                    floating = {
+                        max_height = nil, -- These can be integers or a float between 0 and 1.
+                        max_width = nil, -- Floats will be treated as percentage of your screen.
+                        border = 'single', -- Border style. Can be "single", "double" or "rounded"
+                        mappings = {
+                            close = { 'q', '<Esc>' },
+                        },
+                    },
+                    windows = { indent = 1 },
+                    render = {
+                        max_type_length = nil, -- Can be integer or nil.
+                        max_value_lines = 100, -- Can be integer or nil.
+                    },
+                }
+
+                dap.listeners.after.event_initialized['dapui_config'] = function(
+                )
+                    dapui.open()
+                end
+                dap.listeners.before.event_terminated['dapui_config'] = function(
+                )
+                    dapui.close()
+                end
+                dap.listeners.before.event_exited['dapui_config'] = function()
+                    dapui.close()
+                end
+            end,
+        },
+    },
+}
+
+M.keys = {
+    {
+        'yy',
+        function()
+            require('dap').continue()
+        end,
+    },
+    {
+        'yb',
+        function()
+            require('dap').toggle_breakpoint()
+        end,
     },
 }
 
 M.init = function()
     local dap = require 'dap'
-    map('yy', function()
-        dap.continue()
-    end)
     map('yi', function()
         dap.step_into()
     end)
@@ -24,9 +137,6 @@ M.init = function()
     end)
     map('yx', function()
         dap.close()
-    end)
-    map('yb', function()
-        dap.toggle_breakpoint()
     end)
 end
 
@@ -165,10 +275,8 @@ M.config = function()
             max_value_lines = 100,
         },
     }
-    ------------
-    -- Dap UI --
-    ------------
 
+    -- DapUi
     dap.listeners.after.event_initialized['dapui_config'] = function()
         dapui.open()
     end
@@ -187,66 +295,6 @@ M.config = function()
         'DapStopped',
         { text = '▶️', texthl = '', linehl = '', numhl = '' }
     )
-    ------------
-    -- PYTHON --
-    ------------
-    dap.adapters.python = function(cb, config_py)
-        if config_py.request == 'attach' then
-            ---@diagnostic disable-next-line: undefined-field
-            local port = (config_py.connect or config_py).port
-            ---@diagnostic disable-next-line: undefined-field
-            local host = (config_py.connect or config_py).host or '127.0.0.1'
-            cb {
-                type = 'server',
-                port = assert(
-                    port,
-                    '`connect.port` is required for a python `attach` configuration'
-                ),
-                host = host,
-                options = {
-                    source_filetype = 'python',
-                },
-            }
-        else
-            cb {
-                type = 'executable',
-                command = vim.fs.normalize(
-                    vim.fn.stdpath 'data'
-                        .. '/mason/packages/debugpy/venv/Scripts/python'
-                ),
-                args = { '-m', 'debugpy.adapter' },
-                options = {
-                    source_filetype = 'python',
-                },
-            }
-        end
-    end
-    dap.configurations.python = {
-        {
-            -- The first three options are required by nvim-dap
-            type = 'python', -- the type here established the link to the adapter definition: `dap.adapters.python`
-            request = 'launch',
-            name = 'Launch file',
-            console = 'integratedTerminal',
-            -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
-
-            program = '${file}', -- This configuration will launch the current file if used.
-            pythonPath = function()
-                -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
-                -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
-                -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
-                -- local cwd = vim.fn.getcwd()
-                -- if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
-                --   return cwd .. '/venv/bin/python'
-                -- elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
-                --   return cwd .. '/.venv/bin/python'
-                -- else
-                -- return 'C:\\Windows\\py.exe'
-                return 'C:\\Windows\\py.exe'
-                --end
-            end,
-        },
-    }
 end
 
 return M
