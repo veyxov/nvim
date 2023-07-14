0a47435e0cec30e1af2e19c6d7f520738d158db9
Shekhov Ismoil <veyxov@gmail.com>
2023-07-14 11:55:51 +0500

    neotest: fix mappings

--- lua/plugz/lsp/neotest.lua
+++ lua/plugz/lsp/neotest.lua
@@ -2,12 +2,10 @@ return {
     'nvim-neotest/neotest',
     keys = {
         {
-            'n',
             'ytd',
             function() require('neotest').run.run { strategy = 'dap' } end,
         },
         {
-            'n',
             'ytr',
             function() require('neotest').run.run() end,
         },
