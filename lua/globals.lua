184b90f4f604d87937295b984a379f3e4cc1fb68
Shekhov Ismoil <veyxov@gmail.com>
2023-07-14 10:19:35 +0500

    globals: cleanup

--- lua/globals.lua
+++ lua/globals.lua
@@ -1,18 +1,9 @@
 local M = {}
+
 -- Mapping helper
 M.Map = function(lhs, rhs, mode)
     mode = mode or 'n' -- normal, most of the time
     vim.keymap.set(mode, lhs, rhs)
 end
-
--- Languages that need LSP and CMP
-M.smart_langs = {
-    'go',
-    'javascript',
-    'lua',
-    'rust',
-    'typescript',
-    'vim',
-}
 
 return M
