fc2c8bf6b22cfa6fb9a31aca0b2495a0ef0829d9
Shekhov Ismoil <veyxov@gmail.com>
2023-07-17 20:59:01 +0500

    autotag: removed

--- lua/plugz/editor/autotag.lua
+++
@@ -1,7 +0,0 @@
-local Autotags = {
-    'windwp/nvim-ts-autotag',
-    ft = { 'svelte', 'js', 'ts', 'html', 'vue' },
-    config = function() require('nvim-ts-autotag').setup() end,
-}
-
-return Autotags
