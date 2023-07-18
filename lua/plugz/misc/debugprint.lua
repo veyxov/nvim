6753123e08f290addd44069ad86660b6246ed0ae
Shekhov Ismoil <veyxov@gmail.com>
2023-07-18 10:21:24 +0500

    cleanup: remove unused plugins

--- lua/plugz/misc/debugprint.lua
+++ lua/plugz/misc/debugprint.lua
@@ -1,11 +1,34 @@
-return {
-    "andrewferrier/debugprint.nvim",
+local M = {
+    'andrewferrier/debugprint.nvim',
+    lazy = false,
     config = function()
         local opts = {
             create_keymaps = false,
-            cerate_commands = false
+            cerate_commands = false,
         }
 
-        require("debugprint").setup(opts)
-    end
+        require('debugprint').setup(opts)
+        vim.keymap.set('n', '<Leader>D', function()
+            -- Note: setting `expr=true` and returning the value are essential
+            return require('debugprint').debugprint { above = true }
+        end, {
+            expr = true,
+        })
+    end,
 }
+
+M.keys = {
+    {
+        'n',
+        '<Leader>d',
+        function()
+            -- Note: setting `expr=true` and returning the value are essential
+            return require('debugprint').debugprint()
+        end,
+        {
+            expr = true,
+        },
+    },
+}
+
+return M
--- lua/plugz/misc/visual.lua
+++
@@ -1,5 +0,0 @@
-return {
-    '00sapo/visual.nvim',
-    lazy = false,
-    enabled = false, -- TODO: Experiment with this, when more mature
-}
