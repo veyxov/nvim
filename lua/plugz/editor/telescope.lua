94ae483062d606f43b750e00f3dac1885b89061a
Shekhov Ismoil <veyxov@gmail.com>
2023-07-19 14:10:55 +0500

    telescope: grep files from project root

--- lua/plugz/editor/telescope.lua
+++ lua/plugz/editor/telescope.lua
@@ -48,6 +48,7 @@ function M.config()
             mappings = {
                 i = {
                     ['<esc>'] = actions.close,
+                    -- ['<C-H>'] = TODO: clean current line
                 },
             },
             layout_strategy = 'horizontal',
@@ -64,7 +65,28 @@ function M.config()
     telescope.load_extension 'fzf'
 end
 
-local project_files = function()
+local function live_grep_from_project_git_root()
+    local function is_git_repo()
+        vim.fn.system 'git rev-parse --is-inside-work-tree'
+
+        return vim.v.shell_error == 0
+    end
+
+    local function get_git_root()
+        local dot_git_path = vim.fn.finddir('.git', '.;')
+        return vim.fn.fnamemodify(dot_git_path, ':h')
+    end
+
+    local opts = {}
+
+    if is_git_repo() then opts = {
+        cwd = get_git_root(),
+    } end
+
+    require('telescope.builtin').live_grep(opts)
+end
+
+local function project_files()
     local opts = {} -- define here if you want to define something
     vim.fn.system 'git rev-parse --is-inside-work-tree'
     if vim.v.shell_error == 0 then
@@ -77,7 +99,10 @@ end
 M.keys = {
     { '<leader>n', function() project_files() end },
     { '<leader>gc', '<cmd>Telescope grep_string theme=ivy<cr>' },
-    { '<leader>g', '<cmd>Telescope live_grep theme=ivy<cr>' },
+    {
+        '<leader>g',
+        function() live_grep_from_project_git_root() end,
+    },
     {
         '<leader>/',
         '<cmd>Telescope current_buffer_fuzzy_find theme=get_dropdown previewer=false<cr>',
