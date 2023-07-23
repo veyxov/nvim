efc66efce54030d655d791e80456c8d77930e7b8
Shekhov Ismoil <veyxov@gmail.com>
2023-07-23 09:49:10 +0500

    style: format

--- lua/plugz/ui/color.lua
+++ lua/plugz/ui/color.lua
@@ -30,8 +30,8 @@ local Kanagawa = {
     event = 'VeryLazy',
     config = function()
         require('kanagawa').setup {
-            compile = true,     -- enable compiling the colorscheme
-            undercurl = true,   -- enable undercurls
+            compile = true, -- enable compiling the colorscheme
+            undercurl = true, -- enable undercurls
             dimInactive = true, -- dim inactive window `:h hl-NormalNC`
             overrides = function(colors)
                 local theme = colors.theme
@@ -219,12 +219,10 @@ local Catpuchin = {
 }
 
 local Citruszest = {
-    "zootedb0t/citruszest.nvim",
+    'zootedb0t/citruszest.nvim',
     lazy = false,
     priority = 1000,
-    config = function ()
-        vim.cmd [[colorscheme citruszest]]
-    end
+    config = function() vim.cmd [[colorscheme citruszest]] end,
 }
 
 return {
