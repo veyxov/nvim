edb1e183bb548b87b6c9a21f1a4601dedfa7c7d0
Shekhov Ismoil <veyxov@gmail.com>
2023-07-30 15:12:38 +0500

    plugins: set plenary priority

--- lua/plugz/init.lua
+++ lua/plugz/init.lua
@@ -1,11 +1,11 @@
 return {
+    { 'nvim-lua/plenary.nvim' },
     { import = 'plugz.git' },
     { import = 'plugz.ui' },
     { import = 'plugz.tree-sitter' },
     { import = 'plugz.editor' },
     { import = 'plugz.misc' },
     { import = 'plugz.lsp' },
-    { 'nvim-lua/plenary.nvim' },
     {
         'nvim-tree/nvim-web-devicons',
         config = function() require('nvim-web-devicons').setup {} end,
