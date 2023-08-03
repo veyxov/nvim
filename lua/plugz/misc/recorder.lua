07b64df38317b6c23c746cbbcc38e7940b309efa
Shekhov Ismoil <veyxov@gmail.com>
2023-08-03 10:47:38 +0500

    recorder: configuration

--- lua/plugz/misc/recorder.lua
+++ lua/plugz/misc/recorder.lua
@@ -7,6 +7,9 @@ return {
     'chrisgrieser/nvim-recorder',
     keys = {
         recording,
+        play,
+        switch,
+        edit,
     },
     config = function()
         -- default values
@@ -19,32 +22,24 @@ return {
                 playMacro = play,
                 switchSlot = switch,
                 editMacro = edit,
-                yankMacro = '<leader>kh',
+                yankMacro = '<leader>my',
                 addBreakPoint = '##', -- ⚠️ this should be a string you don't use in insert mode during a macro
             },
 
             -- Clears all macros-slots on startup.
             clear = false,
 
-            -- Log level used for any notification, mostly relevant for nvim-notify.
-            -- (Note that by default, nvim-notify does not show the levels trace & debug.)
-            logLevel = vim.log.levels.INFO,
-
             -- If enabled, only critical notifications are sent.
             -- If you do not use a plugin like nvim-notify, set this to `true`
             -- to remove otherwise annoying messages.
-            lessNotifications = false,
+            lessNotifications = true,
 
-            -- Use nerdfont icons in the status bar components and keymap descriptions
-            useNerdfontIcons = true,
+            useNerdfontIcons = false,
 
-            -- Performance optimzations for macros with high count. When `playMacro` is
-            -- triggered with a count higher than the threshold, nvim-recorder
-            -- temporarily changes changes some settings for the duration of the macro.
             performanceOpts = {
-                countThreshold = 100,
-                lazyredraw = true, -- enable lazyredraw (see `:h lazyredraw`)
-                noSystemClipboard = true, -- remove `+`/`*` from clipboard option
+                countThreshold = 10,
+                lazyredraw = true,
+                noSystemClipboard = true,
                 autocmdEventsIgnore = { -- temporarily ignore these autocmd events
                     'TextChangedI',
                     'TextChanged',
@@ -53,10 +48,6 @@ return {
                     'InsertCharPre',
                 },
             },
-
-            -- [experimental] partially share keymaps with nvim-dap.
-            -- (See README for further explanations.)
-            dapSharedKeymaps = false,
         }
     end,
 }
