a73f13d8ea9e15260b3cabdb0513cd5ceafc1a00
Shekhov Ismoil <veyxov@gmail.com>
2023-08-03 13:46:30 +0500

    mappings: remove mapping, handled by plugin

--- lua/mapings.lua
+++ lua/mapings.lua
@@ -8,22 +8,6 @@ map('<leader>q', '<cmd>qall!<cr>')
 map('<C-H>', '<C-W>', 'i')
 map('<C-H>', '<cmd>q<cr>')
 
---- Replace text-object with yanked content
--- TODO: Any better alternatives?
--- NOTE: There is a plugin for this
-map(';r', function()
-    local paste_cmd = 'p'
-    local prev_func = vim.go.operatorfunc
-
-    _G.paste_replace = function()
-        vim.api.nvim_feedkeys('`[v`]' .. paste_cmd, 'n', true)
-        vim.go.operatorfunc = prev_func
-        _G.paste_replace = nil
-    end
-    vim.go.operatorfunc = 'v:lua.paste_replace'
-    vim.api.nvim_feedkeys('g@', 'n', false)
-end)
-
 -- Window navigation
 map('<C-Down>', '<C-w><Down>')
 map('<C-Left>', '<C-w><Left>')
