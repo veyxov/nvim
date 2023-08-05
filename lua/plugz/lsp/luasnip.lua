2ee99aa7645dbc6004ba75b180a8c50c92d6ab56
Shekhov Ismoil <veyxov@gmail.com>
2023-08-05 08:42:47 +0500

    snippets: installed pre-made snippets for luansip

--- lua/plugz/lsp/luasnip.lua
+++ lua/plugz/lsp/luasnip.lua
@@ -1,7 +1,10 @@
 local M = {
     'L3MON4D3/LuaSnip',
+    dependencies = { "rafamadriz/friendly-snippets" },
 }
 
-M.config = function() end
+M.config = function()
+    require("luasnip.loaders.from_vscode").lazy_load()
+end
 
 return M
