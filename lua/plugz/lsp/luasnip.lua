17c4b9d67e0436407fa03ab5376a9ebe7614d0b2
Shekhov Ismoil <veyxov@gmail.com>
2023-08-05 08:43:07 +0500

    style: format

--- lua/plugz/lsp/luasnip.lua
+++ lua/plugz/lsp/luasnip.lua
@@ -1,10 +1,8 @@
 local M = {
     'L3MON4D3/LuaSnip',
-    dependencies = { "rafamadriz/friendly-snippets" },
+    dependencies = { 'rafamadriz/friendly-snippets' },
 }
 
-M.config = function()
-    require("luasnip.loaders.from_vscode").lazy_load()
-end
+M.config = function() require('luasnip.loaders.from_vscode').lazy_load() end
 
 return M
