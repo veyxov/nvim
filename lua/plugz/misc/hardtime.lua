a2c4ada1d1ab6fe96dea1bbd970b454d31920232
Shekhov Ismoil <veyxov@gmail.com>
2023-08-20 22:43:30 +0500

    hardtime: installed, maybe I'll find some bad habbits 😁

---
+++ lua/plugz/misc/hardtime.lua
@@ -0,0 +1,8 @@
+return {
+    "m4xshen/hardtime.nvim",
+    event = "VeryLazy",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+        require("hardtime").setup()
+    end
+}
