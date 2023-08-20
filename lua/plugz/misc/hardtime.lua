5bbed1b52172833a12d61c63e1f6a7dc887bccc2
Shekhov Ismoil <veyxov@gmail.com>
2023-08-20 22:43:34 +0500

    style: formatt

--- lua/plugz/misc/hardtime.lua
+++ lua/plugz/misc/hardtime.lua
@@ -1,8 +1,6 @@
 return {
-    "m4xshen/hardtime.nvim",
-    event = "VeryLazy",
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    config = function()
-        require("hardtime").setup()
-    end
+    'm4xshen/hardtime.nvim',
+    event = 'VeryLazy',
+    dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+    config = function() require('hardtime').setup() end,
 }
