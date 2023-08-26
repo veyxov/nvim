223c30801107a5f8a86609ca68e7d2c2ebb65902
Shekhov Ismoil <veyxov@gmail.com>
2023-08-26 11:00:31 +0500

    zen: cleanup

--- lua/plugz/ui/zen.lua
+++ lua/plugz/ui/zen.lua
@@ -1,30 +1,18 @@
 local M = {
-    {
-        'folke/zen-mode.nvim',
-        keys = {
-            {
-                '<leader>c',
-                function() require('zen-mode').toggle {} end,
-            },
+    'folke/zen-mode.nvim',
+    keys = {
+        {
+            '<leader>c',
+            function() require('zen-mode').toggle {} end,
         },
     },
 }
-
-local function toggle_fullscreen()
-    local cmd = 'swaymsg'
-    local args = { 'fullscreen' }
-    local on_exit = nil
-
-    vim.loop.spawn(cmd, { args = args }, on_exit)
-end
 
 M.config = function()
     require('zen-mode').setup {
         window = {
             width = 0.69,
-        },
-        on_open = function(_) toggle_fullscreen() end,
-        on_close = function() toggle_fullscreen() end,
+        }
     }
 end
 
