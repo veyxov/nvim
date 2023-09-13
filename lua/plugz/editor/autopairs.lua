4fd5cb4b04f13523e8b43d4a9eac3d41201271dd
Shekhov Ismoil <veyxov@gmail.com>
2023-09-13 20:44:14 +0500

    autopairs: fix backspace

--- lua/plugz/editor/autopairs.lua
+++ lua/plugz/editor/autopairs.lua
@@ -3,6 +3,9 @@ return {
     event = { 'InsertEnter', 'CmdlineEnter' },
     branch = 'v0.6',
     opts = {
-
+        bs = {
+            map = '<C-h>',
+            cmap = '<C-h>',
+        }
     },
 }
