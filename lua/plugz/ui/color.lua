7426f659f9c203f1f62ae95f90a6199eae118be1
Shekhov Ismoil <veyxov@gmail.com>
2023-09-15 16:29:00 +0500

    color: install bamboo

--- lua/plugz/ui/color.lua
+++ lua/plugz/ui/color.lua
@@ -237,7 +237,7 @@ local Nordic = {
 
 local Github = {
     'projekt0n/github-nvim-theme',
-    lazy = false,  -- make sure we load this during startup if it is your main colorscheme
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
         require('github-theme').setup()
@@ -246,4 +246,17 @@ local Github = {
     end,
 }
 
-return { Gruvbox }
+local Bamboo = {
+    -- Using lazy.nvim
+    'ribru17/bamboo.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require('bamboo').setup {
+            -- optional configuration here
+        }
+        require('bamboo').load()
+    end,
+}
+
+return { Bamboo }
