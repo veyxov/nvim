5abeb24b01a41eaed1d3f7c206586f3829debb18
Shekhov Ismoil <veyxov@gmail.com>
2023-10-02 21:24:26 +0500

    lsp: added new style mappings

--- lua/plugz/lsp/lsp.lua
+++ lua/plugz/lsp/lsp.lua
@@ -81,9 +81,12 @@ function M.config()
     )
 
     map('<leader>ca', vim.lsp.buf.code_action, { 'n', 'v' })
-    map('yt', vim.lsp.buf.code_action, { 'n', 'v' })
     map('<leader>f', vim.lsp.buf.format)
+
+    map('ya', vim.lsp.buf.code_action, { 'n', 'v' })
+    map('yt', vim.lsp.buf.code_action, { 'n', 'v' })
     map('yr', vim.lsp.buf.rename) -- TODO
+
     map('<C-E>', vim.lsp.buf.signature_help, 'i')
 
     map('<leader>lss', "<cmd> echo 'starting lsp server...' | LspStart<cr>")
