2ba6bdcb89a68e10cde744579cb1ec19e883d4e8
Shekhov Ismoil <veyxov@gmail.com>
2023-10-06 22:10:39 +0500

    flash: added match2d mode

--- lua/plugz/editor/flash.lua
+++ lua/plugz/editor/flash.lua
@@ -33,13 +33,61 @@ local M = {
             desc = 'Treesitter Search',
         },
         {
+            'rs',
+            function()
+                local Flash = require("flash")
+
+---@param opts Flash.Format
+local function format(opts)
+  -- always show first and second label
+  return {
+    { opts.match.label1, "FlashMatch" },
+    { opts.match.label2, "FlashLabel" },
+  }
+end
+
+Flash.jump({
+  search = { mode = "search" },
+  label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
+  pattern = [[\<]],
+  action = function(match, state)
+    state:hide()
+    Flash.jump({
+      search = { max_length = 0 },
+      highlight = { matches = false },
+      label = { format = format },
+      matcher = function(win)
+        -- limit matches to the current label
+        return vim.tbl_filter(function(m)
+          return m.label == match.label and m.win == win
+        end, state.results)
+      end,
+      labeler = function(matches)
+        for _, m in ipairs(matches) do
+          m.label = m.label2 -- use the second label
+        end
+      end,
+    })
+  end,
+  labeler = function(matches, state)
+    local labels = state:labels()
+    for m, match in ipairs(matches) do
+      match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+      match.label2 = labels[(m - 1) % #labels + 1]
+      match.label = match.label1
+    end
+  end,
+})
+            end
+        },
+        {
             -- Show diagnostic details
             '<leader>dd',
             function()
                 require('flash').jump {
                     matcher = function(win)
                         return vim.tbl_map(
-                            ---@param diag Diagnostic
+                        ---@param diag Diagnostic
                             function(diag)
                                 return {
                                     pos = { diag.lnum + 1, diag.col },
