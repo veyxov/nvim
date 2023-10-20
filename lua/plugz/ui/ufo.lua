99269aae2205d52e74b5833df3451b659ff6fddb
Shekhov Ismoil <veyxov@gmail.com>
2023-10-20 22:13:51 +0500

    ufo: organize mappings

--- lua/plugz/ui/ufo.lua
+++ lua/plugz/ui/ufo.lua
@@ -9,8 +9,12 @@ return {
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
 
-        vim.keymap.set('n', '<leader>fa', require('ufo').openAllFolds)
+        vim.keymap.set('n', '<leader>fa', "za")
         vim.keymap.set('n', '<leader>fl', require('ufo').closeAllFolds)
+        vim.keymap.set('n', '<leader>fn', require('ufo').goNextClosedFold)
+        vim.keymap.set('n', '<leader>fx', "zE")
+        vim.keymap.set('n', '<leader>ff', "zO")
+        vim.keymap.set('n', '<leader>fe', "zC")
         require('ufo').setup({
             provider_selector = function(bufnr, filetype, buftype)
                 return { 'treesitter', 'indent' }
