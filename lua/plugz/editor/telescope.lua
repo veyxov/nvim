324d8434e92a19db74ce71d686b810a123fd074b
Shekhov Ismoil <veyxov@gmail.com>
2023-10-24 09:36:35 +0500

    telescope: cleeeeeeeaaaanup!

--- lua/plugz/editor/telescope.lua
+++ lua/plugz/editor/telescope.lua
@@ -21,27 +21,6 @@ function M.config()
     telescope.load_extension 'fzf'
 end
 
-local function live_grep_from_project_git_root()
-    local function is_git_repo()
-        vim.fn.system 'git rev-parse --is-inside-work-tree'
-
-        return vim.v.shell_error == 0
-    end
-
-    local function get_git_root()
-        local dot_git_path = vim.fn.finddir('.git', '.;')
-        return vim.fn.fnamemodify(dot_git_path, ':h')
-    end
-
-    local opts = {}
-
-    if is_git_repo() then opts = {
-        cwd = get_git_root(),
-    } end
-
-    require('telescope.builtin').live_grep(opts)
-end
-
 M.keys = {
     {
         '<leader>?',
@@ -57,10 +36,6 @@ M.keys = {
     },
     { '<leader>gc', '<cmd>Telescope grep_string theme=ivy<cr>' },
     {
-        '<leader>G',
-        function() live_grep_from_project_git_root() end,
-    },
-    {
         '<leader>gg',
         function() require('telescope.builtin').live_grep() end,
     },
@@ -68,7 +43,6 @@ M.keys = {
         '<leader>/',
         '<cmd>Telescope current_buffer_fuzzy_find theme=get_dropdown previewer=false<cr>',
     },
-    { 'jh', '<cmd>Telescope git_branches<cr>' },
 }
 
 return M
