64491172539e934407a259d253490b0cd3610c3d
Shekhov Ismoil <veyxov@gmail.com>
2023-10-24 10:05:44 +0500

    ufo: set a starting fold option, hide deep nested code on startup

--- lua/plugz/ui/ufo.lua
+++ lua/plugz/ui/ufo.lua
@@ -6,16 +6,13 @@ return {
     config = function()
         vim.o.foldcolumn = '0'
         vim.o.foldlevel = 99
-        vim.o.foldlevelstart = 99
+        vim.o.foldlevelstart = 3
         vim.o.foldenable = true
 
         vim.keymap.set('n', '<leader>fa', 'za')
-        vim.keymap.set('n', '<leader>ff', require('ufo').openAllFolds)
-        vim.keymap.set('n', '<leader>fl', require('ufo').closeAllFolds)
+        vim.keymap.set('n', '<leader>fo', require('ufo').openAllFolds)
+        vim.keymap.set('n', '<leader>fc', require('ufo').closeAllFolds)
         vim.keymap.set('n', '<leader>fn', require('ufo').goNextClosedFold)
-        vim.keymap.set('n', '<leader>fx', 'zE')
-        vim.keymap.set('n', '<leader>ff', 'zO')
-        vim.keymap.set('n', '<leader>fe', 'zC')
         require('ufo').setup {
             provider_selector = function(bufnr, filetype, buftype)
                 return { 'treesitter', 'indent' }
