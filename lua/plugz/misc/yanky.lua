74779065070afd8fc56da7f843820aaf396c02c7
Shekhov Ismoil <veyxov@gmail.com>
2023-10-25 09:30:58 +0500

    yanky: cleanup

--- lua/plugz/misc/yanky.lua
+++ lua/plugz/misc/yanky.lua
@@ -50,51 +50,5 @@ return {
             mode = nx,
         },
     },
-    config = function()
-        local utils = require 'yanky.utils'
-        local mapping = require 'yanky.telescope.mapping'
-
-        require('yanky').setup {
-            ring = {
-                history_length = 100,
-                storage = 'shada',
-                sync_with_numbered_registers = false,
-                cancel_event = 'update',
-            },
-            picker = {
-                select = {
-                    action = nil, -- nil to use default put action
-                },
-                telescope = {
-                    mappings = {
-                        default = mapping.put 'p',
-                        i = {
-                            ['<c-p>'] = mapping.put 'p',
-                            ['<c-k>'] = mapping.put 'P',
-                            ['<c-x>'] = mapping.delete(),
-                            ['<c-r>'] = mapping.set_register(
-                                utils.get_default_register()
-                            ),
-                        },
-                        n = {
-                            p = mapping.put 'p',
-                            P = mapping.put 'P',
-                            d = mapping.delete(),
-                        },
-                    },
-                },
-            },
-            system_clipboard = {
-                sync_with_ring = true,
-            },
-            highlight = {
-                on_put = true,
-                on_yank = true,
-                timer = 200,
-            },
-            preserve_cursor_position = {
-                enabled = true,
-            },
-        }
-    end,
+    opts = {}
 }
