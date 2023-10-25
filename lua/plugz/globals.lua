eb2a7b9876939fc1f06b16035eafe42a5945b621
Shekhov Ismoil <veyxov@gmail.com>
2023-10-25 11:38:40 +0500

    globals: move to seperate file

---
+++ lua/plugz/globals.lua
@@ -0,0 +1,7 @@
+return {
+    { 'nvim-lua/plenary.nvim' },
+    {
+        'nvim-tree/nvim-web-devicons',
+        config = function() require('nvim-web-devicons').setup {} end,
+    },
+}
--- lua/plugz/init.lua
+++ lua/plugz/init.lua
@@ -1,13 +1,8 @@
 return {
-    { 'nvim-lua/plenary.nvim' },
     { import = 'plugz.git' },
     { import = 'plugz.ui' },
     { import = 'plugz.tree-sitter' },
     { import = 'plugz.editor' },
     { import = 'plugz.misc' },
     { import = 'plugz.lsp' },
-    {
-        'nvim-tree/nvim-web-devicons',
-        config = function() require('nvim-web-devicons').setup {} end,
-    },
 }
