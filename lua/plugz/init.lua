38ba37d71da4e06245443fcb1d2c2d8d51aa1c99
Shekhov Ismoil <veyxov@gmail.com>
2023-10-28 19:43:24 +0500

    global: fix imports

--- lua/plugz/init.lua
+++ lua/plugz/init.lua
@@ -1,9 +1,3 @@
 return {
     { import = 'plugz.standard' },
-    { import = 'plugz.git' },
-    { import = 'plugz.ui' },
-    { import = 'plugz.tree-sitter' },
-    { import = 'plugz.editor' },
-    { import = 'plugz.misc' },
-    { import = 'plugz.lsp' },
 }
