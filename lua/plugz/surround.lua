0c84f32ff905050377a6377ed0b04db53afd35b3
Shekhov Ismoil <veyxov@gmail.com>
2023-11-10 11:03:10 +0500

    surround: fix mapping in visual mode

--- lua/plugz/surround.lua
+++ lua/plugz/surround.lua
@@ -1,6 +1,6 @@
 local M = {
     'kylechui/nvim-surround',
-    keys = { { 'l', mode = 'n' }, 'll', 'cs', 'ds', { 'l', mode = 'v' } },
+    keys = { {'L', mode = 'v'}, { 'l', mode = 'n' }, 'll', 'cs', 'ds', { 'l', mode = 'v' } },
 }
 
 M.config = function()
