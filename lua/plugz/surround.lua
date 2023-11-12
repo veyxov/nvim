dbd6f86c319ccd5c1820f26163662d3f87a148ce
Shekhov Ismoil <veyxov@gmail.com>
2023-11-12 21:11:21 +0500

    surround: fix current line line-surround binding

--- lua/plugz/surround.lua
+++ lua/plugz/surround.lua
@@ -1,6 +1,6 @@
 local M = {
     'kylechui/nvim-surround',
-    keys = { {'L', mode = 'v'}, { 'l', mode = 'n' }, 'll', 'cs', 'ds', { 'l', mode = 'v' } },
+    keys = { 'L', { 'l', mode = 'n' }, 'll', 'cs', 'ds', { 'l', mode = 'v' } },
 }
 
 M.config = function()
@@ -10,7 +10,7 @@ M.config = function()
             normal = 'l',
             normal_cur = 'll',
             normal_line = 'L',
-            normal_cur_line = 'Ll',
+            normal_cur_line = 'LL',
             visual = 'l',
             visual_line = 'L',
             delete = 'ds',
