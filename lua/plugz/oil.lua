dace70e114ceca95829931fe182658a85d4d5645
Shekhov Ismoil <veyxov@gmail.com>
2023-11-18 21:26:31 +0500

    oli: refactor the config to use `opts`
    - also clean up the config, remove unneccessary configuration fields

--- lua/plugz/oil.lua
+++ lua/plugz/oil.lua
@@ -6,43 +6,12 @@ local M = {
             function() require('oil').open() end,
         },
     },
-}
-
-M.config = function()
-    require('oil').setup {
-        columns = {
-            'icon',
-            'size',
-        },
-        win_options = {
-            conceallevel = 3,
-            concealcursor = 'in',
-        },
+    opts = {
         skip_confirm_for_simple_edits = true,
         keymaps = {
-            ['t'] = 'actions.select',
-            ['<C-i>'] = 'actions.select_vsplit',
-            ['<C-e>'] = 'actions.select_split',
-            ['<C-t>'] = 'actions.select_tab',
-            ['<C-p>'] = 'actions.preview',
-            ['n'] = 'actions.parent',
-            ['qu'] = 'actions.close',
-            ['<C-h>'] = 'actions.close',
-            ['<C-l>'] = 'actions.refresh',
-            --["h"] = "actions.open_cwd",
-            ['cd'] = 'actions.cd',
-            ['~'] = 'actions.tcd',
-            ['.'] = 'actions.toggle_hidden',
-            ['yy'] = 'actions.copy_entry_path',
-        },
-        float = {
-            padding = 3,
-            border = 'single',
-            win_options = {
-                winblend = 5,
-            },
-        },
+            ["<C-h>"] = "actions.close",
+        }
     }
-end
+}
 
 return M
