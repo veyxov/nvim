d55cecf7193dbd43ad6f7efaae3bda135f1e7181
Shekhov Ismoil <veyxov@gmail.com>
2024-01-18 09:13:56 +0500

    cleanup: remove unused

--- lua/plugz/monkey.lua
+++
@@ -1,9 +0,0 @@
-return {
-    lazy = false,
-    "https://github.com/atusy/treemonkey.nvim",
-    init = function()
-        vim.keymap.set({ "x", "o" }, "m", function()
-            require("treemonkey").select({ ignore_injections = false })
-        end)
-    end
-}
--- lua/plugz/spectre.lua
+++
@@ -1,6 +0,0 @@
-return {
-    'nvim-pack/nvim-spectre',
-    keys = {
-        { '<leader>sr', function() require('spectre').open() end },
-    },
-}
