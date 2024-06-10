701bf288c88b40b3050789c8d0feb7cac177a147
Shekhov Ismoil <veyxov@gmail.com>
2024-06-10 14:32:14 +0500

    grugFar: decided to stick with the default mappings

--- lua/plugz/replace.lua
+++ lua/plugz/replace.lua
@@ -3,19 +3,6 @@ return {
     config = function()
         require('grug-far').setup({
             windowCreationCommand = 'tabnew %',
-            keymaps = {
-                replace = { n = '<localleader>r' },
-                qflist = { n = '<localleader>q' },
-                syncLocations = { n = '<localleader>s' },
-                syncLine = { n = '<localleader>l' },
-                close = { n = '<localleader>c' },
-                historyOpen = { n = '<localleader>t' },
-                historyAdd = { n = '<localleader>a' },
-                refresh = { n = '<localleader>f' },
-                gotoLocation = { n = '<enter>' },
-                pickHistoryEntry = { n = '<enter>' },
-                abort = { n = '<localleader>b' },
-            }
         })
     end,
     keys = {
