215044c3f9926545d61843320e8c8dd2dcadc3d6
Shekhov Ismoil <veyxov@gmail.com>
2024-11-12 10:01:10 +0500

    compact

--- init.lua
+++ init.lua
@@ -1,30 +1,20 @@
 -- The Big Bang
 local o = vim.opt
-local g = vim.g
-g.mapleader = ' '
-g.editorconfig = false
-o.wrap = false
-o.list = true
-o.listchars = { trail = '·', nbsp = '␣', tab = '->' }
-o.swapfile = false
-o.cmdheight = 0
-o.laststatus = 3
-o.ignorecase = true
-o.smartcase = true
-o.updatetime = 250
-o.timeoutlen = 300
+vim.g.mapleader = ' '
 
-local tabs = 4
-o.expandtab = true
-o.tabstop = tabs
-o.shiftwidth = tabs
-o.softtabstop = tabs
+o.list = true;o.listchars = { trail = '·', nbsp = '␣', tab = '->' }
+o.swapfile = false
+o.wrap = false
+o.cmdheight = 0;o.laststatus = 3
+o.ignorecase = true;o.smartcase = true
+local tabs = 4;o.expandtab = true;o.tabstop = tabs;o.shiftwidth = tabs;o.softtabstop = tabs
 
 local GROUP = vim.api.nvim_create_augroup('VEYXOV_GROUP', {})
 local function autocmd(event, callback)
     vim.api.nvim_create_autocmd(event, { callback = callback, group = GROUP })
 end
 autocmd('TextYankPost', function() vim.highlight.on_yank() end)
+
 -- Go to last loc when opening a buffer
 autocmd('BufReadPost', function()
     local mark = vim.api.nvim_buf_get_mark(0, '"')
@@ -34,26 +24,12 @@ autocmd('BufReadPost', function()
 end)
 
 local map = require('globals').Map
-map('ge', '<cmd>lua vim.diagnostic.goto_next()<cr>')
-map('<leader>s', '<cmd>wall<cr>') -- Save file
--- Quitting made easy
-map('<C-H>', '<cmd>q<cr>') -- fast quit
-map('qu', '<cmd>wq<cr>')
-map('<leader>q', '<cmd>qall!<cr>')
--- Window navigation
-map('<C-Down>', '<C-w><Down>')
-map('<C-Left>', '<C-w><Left>')
-map('<C-Right>', '<C-w><Right>')
-map('<C-Up>', '<C-w><Up>')
--- Add undo break-points
-map('<leader>v', '<C-V>') -- Visual block mode
--- better indenting
-map('<Esc>', ':nohl<CR>:echo<CR>') -- Clear
+map('<leader>s', '<cmd>wall<cr>')
+map('<C-H>', '<cmd>q<cr>');map('qu', '<cmd>wq<cr>');map('<leader>q', '<cmd>qall!<cr>')
+map('<C-Down>', '<C-w><Down>');map('<C-Left>', '<C-w><Left>');map('<C-Right>', '<C-w><Right>');map('<C-Up>', '<C-w><Up>')
+map('<leader>v', '<C-V>')
+map('<Esc>', ':nohl<CR>:echo<CR>')
 
--- Quickfix mappings
 map('<leader>n', '<cmd>cnext<cr>zz')
--- switch between multiple quickfix lists
-map('<leader>co', ':colder<cr>zz')
-map('<leader>cn', ':cnewer<cr>zz')
 
 require 'lazyopt'
--- lua/lazyopt.lua
+++ lua/lazyopt.lua
@@ -1,12 +1,10 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system {
-        'git', 'clone', '--filter=blob:none', '--single-branch',
-        'https://github.com/folke/lazy.nvim.git',
+        'git', 'clone', '--filter=blob:none', '--single-branch', 'https://github.com/folke/lazy.nvim.git',
         lazypath
     }
 end
-
 vim.opt.runtimepath:prepend(lazypath)
 
 require('lazy').setup('plugz', {
