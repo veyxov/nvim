697610391d82e1597c84f7621665206933c96608
Shekhov Ismoil <veyxov@gmail.com>
2024-11-22 09:25:14 +0500

    feat: treesitter objects, swap and move

--- lua/plugz/objects.lua
+++ lua/plugz/objects.lua
@@ -5,6 +5,10 @@ return {
         'nvim-treesitter/nvim-treesitter'
     },
     config = function()
+        local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+
+        vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+        vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
         require'nvim-treesitter.configs'.setup {
             textobjects = {
                 select = {
@@ -15,6 +19,35 @@ return {
                         ["aa"] = "@parameter.inner",
                         ["ia"] = "@parameter.inner",
                         ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+                    },
+                },
+                swap = {
+                    enable = true,
+                    swap_next = {
+                        ["<leader>xa"] = "@parameter.inner",
+                        ["<leader>xf"] = "@function.outer",
+                        ["<leader>xc"] = "@call.outer",
+                    },
+                    swap_previous = {
+                        ["<leader>xA"] = "@parameter.inner",
+                        ["<leader>xF"] = "@function.outer",
+                    },
+                },
+
+                move = {
+                    enable = true,
+                    set_jumps = true,
+                    goto_next_start = {
+                        ["gl"] = "@function.outer",
+                    },
+                    goto_next_end = {
+                        ["gL"] = "@function.outer",
+                    },
+                    goto_next = {
+                        ["]d"] = "@conditional.outer",
+                    },
+                    goto_previous = {
+                        ["[d"] = "@conditional.outer",
                     },
                 },
             },
