640cde467992b79d65e718b9ffe38484919c2425
Shekhov Ismoil <veyxov@gmail.com>
2025-06-27 10:29:25 +0500

    snacks: re-itteration

--- lua/plugz/snacks.lua
+++ lua/plugz/snacks.lua
@@ -10,35 +10,35 @@ return {
         },
         bigfile = { enabled = true },
         dashboard = {
-            sections = {
-                { section = "header" },
-                {
-                    pane = 2,
-                    section = "terminal",
-                    cmd = "colorscript -e square",
-                    height = 5,
-                    padding = 1,
-                },
-                { section = "keys", gap = 1, padding = 1 },
-                { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-                { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-                {
-                    pane = 2,
-                    icon = " ",
-                    title = "Git Status",
-                    section = "terminal",
-                    enabled = function()
-                        return Snacks.git.get_root() ~= nil
-                    end,
-                    cmd = "git status --short --branch --renames",
-                    height = 5,
-                    padding = 1,
-                    ttl = 5 * 60,
-                    indent = 3,
-                },
-                { section = "startup" },
-            },
-        },
+  sections = {
+    { section = "header" },
+    {
+      pane = 2,
+      section = "terminal",
+      cmd = "square",
+      height = 5,
+      padding = 1,
+    },
+    { section = "keys", gap = 1, padding = 1 },
+    { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+    { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+    {
+      pane = 2,
+      icon = " ",
+      title = "Git Status",
+      section = "terminal",
+      enabled = function()
+        return Snacks.git.get_root() ~= nil
+      end,
+      cmd = "git status --short --branch --renames",
+      height = 5,
+      padding = 1,
+      ttl = 5 * 60,
+      indent = 3,
+    },
+    { section = "startup" },
+  },
+},
         indent = { enabled = true },
         input = { enabled = true },
         notifier = {
@@ -60,7 +60,7 @@ return {
 
         { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
         { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
-        { "<leader>cR", function() Snacks.rename.rename_file() end, desc = "Rename File" },
+        { "<leader>cr", function() Snacks.rename.rename_file() end, desc = "Rename File" },
         { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
         { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
         { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
@@ -86,10 +86,10 @@ return {
 
                 -- Create some toggle mappings
                 Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-                Snacks.toggle.diagnostics():map("<leader>ud")
-                Snacks.toggle.inlay_hints():map("<leader>uh")
+                Snacks.toggle.diagnostics():map("<leader>uD")
+                Snacks.toggle.inlay_hints():map("<leader>ua")
                 Snacks.toggle.indent():map("<leader>ug")
-                Snacks.toggle.dim():map("<leader>uD")
+                Snacks.toggle.dim():map("<leader>ud")
             end,
         })
     end,
