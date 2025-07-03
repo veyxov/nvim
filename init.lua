7b69ce82ae82ae2c6abbc6a8ed8ebae8c78db52a
Shekhov Ismoil <veyxov@gmail.com>
2025-07-03 09:18:22 +0500

    fix old keyboard mappings

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
--- The Big Bang
 local o = vim.opt
 vim.g.mapleader = ' '
 
@@ -25,7 +24,7 @@ end)
 
 local map = require('globals').Map
 map('<leader>s', '<cmd>wall<cr>')
-map('<C-H>', '<cmd>q<cr>');map('qu', '<cmd>wq<cr>');map('<leader>q', '<cmd>qall!<cr>')
+map('<Backspace>', '<cmd>q<cr>');map('qu', '<cmd>wq<cr>');map('<leader>q', '<cmd>qall!<cr>')
 map('<C-Down>', '<C-w><Down>');map('<C-Left>', '<C-w><Left>');map('<C-Right>', '<C-w><Right>');map('<C-Up>', '<C-w><Up>')
 map('<leader>v', '<C-V>')
 map('<Esc>', ':nohl<CR>:echo<CR>')
