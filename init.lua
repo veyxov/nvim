5950516290fe510f5318b3e8822b399c25f19d32
Shekhov Ismoil <veyxov@gmail.com>
2025-08-15 12:03:49 +0500

    init: cleanup

--- init.lua
+++ init.lua
@@ -1,25 +1,15 @@
 local o = vim.opt
 vim.g.mapleader = ' '
 
-o.list = true;o.listchars = { trail = '·', nbsp = '␣', tab = '->' }
 o.swapfile = false
-o.wrap = false
-o.cmdheight = 0;o.laststatus = 0
+o.cmdheight = 0; o.laststatus = 3
 o.ignorecase = true;o.smartcase = true
 local tabs = 4;o.expandtab = true;o.tabstop = tabs;o.shiftwidth = tabs;o.softtabstop = tabs
 
-local GROUP = vim.api.nvim_create_augroup('VEYXOV_GROUP', {})
 local function autocmd(event, callback)
-    vim.api.nvim_create_autocmd(event, { callback = callback, group = GROUP })
+    vim.api.nvim_create_autocmd(event, { callback = callback, group = vim.api.nvim_create_augroup('xyz', {}) })
 end
 autocmd('TextYankPost', function() vim.highlight.on_yank() end)
--- Go to last loc when opening a buffer
-autocmd('BufReadPost', function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    if mark[1] > 0 and mark[1] <= vim.api.nvim_buf_line_count(0) then
-        pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-end)
 
 local map = require('globals').Map
 map('<leader>s', '<cmd>wall<cr>')
