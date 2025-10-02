31bfec71deb5bbcfe27020564be55d2d04f78ec1
Shekhov Ismoil <veyxov@gmail.com>
2025-10-02 10:50:41 +0500

    Revert "cleanup"
    This reverts commit bdf587460d0c00be84a793a6008ee48e285fa358.

---
+++ lua/plugz/context.lua
@@ -0,0 +1,13 @@
+return {
+    'nvim-treesitter/nvim-treesitter-context',
+    event = 'VeryLazy',
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter'
+    },
+    config = function()
+        require'treesitter-context'.setup {
+            multiline_threshold = 1, -- don't show every constructor parameter
+            mode = 'topline', -- too noisy when moving cursor
+        }
+    end
+}
