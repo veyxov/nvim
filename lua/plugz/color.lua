42308c5cbe230c671c6df07afd8f6c6988993ec6
Shekhov Ismoil <veyxov@gmail.com>
2025-11-05 10:46:20 +0500

    colors: add tokyonight

--- lua/plugz/color.lua
+++ lua/plugz/color.lua
@@ -1,4 +1,4 @@
-return {
+local gruvbox = {
       'sainnhe/gruvbox-material',
       lazy = false,
       priority = 1000,
@@ -7,3 +7,25 @@ return {
         vim.cmd.colorscheme 'gruvbox-material'
       end
     }
+
+local catppuccin = { 
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    config = function()
+        vim.cmd.colorscheme 'catppuccin'
+    end
+}
+
+local tokyonight = {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+      vim.cmd.colorscheme 'tokyonight'
+  end
+}
+
+return tokyonight
