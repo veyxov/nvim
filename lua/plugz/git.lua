4fa980e29f57823c898f727968a07ad622fe1b7a
Shekhov Ismoil <veyxov@gmail.com>
2025-11-05 10:51:28 +0500

    git: fix quitting using 'qu' on Neogit

--- lua/plugz/git.lua
+++ lua/plugz/git.lua
@@ -74,8 +74,23 @@ local neogit = {
     },
     cmd = { "Neogit" },
     opts = {
+        disable_hint = true,
         graph_style = "kitty",
-    },
+        integrations = {
+            diffview = true,
+            telescope = true
+        },
+
+        commit_editor = {
+            ["qu"]         = "Close",
+            ["<c-c><c-c>"] = "Submit",
+            ["<c-c><c-k>"] = "Abort",
+        },
+        commit_editor_I = {
+            ["<c-c><c-c>"] = "Submit",
+            ["<c-c><c-k>"] = "Abort",
+        }
+    }
 }
 
 return {
