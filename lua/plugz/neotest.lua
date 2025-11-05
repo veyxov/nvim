029bf59694cafbe3e118b2265c989913a4c59d5f
Shekhov Ismoil <veyxov@gmail.com>
2025-11-05 10:29:10 +0500

    cleanup: remove inline-diagnostics

--- lua/plugz/neotest.lua
+++ lua/plugz/neotest.lua
@@ -4,18 +4,6 @@ return {
     dependencies = {
         "nsidorenco/neotest-vstest",
         "nvim-neotest/nvim-nio",
-        "nvim-lua/plenary.nvim",
-        "antoinemadec/FixCursorHold.nvim",
-        "nvim-treesitter/nvim-treesitter",
-        {
-            "rachartier/tiny-inline-diagnostic.nvim",
-            event = "VeryLazy", -- Or `LspAttach`
-            priority = 1000, -- needs to be loaded in first
-            config = function()
-                require('tiny-inline-diagnostic').setup()
-                vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
-            end
-        }
     },
     config = function()
         require("neotest").setup({
