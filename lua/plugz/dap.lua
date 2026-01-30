ac77c1046f7fead65027a75600b096d1eb757a77
Shekhov Ismoil <veyxov@gmail.com>
2026-01-30 17:27:40 +0500

    dap: configure nvim-dap

--- lua/plugz/dap.lua
+++ lua/plugz/dap.lua
@@ -1,37 +1,51 @@
 return {
-  "mfussenegger/nvim-dap",
-  lazy = false,
+  'mfussenegger/nvim-dap',
   dependencies = {
-    "rcarriga/nvim-dap-ui",
-    "nvim-neotest/nvim-nio", 
+    'rcarriga/nvim-dap-ui',
+    'nvim-neotest/nvim-nio',
+  },
+  keys = {
+    -- Basic debugging keymaps, feel free to change to your liking!
+    { 'hs', function() require('dap').continue() end, desc = 'Debug: Start/Continue' },
+    { 'hi', function() require('dap').step_into() end, desc = 'Debug: Step Into' },
+    { 'hn', function() require('dap').step_over() end, desc = 'Debug: Step Over' },
+    { 'ho', function() require('dap').step_out() end, desc = 'Debug: Step Out' },
+    { 'ht', function() require('dap').toggle_breakpoint() end, desc = 'Debug: Toggle Breakpoint' },
+    { 'hf', function() require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ') end, desc = 'Debug: Set Breakpoint' },
+    { 'hd', function() require('dap.ui.widgets').hover() end, desc = 'Debug: Debug' },
+    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+    {
+      'hl',
+      function() require('dapui').toggle() end,
+      desc = 'Debug: See last session result.',
+    },
   },
   config = function()
-    local dap = require("dap")
-    local dapui = require("dapui")
-    local widgets = require("dap.ui.widgets") -- Import widgets
+    local dap = require 'dap'
+    local dapui = require 'dapui'
 
-    dapui.setup()
+    dapui.setup {
+      -- Set icons to characters that are more likely to work in every terminal.
+      --    Feel free to remove or use ones that you like more! :)
+      --    Don't feel like these are good choices.
+      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      controls = {
+        icons = {
+          pause = '⏸',
+          play = '▶',
+          step_into = '⏎',
+          step_over = '⏭',
+          step_out = '⏮',
+          step_back = 'b',
+          run_last = '▶▶',
+          terminate = '⏹',
+          disconnect = '⏏',
+        },
+      },
+    }
 
-    -- Listeners to open UI automatically
-    dap.listeners.before.attach.dapui_config = function() dapui.open() end
-    dap.listeners.before.launch.dapui_config = function() dapui.open() end
-    dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
-    dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
-
-    -- Existing keymaps
-    vim.keymap.set("n", "<leader>dQ", function() dap.terminate(); dap.clear_breakpoints() end, { desc = "DAP: Terminate" })
-    vim.keymap.set("n", "<leader>ds", dap.continue, { desc = "Start/continue" })
-    vim.keymap.set("n", "<leader>dc", dap.step_over, { desc = "Step over" })
-    vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Step into" })
-    vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "Step out" })
-    vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
-    vim.keymap.set("n", "<leader>dr", dap.repl.toggle, { desc = "Toggle REPL" })
-    vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Toggle DAP UI" })
-
-    -- THE NEW KEYMAP FOR FLOATING WINDOW
-    vim.keymap.set({"n", "v"}, "<leader>dk", function()
-      widgets.hover()
-    end, { desc = "DAP: Hover variable" })
-
+    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    dap.listeners.before.event_exited['dapui_config'] = dapui.close
   end,
 }
