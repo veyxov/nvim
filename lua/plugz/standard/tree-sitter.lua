579b24b6c82a4eb4e6eeee1a5ed9e9804e30da4e
Shekhov Ismoil <veyxov@gmail.com>
2026-02-04 09:59:08 +0500

    tree-sitter: move to the main branch

--- lua/plugz/standard/tree-sitter.lua
+++ lua/plugz/standard/tree-sitter.lua
@@ -1,22 +1,13 @@
-local M = {
+return {
     'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
     lazy = false,
+    build = ':TSUpdate',
     config = function()
-        require'nvim-treesitter.configs'.setup {
-            ensure_installed = {"lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-            sync_install = false,
-            highlight = {
-                enable = true,
-                disable = function(lang, buf)
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
-                    end
-                end,
-            },
-        }
-    end
+        local filetypes = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'c_sharp', 'json' }
+        require('nvim-treesitter').install(filetypes)
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = filetypes,
+            callback = function() vim.treesitter.start() end,
+        })
+    end,
 }
-return M
