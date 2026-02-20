46ae3323d06fbdf39355ad05537c0010730938cf
Shekhov Ismoil <veyxov@gmail.com>
2026-02-20 17:14:15 +0500

    tree-sitter: add http

--- lua/plugz/standard/tree-sitter.lua
+++ lua/plugz/standard/tree-sitter.lua
@@ -15,7 +15,7 @@ local tree = {
     lazy = false,
     build = ':TSUpdate',
     config = function()
-        local filetypes = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'c_sharp', 'json' }
+        local filetypes = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'c_sharp', 'json', 'http' }
         require('nvim-treesitter').install(filetypes)
         vim.api.nvim_create_autocmd('FileType', {
             pattern = filetypes,
