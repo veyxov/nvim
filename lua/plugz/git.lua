186390c2e7882ba09d9b70948a4675fa1c0e19eb
Shekhov Ismoil <veyxov@gmail.com>
2026-03-02 15:59:33 +0500

    git: merge neogit and other git plugins

--- lua/plugz/git.lua
+++ lua/plugz/git.lua
@@ -50,9 +50,50 @@ local diff = {
     end
 }
 
+local neogit = {
+    "NeogitOrg/neogit",
+    keys = {
+        {
+            "<leader>ga",
+            function()
+                require("neogit").action("commit", "commit")()
+            end,
+            desc = "Git commit",
+        },
+        {
+            "<leader>gc",
+            function()
+                require("neogit").action("push", "to_upstream")()
+            end,
+            desc = "Git push",
+        },
+        {
+            "<leader>gg",
+            "<cmd>Neogit<cr>",
+            desc = "Git status",
+        },
+    },
+    cmd = { "Neogit" },
+    opts = {
+        disable_hint = true,
+        graph_style = "kitty",
+        integrations = {
+            diffview = true,
+        },
 
+        commit_editor = {
+            ["qu"]         = "Submit",
+            ["<c-c><c-k>"] = "Abort",
+        },
+        commit_editor_I = {
+            ["<c-c><c-c>"] = "Submit",
+            ["<c-c><c-k>"] = "Abort",
+        }
+    }
+}
 
 return {
     gitsigns,
-    diff
+    diff,
+    neogit
 }
