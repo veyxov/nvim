f6a55a4e7cd2b467a90f9a71f9b22277f777bdba
Shekhov Ismoil <veyxov@gmail.com>
2026-03-03 11:42:19 +0500

    dotnet: fix opening on every buf-enter

--- lua/plugz/dotnet.lua
+++ lua/plugz/dotnet.lua
@@ -1,7 +1,6 @@
 return {
     'GustavEikaas/easy-dotnet.nvim',
     ft = { 'cs', 'csharp', "c_sharp" },
-    event = "BufEnter",
     build = ':Dotnet _server update',
     config = function()
         local dotnet = require 'easy-dotnet'
