10f92e6428dbb6bf89e4135e0b5e1e03845b158c
Shekhov Ismoil <veyxov@gmail.com>
2026-03-03 11:24:35 +0500

    init: cleanup

--- init.lua
+++ init.lua
@@ -1,11 +1,19 @@
 local o = vim.opt
 vim.g.mapleader = ' '
 
+-- [[ options
 o.swapfile = false
 o.cmdheight = 0; o.laststatus = 3
 o.ignorecase = true;o.smartcase = true
 o.wrap = false
+o.updatetime = 250
+o.timeoutlen = 250
+o.scrolloff = 10
+o.undofile = true
+o.cursorline = true
+o.termguicolors = true
 local tabs = 4;o.expandtab = true;o.tabstop = tabs;o.shiftwidth = tabs;o.softtabstop = tabs
+-- ]]
 
 local function autocmd(event, callback)
     vim.api.nvim_create_autocmd(event, { callback = callback, group = vim.api.nvim_create_augroup('xyz', {}) })
@@ -14,41 +22,27 @@ autocmd('TextYankPost', function() vim.highlight.on_yank() end)
 
 local map = require('globals').Map
 map('<leader>w', '<cmd>wall<cr>')
-map('<Backspace>', '<cmd>q<cr>');map('qu', '<cmd>wq<cr>');map('<leader>q', '<cmd>qall!<cr>')
+map('<Backspace>', '<cmd>q<cr>');map('qu', '<cmd>wq<cr>')
 map('<C-Down>', '<C-w><Down>');map('<C-Left>', '<C-w><Left>');map('<C-Right>', '<C-w><Right>');map('<C-Up>', '<C-w><Up>')
 map('<leader>v', '<C-V>')
-map('<Esc>', ':nohl<CR>:echo<CR>')
+map('<Esc>', '<cmd>nohl<cr>')
 
+-- yank key y -> h
 map('h', 'y', { 'n', 'v', 'x'})
 map('hh', 'yy')
 map('H', 'y$')
-
 map('<leader>h', '"+y', {'n', 'v', 'x'})
 map('<leader>hh', '"+yy')
 
 map('<leader>n', ':cnext<cr>');
 map('<leader>p', ':cprev<cr>');
 
-require 'lazyopt'
-
 vim.diagnostic.config {
-  float = { border = 'rounded', source = 'if_many' },
-  underline = { severity = vim.diagnostic.severity.ERROR },
-  virtual_lines = true,
-  jump = { float = true },
+    float = { border = 'rounded', source = 'if_many' },
+    underline = { severity = vim.diagnostic.severity.ERROR },
+    virtual_text = false,
+    virtual_lines = { only_current_line = true },
+    jump = { float = true },
 }
 
-vim.filetype.add({
-  extension = {
-    ['http'] = 'http',
-  },
-})
-
-vim.g.loaded_gzip = true
-vim.g.loaded_matchit = true
-vim.g.loaded_netrwPlugin = true
-vim.g.loaded_tarPlugin = true
-vim.g.loaded_zipPlugin = true
-vim.g.loaded_man = true
-vim.g.loaded_2html_plugin = true
-vim.g.loaded_remote_plugins = true
+require 'lazyopt'
