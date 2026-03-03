072a1805dc6d65b1e368d683f6563f2d5d04805c
Shekhov Ismoil <veyxov@gmail.com>
2026-03-03 11:42:42 +0500

    init: cleanup

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ vim.g.mapleader = ' '
 o.swapfile = false
 o.cmdheight = 0; o.laststatus = 3
 o.ignorecase = true;o.smartcase = true
+o.hlsearch = false
 o.wrap = false
 o.updatetime = 250
 o.timeoutlen = 250
@@ -12,14 +13,23 @@ o.scrolloff = 10
 o.undofile = true
 o.cursorline = true
 o.termguicolors = true
+o.showmode = false
+o.shortmess:append("c")
 local tabs = 4;o.expandtab = true;o.tabstop = tabs;o.shiftwidth = tabs;o.softtabstop = tabs
 -- ]]
 
+-- [[ autocmds
+local g = vim.api.nvim_create_augroup('xyz', { clear = true })
 local function autocmd(event, callback)
-    vim.api.nvim_create_autocmd(event, { callback = callback, group = vim.api.nvim_create_augroup('xyz', {}) })
+  vim.api.nvim_create_autocmd(event, {
+    callback = callback,
+    group = g,
+  })
 end
 autocmd('TextYankPost', function() vim.highlight.on_yank() end)
+--]]
 
+-- [[ maps
 local map = require('globals').Map
 map('<leader>w', '<cmd>wall<cr>')
 map('<Backspace>', '<cmd>q<cr>');map('qu', '<cmd>wq<cr>')
@@ -36,6 +46,7 @@ map('<leader>hh', '"+yy')
 
 map('<leader>n', ':cnext<cr>');
 map('<leader>p', ':cprev<cr>');
+--]]
 
 vim.diagnostic.config {
     float = { border = 'rounded', source = 'if_many' },
