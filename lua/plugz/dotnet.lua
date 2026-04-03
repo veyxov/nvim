30c0a5cfde96db6d278a3948c0e5802a3189f036
Shekhov Ismoil <veyxov@gmail.com>
2026-04-03 16:49:55 +0500

    don't know what I was doing...

--- lua/plugz/dotnet.lua
+++ lua/plugz/dotnet.lua
@@ -1,11 +1,55 @@
 return {
-    'GustavEikaas/easy-dotnet.nvim',
-    ft = { 'cs', 'csharp', "c_sharp" },
-    build = ':Dotnet _server update',
-    config = function()
-        local dotnet = require 'easy-dotnet'
-        dotnet.setup({
-            picker = 'snacks'
-        })
-    end
+  "seblj/roslyn.nvim",
+  lazy = false,
+  config = function()
+    require("roslyn").setup({
+      config = {
+        settings = {
+          ["csharp|background_analysis"] = {
+            dotnet_compiler_diagnostics_scope = "fullSolution"
+          },
+          ["csharp|inlay_hints"] = {
+            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+            csharp_enable_inlay_hints_for_types = true,
+            dotnet_enable_inlay_hints_for_indexer_parameters = true,
+            dotnet_enable_inlay_hints_for_literal_parameters = true,
+            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+            dotnet_enable_inlay_hints_for_other_parameters = true,
+            dotnet_enable_inlay_hints_for_parameters = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+          },
+          ["csharp|code_lens"] = {
+            dotnet_enable_references_code_lens = true,
+          },
+        }
+      },
+      exe = {
+        "dotnet",
+        vim.fs.joinpath(vim.fn.stdpath("data"), "roslyn", "Microsoft.CodeAnalysis.LanguageServer.dll"),
+      },
+      filewatching = true,
+    })
+
+
+    vim.keymap.set("n", "<leader>p", function()
+      local clients = vim.lsp.get_clients()
+      for _, value in ipairs(clients) do
+        if value.name == "roslyn" then
+          vim.notify("roslyn client found")
+          value.rpc.request("workspace/diagnostic", { previousResultIds = {} }, function(err, result)
+            if err ~= nil then
+              print(vim.inspect(err))
+            end
+            if result ~= nil then
+              print(vim.inspect(result))
+            end
+          end)
+        end
+      end
+    end, { noremap = true, silent = true })
+  end
 }
