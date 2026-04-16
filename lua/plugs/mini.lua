665b198c91cf1c5502b9864f29e16c74605645d2
Shekhov Ismoil <veyxov@gmail.com>
2026-04-16 14:11:09 +0500

    mini: reshape

--- lua/plugs/mini.lua
+++ lua/plugs/mini.lua
@@ -37,16 +37,26 @@ require 'mini.surround'.setup({
 })
 
 require('mini.jump2d').setup({
-  labels = "etaoinshrdlcumwfgypbvkjxqz",
+  labels = 'etaioshrdlc',
   view = {
     dim = true,
-    n_steps_ahead = 2,
+    n_steps_ahead = 1,
   }
 })
 map('s', function()
   MiniJump2d.start(MiniJump2d.builtin_opts.single_character)
   end, { 'o', 'x', 'n' }
 )
+
+require 'mini.jump'.setup({
+  mappings = {
+    forward = 'f',
+    backward = 'F',
+    -- can't use these, because using t as the second Leader key
+    forward_till = '',
+    backward_till = '',
+  },
+})
 
 require 'mini.operators'.setup({
   replace = { prefix = 'r' },
@@ -57,7 +67,6 @@ require 'mini.operators'.setup({
 -- currently slow...
 -- require 'mini.animate'.setup()
 
-require 'mini.diff'.setup()
 
 require 'mini.statusline'.setup()
 require 'mini.tabline'.setup()
@@ -70,4 +79,26 @@ require 'mini.starter'.setup()
 
 require 'mini.splitjoin'.setup({mappings = {toggle = 'S'}})
 
--- next: mini-extra, mini-misc and todo highlight also bracketed
+require 'mini.bracketed'.setup()
+
+local hipatterns = require('mini.hipatterns')
+hipatterns.setup({
+  highlighters = {
+    -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+    fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+    todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
+    note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
+
+    -- Highlight hex color strings (`#rrggbb`) using that color
+    hex_color = hipatterns.gen_highlighter.hex_color(),
+  },
+})
+
+require 'mini.cmdline'.setup();
+
+require 'mini.diff'.setup {
+  view = {
+      style = 'sign',
+      signs = {add = '│', change = '│', delete = '_'},
+  }
+}
