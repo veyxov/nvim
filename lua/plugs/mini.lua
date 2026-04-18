a68bf97360ad981111b13a3c91d4ffca45985cd3
Shekhov Ismoil <veyxov@gmail.com>
2026-04-18 18:56:52 +0500

    mini: changes

--- lua/plugs/mini.lua
+++ lua/plugs/mini.lua
@@ -3,7 +3,7 @@ vim.pack.add({'https://github.com/nvim-mini/mini.nvim'})
 
 --{{{basics
 require 'mini.basics'.setup()
-require 'mini.icons'.setup()
+require 'mini.icons' .setup()
 --}}}
 
 --{{{ files
@@ -19,26 +19,37 @@ map('-', function() MiniFiles.open() end)
 --}}}
 
 --{{{ picker
-require 'mini.pick'.setup()
+require 'mini.pick'.setup {
+  mappings = {
+    choose_in_split   = '<C-x>',
+    choose_in_vsplit  = '<C-v>',
+    choose_marked     = '<C-d>',
+
+    mark     = '<C-s>',
+    refine = '<C-n>'
+  },
+  options = {use_cache = true}
+}
 map('<leader>t', function() MiniPick.builtin.files() end)
 map('<leader>g', function() MiniPick.builtin.grep_live() end)
 --}}}
 
 --{{{edit
-require 'mini.ai'.setup()
-require 'mini.pairs'.setup()
+require 'mini.ai'       .setup()
+require 'mini.pairs'    .setup()
 require 'mini.splitjoin'.setup({mappings = {toggle = 'S'}})
 require 'mini.bracketed'.setup()
+require 'mini.align'    .setup()
 --}}}
 
 --{{{surround
 require 'mini.surround'.setup({
-  mappings = {
-    add = 'ya',
-    delete = 'ds',
-    replace = 'cs',
+  mappings    = {
+    add       = 'ya',
+    delete    = 'ds',
+    replace   = 'cs',
 
-    find = 'yn',
+    find      = 'yn',
     find_left = 'yp',
     highlight = 'yh'
   }
@@ -62,10 +73,10 @@ map('s', function()
 --{{{jump
 require 'mini.jump'.setup({
   mappings = {
-    forward = 'f',
-    backward = 'F',
+    forward       = 'f',
+    backward      = 'F',
     -- can't use these, because using t as the second Leader key
-    forward_till = '',
+    forward_till  = '',
     backward_till = '',
   },
 })
@@ -73,24 +84,24 @@ require 'mini.jump'.setup({
 
 --{{{operators
 require 'mini.operators'.setup({
-  replace = { prefix = 'r' },
+  replace  = { prefix = 'r' },
   multiply = { prefix = 'gl' },
   exchange = { prefix = 'gt' }
 })
 --}}}
 
 --{{{ui
-require 'mini.starter'.setup()
-require 'mini.statusline'.setup()
-require 'mini.tabline'.setup()
+-- require 'mini.starter'    .setup()
+require 'mini.statusline' .setup()
+require 'mini.tabline'    .setup()
 require 'mini.indentscope'.setup()
-require 'mini.trailspace'.setup()
-map('<leader>ds', function() MiniTrailspace.trim(); MiniTrailspace.trim_last_lines(); end)
-require 'mini.cmdline'.setup();
-require 'mini.diff'.setup {
+require 'mini.trailspace' .setup()
+require 'mini.cmdline'    .setup()
+require 'mini.diff'       .setup {
   view = {
       style = 'sign',
       signs = {add = '│', change = '│', delete = '_'},
   }
 }
+map('<leader>ds', function() MiniTrailspace.trim(); MiniTrailspace.trim_last_lines(); end)
 --}}}
