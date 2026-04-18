8bd1b8d441cdafcba8221e96c2c8bfa989e8a22c
Shekhov Ismoil <veyxov@gmail.com>
2026-04-18 21:02:53 +0500

    options: don't remove clipboard_provider plugin
    causes errors on system clipboard copy, or at least you need to add your
    specific way of copy... maybe switching to different OSes will be
    harder...

--- lua/options.lua
+++ lua/options.lua
@@ -20,9 +20,9 @@ local disabled_builtins = {
   'gzip',    'tar',           'tarPlugin',     'zip',               'zipPlugin',    'getscript', 'getscriptPlugin',
   'vimball', 'vimballPlugin', 'matchit',       'matchparen',        '2html_plugin', 'logiPat',   'rrhelper',
   'netrw',   'netrwPlugin',   'netrwSettings', 'netrwFileHandlers', 'spellfile_plugin', 'tutor_mode_plugin',
-  'remote_plugins', 'nvim_net_plugin', 'man', 'clipboard_provider'
+  'remote_plugins', 'nvim_net_plugin', 'man'
 }
--- still there: treesitter, shada_plugin
+-- still there: treesitter, shada_plugin, 'clipboard_provider'
 
 require 'vim._core.ui2'.enable()
 for _, plugin in ipairs(disabled_builtins) do
