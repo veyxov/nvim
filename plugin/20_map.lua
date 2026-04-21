89a718262fbfff3d3a40f0bd15f82cdb377fd950
Shekhov Ismoil <veyxov@gmail.com>
2026-04-21 17:23:49 +0500

    maps

--- plugin/20_map.lua
+++ plugin/20_map.lua
@@ -12,7 +12,11 @@ cmap('<C-W><C-W>', 'q');
 -- ctrl+v is for paste in my terminal, so this is how to enter the block-visual mode
 map('<leader>v', '<C-V>')
 
--- yank key y -> h
+-- The ancient tradition of hjkl slumbers in a dedicated layer.
+-- Here on the home row of Hands-Down-Gold, prime real estate is reborn:
+-- 'h' becomes the harvester, 'j' and 'k' the gates of scope.
+map('j', '[', { 'n', 'v', 'x' }, { remap = true, silent = true })
+map('k', ']', { 'n', 'v', 'x' }, { remap = true, silent = true })
 map('h', 'y', { 'n', 'v', 'x'}); map('hh', 'yy')
 map('<leader>h', '"+y', {'n', 'v', 'x'}); map('<leader>hh', '"+yy')
 
@@ -41,3 +45,14 @@ cmap('<S-Tab>', 'bprevious')
 map('==', function() vim.fn.system(string.format("wl-copy --type text/uri-list 'file://%s'", vim.fn.expand('%:p'))) end)
 
 -- C-o is hard, maybe gl to go back?
+cmap('t?', 'lua vim.diagnostic.open_float()')
+cmap('ta', 'lua vim.lsp.buf.code_action()')
+cmap('ti', 'lua vim.lsp.buf.implementation()')
+cmap('th', 'lua vim.lsp.buf.hover()')
+cmap('tl', 'lua vim.lsp.codelens.run()')
+cmap('tc', 'lua vim.lsp.buf.rename()')
+cmap('tR', 'lua vim.lsp.buf.references()')
+cmap('ts', 'lua vim.lsp.buf.definition()')
+cmap('td', 'lua vim.lsp.buf.type_definition()')
+
+cmap('tr', 'Pick lsp scope="references"')
