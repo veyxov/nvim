9a116ce566ff5b31f4cad93a12e0db3ccc25c74f
Shekhov Ismoil <veyxov@gmail.com>
2026-04-24 10:14:36 +0500

    better sql in c# injections

--- after/queries/c_sharp/injections.scm
+++ after/queries/c_sharp/injections.scm
@@ -1,9 +1,52 @@
 ;; extends
 
-((argument
-   (raw_string_literal
-     (raw_string_content) @injection.content))
+;; Dapper: first positional arg is SQL (raw or regular string)
+((invocation_expression
+   function: (member_access_expression
+     name: (identifier) @_method)
+   arguments: (argument_list
+     .
+     (argument
+       [(raw_string_literal (raw_string_content) @injection.content)
+        (string_literal (string_literal_content) @injection.content)])))
+ (#any-of? @_method
+   "Query" "QueryAsync"
+   "QueryFirst" "QueryFirstAsync"
+   "QueryFirstOrDefault" "QueryFirstOrDefaultAsync"
+   "QuerySingle" "QuerySingleAsync"
+   "QuerySingleOrDefault" "QuerySingleOrDefaultAsync"
+   "Execute" "ExecuteAsync"
+   "ExecuteScalar" "ExecuteScalarAsync"
+   "QueryMultiple" "QueryMultipleAsync")
  (#set! injection.language "sql")
  (#set! injection.include-children)
- (#set! injection.self-contained)
- (#set! priority 200))
+ (#set! injection.self-contained))
+
+;; Dapper CommandDefinition: first arg is SQL
+((object_creation_expression
+   type: (identifier) @_type
+   arguments: (argument_list
+     .
+     (argument
+       [(raw_string_literal (raw_string_content) @injection.content)
+        (string_literal (string_literal_content) @injection.content)])))
+ (#eq? @_type "CommandDefinition")
+ (#set! injection.language "sql")
+ (#set! injection.include-children)
+ (#set! injection.self-contained))
+
+;; Local var named sql/query/command/cmd (raw or regular string)
+((local_declaration_statement
+   (variable_declaration
+     (variable_declarator
+       (identifier) @_name
+       [(raw_string_literal (raw_string_content) @injection.content)
+        (string_literal (string_literal_content) @injection.content)])))
+ (#any-of? @_name
+   "sql" "Sql" "SQL"
+   "query" "Query"
+   "command" "Command"
+   "cmd" "Cmd")
+ (#set! injection.language "sql")
+ (#set! injection.include-children)
+ (#set! injection.self-contained))
