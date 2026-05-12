489e66123097f0f77df4c78ca3a16ae1318968e4
Shekhov Ismoil <veyxov@gmail.com>
2026-05-12 09:15:21 +0500

    better tap and ui

--- plugin/10_opt.lua
+++ plugin/10_opt.lua
@@ -7,7 +7,16 @@ o.shada = "'20,<50,s10,h"
 
 o.cmdheight = 0
 o.laststatus = 3
+o.cursorline = true
 
 o.expandtab = true
+o.shiftwidth = 4
+o.tabstop = 4
+
+o.ignorecase = true
+o.smartcase = true
+
+o.updatetime = 250
+o.timeoutlen = 225
 
 Cfg.au('TextYankPost', { pattern = '*', callback = function() vim.highlight.on_yank() end })
