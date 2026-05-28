0fb0c226cc782a29523bee442780d0aba12f2ef8
Shekhov Ismoil <veyxov@gmail.com>
2026-05-28 12:14:13 +0500

    treesitter: expand parsers to daily-edited filetypes
    Add lua, markdown(+inline), json/jsonc, yaml/toml, vim/vimdoc,
    bash, regex, diff, gitcommit, go. Extend FileType seed list so
    vim.treesitter.start fires on help/sh/zsh/gitcommit/diff buffers.

--- plugin/25_plug.lua
+++ plugin/25_plug.lua
@@ -11,14 +11,25 @@ now_if_args(function()
         add({'https://github.com/nvim-treesitter/nvim-treesitter'})
         add({'https://github.com/nvim-treesitter/nvim-treesitter-textobjects'})
 
-        local languages = {'c_sharp', 'sql', 'http'}
+        local languages = {
+                'c_sharp', 'sql', 'http', 'go',
+                'lua', 'markdown', 'markdown_inline',
+                'json', 'jsonc', 'yaml', 'toml',
+                'vim', 'vimdoc', 'bash',
+                'regex', 'diff', 'gitcommit',
+        }
         local isnt_installed = function(lang)
                 return #vim.api.nvim_get_runtime_file('parser/' .. lang .. '.*', false) == 0
         end
         local to_install = vim.tbl_filter(isnt_installed, languages)
         if #to_install > 0 then require('nvim-treesitter').install(to_install) end
 
-        local filetypes = { 'cs', 'sql', 'markdown', 'go', 'http' }
+        local filetypes = {
+                'cs', 'sql', 'markdown', 'go', 'http',
+                'lua', 'json', 'jsonc', 'yaml', 'toml',
+                'vim', 'help', 'sh', 'bash', 'zsh',
+                'gitcommit', 'diff',
+        }
         for _, lang in ipairs(languages) do
                 for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
                         table.insert(filetypes, ft)
