73f36be18b4abec7a931b36ab314282b5a4beafd
Shekhov Ismoil <veyxov@gmail.com>
2026-08-05 17:10:43 +0500

    editor QoL: confirm, live :s preview, autoread, virtual_lines diagnostics, ui2

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -14,8 +14,19 @@ o.signcolumn = 'yes'
 o.swapfile = false
 o.updatetime = 250
 o.timeoutlen = 400
+o.confirm = true
+o.inccommand = 'split'
+o.autoread = true
 
 vim.diagnostic.config {
   severity_sort = true,
-  virtual_text = { prefix = '●' }
+  virtual_lines = { current_line = true },
 }
+
+-- autoread alone only checks on a few triggers; force it on focus/buffer entry
+-- (matters with mini.git branch switches / external git pull)
+autocmd({ 'FocusGained', 'BufEnter' }, 'autoread', { command = 'checktime' })
+
+-- experimental (0.12): kills "Press ENTER to continue", ephemeral msg window
+-- instead of the classic msg area -- fits cmdheight=0 better than the default
+require 'vim._core.ui2'.enable()
