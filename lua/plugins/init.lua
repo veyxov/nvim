cd3e975e94c2b717feea96e9909ce7a23d66d5a7
Shekhov Ismoil <veyxov@gmail.com>
2026-08-05 15:28:55 +0500

    format on save via attached lsp client

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -23,3 +23,8 @@ require 'plugins.kulala'
 require 'plugins.copilot'
 
 vim.cmd.colorscheme 'wildcharm'
+
+-- format on save with whatever lsp client attached (roslyn/easy-dotnet); no-op if none support it
+autocmd('BufWritePre', 'fmt', {
+  callback = function(a) vim.lsp.buf.format({ bufnr = a.buf, timeout_ms = 1000 }) end,
+})
