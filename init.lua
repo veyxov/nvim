deb20505578c828787a850b0c092b01c7a0465e0
Shekhov Ismoil <veyxov@gmail.com>
2026-09-18 13:01:25 +0500

    init: cleanup

--- init.lua
+++ init.lua
@@ -1,12 +1,8 @@
 vim.loader.enable()
 
-local function disable(names, value, prefix, suffix)
-  prefix, suffix = prefix or 'loaded_', suffix or '_provider'
-  for _, n in ipairs(names) do vim.g[prefix .. n .. suffix] = value end
+local function disable(t, val, suffix)
+  for _, n in ipairs(t) do vim.g['loaded_' .. n .. (suffix or '')] = val end
 end
 
-disable({
-  'netrw', 'netrwPlugin', 'tarPlugin', 'zipPlugin', 'gzip',
-  '2html_plugin', 'tutor_mode_plugin', 'matchit', 'spellfile_plugin', 'fzf',
-}, 1, nil, '')
-disable({ 'python3', 'ruby', 'perl', 'node' }, 0)
+disable({ 'netrw', 'netrwPlugin', 'tarPlugin', 'zipPlugin', 'gzip', '2html_plugin', 'tutor_mode_plugin', 'matchit', 'spellfile_plugin', 'fzf' }, 1)
+disable({ 'python3', 'ruby', 'perl', 'node' }, 0, '_provider')
